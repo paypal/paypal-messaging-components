fe2e10bf47a8f042172d95e62ec53ebc26030ef7
Nathan Schott <nschott3@gmail.com>
2019-06-28 15:41:33 -0400

    Fix warning log

--- src/services/logger/index.js
+++ src/services/logger/index.js
@@ -98,7 +98,7 @@ export const logger = {
     },
     track: sendBeacon,
     warn(...messages) {
-        console.warn(['PayPal Messages'], ...messages);
+        console.warn('[PayPal Messages]', ...messages);
     }
 };
 
