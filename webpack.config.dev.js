ee810a27a660065ffbeb7db1342ba2206c5a8f03
Nathan Schott <nschott3@gmail.com>
2019-06-28 08:47:16 -0400

    Setup demo pages for all integration types

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,43 +9,41 @@ const HOSTNAME = 'localhost.paypal.com';
 const PORT = 8080;
 
 module.exports = (env = {}) => {
-    let config;
-    if (env.standalone || env.analyze) {
-        config = getWebpackConfig({
-            entry: './src/index.js',
-            filename: `messaging.js`,
-            libraryTarget: 'window',
-            modulename: ['paypal', 'Messages'],
-            debug: true,
-            minify: true,
-            env: 'sandbox',
-            vars: { ...globals(env), __SDK__: false }
-        });
-        config.output.libraryExport = 'Messages';
-    } else {
-        config = getWebpackConfig({
-            entry: './paypal.dev.js',
-            filename: `${FILE_NAME}.js`,
-            debug: true,
-            minify: false,
-            env: 'local',
-            vars: {
-                ...globals(env),
-                __PROTOCOL__: PROTOCOL,
-                __HOST__: `${HOSTNAME}:${PORT}`,
-                __SDK_HOST__: `${HOSTNAME}:${PORT}`,
-                __PORT__: PORT,
-                __PATH__: `/${FILE_NAME}.js`,
-                __NAMESPACE__: 'paypal',
-                __VERSION__: '1.0.55',
-                __COMPONENTS__: ['messages']
-            }
-        });
-    }
+    const config = env.sdk
+        ? getWebpackConfig({
+              entry: './paypal.dev.js',
+              filename: `${FILE_NAME}.js`,
+              debug: true,
+              minify: false,
+              env: 'local',
+              vars: {
+                  ...globals(env),
+                  __PROTOCOL__: PROTOCOL,
+                  __HOST__: `${HOSTNAME}:${PORT}`,
+                  __SDK_HOST__: `${HOSTNAME}:${PORT}`,
+                  __PORT__: PORT,
+                  __PATH__: `/${FILE_NAME}.js`,
+                  __NAMESPACE__: 'paypal',
+                  __VERSION__: '1.0.55',
+                  __COMPONENTS__: ['messages']
+              }
+          })
+        : getWebpackConfig({
+              entry: './src/index.js',
+              filename: env.legacy ? 'merchant.js' : 'messaging.js',
+              libraryTarget: 'window',
+              modulename: ['paypal', 'Messages'],
+              debug: true,
+              minify: true,
+              env: 'local',
+              vars: { ...globals(env), __SDK__: false }
+          });
 
+    config.output.libraryExport = env.sdk ? '' : 'Messages';
     config.devServer = {
         contentBase: './demo',
         publicPath: '/',
+        openPage: env.sdk ? '' : env.legacy ? 'legacy.html' : 'standalone.html', // eslint-disable-line no-nested-ternary
         compress: true,
         host: 'localhost.paypal.com',
         port: 8080,
