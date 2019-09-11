7694e426ca5a391c64f1e25d77d874cabbadd727
Ryan Gilbert <gilbertstheman+github@gmail.com>
2019-09-11 12:01:11 -0400

    Add terms table styles

--- src/locale/DE/termsTable.js
+++ src/locale/DE/termsTable.js
@@ -6,31 +6,51 @@ export default function insertTermsTable(terms) {
     }
 
     return `
-        <h3>${offer.term} monatliche Zahlungen von je €${offer.monthly}</h3>
-        <br />
-        <table>
-            <tbody>
-                <tr>
-                    <td>E-Geld Transaktionsbetrag</td>
-                    <td>€${terms.amount}</td>
-                </tr>
-                <tr>
-                    <td>Effektiver Jahreszinssatz</td>
-                    <td>${offer.apr}%</td>
-                </tr>
-                <tr>
-                    <td>Fester Sollzinssatz</td>
-                    <td>${offer.nominalRate}%</td>
-                </tr>
-                <tr>
-                    <td>Zinsbetrag</td>
-                    <td>€${offer.totalInterest}</td>
-                </tr>
-                <tr>
-                    <td><b>Gesamtbetrag</b></td>
-                    <td><b>€${offer.total}</b></td>
-                </tr>
-            </tbody>
-        </table>
+        <style>
+            #terms-container {
+                max-width: 350px;
+                width: 100%;
+            }
+
+            #terms-table {
+                width: 100%;
+            }
+
+            h3 {
+                text-align: center;
+            }
+
+            #terms-table td:nth-child(2) {
+                text-align: right;
+            }
+        </style>
+        <div id="terms-container">
+            <h3>${offer.term} monatliche Zahlungen von je €${offer.monthly}</h3>
+            <hr />
+            <table id="terms-table">
+                <tbody>
+                    <tr>
+                        <td>E-Geld Transaktionsbetrag</td>
+                        <td>€${terms.amount}</td>
+                    </tr>
+                    <tr>
+                        <td>Effektiver Jahreszinssatz</td>
+                        <td>${offer.apr}%</td>
+                    </tr>
+                    <tr>
+                        <td>Fester Sollzinssatz</td>
+                        <td>${offer.nominalRate}%</td>
+                    </tr>
+                    <tr>
+                        <td>Zinsbetrag</td>
+                        <td>€${offer.totalInterest}</td>
+                    </tr>
+                    <tr>
+                        <td><b>Gesamtbetrag</b></td>
+                        <td><b>€${offer.total}</b></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     `;
 }
