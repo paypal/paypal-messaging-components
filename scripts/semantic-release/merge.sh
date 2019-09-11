cd5c56a03047bc47a2c1dfbbbd791cac0248800e
Nathan Schott <nschott3@gmail.com>
2019-09-11 12:33:41 -0400

    ci: merge.sh cleanup

--- scripts/semantic-release/merge.sh
+++ scripts/semantic-release/merge.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 REPO_URL=$1
-HASH=$(git rev-parse HEAD)
+RELEASE_HASH=$(git rev-parse HEAD)
 
-# Allows fetching and checking out other branches
-git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+{
+    # Allows fetching and checking out other branches
+    git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 
-# Allows pushing to remote
-git remote set-url origin ${REPO_URL} > /dev/null 2>&1
+    # Allows pushing to remote
+    git remote set-url origin ${REPO_URL}
 
-# Merge release changes into develop
-git pull --depth=1 origin develop
-git checkout develop
-git merge $HASH
-git push origin develop
-git checkout $HASH
+    # Merge changes into develop
+    git pull --depth=1 origin develop
+    git checkout develop
+    git merge $RELEASE_HASH
+    git push origin develop
+} > /dev/null
