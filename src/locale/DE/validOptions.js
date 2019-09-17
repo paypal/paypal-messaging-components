081acfb4936166ad592df8a718e4de9651600003
Ryan Gilbert <gilbertstheman+github@gmail.com>
2019-09-17 12:02:16 -0400

    Code comments

--- src/locale/DE/validOptions.js
+++ src/locale/DE/validOptions.js
@@ -12,9 +12,5 @@ export default {
     flex: {
         color: [Types.STRING, ['blue', 'black', 'white', 'gray|grey']],
         ratio: [Types.STRING, ['1x1', '1x4', '8x1', '20x1']]
-    },
-    custom: {
-        markup: [Types.STRING],
-        ratio: [Types.ANY]
     }
 };
--- tests/utils/setup.js
+++ tests/utils/setup.js
@@ -2,4 +2,4 @@ import '@testing-library/jest-dom/extend-expect';
 import globals from '../../globals';
 
 // Tests will currently fail with non-US locale
-Object.assign(window, globals({}));
+Object.assign(window, globals());
