4f5ec43b775f40bd806cb54ae62c415e91435053
Nathan <Seavenly@users.noreply.github.com>
2019-11-05 17:21:12 -0500

    fix: sandbox mock (#76)

--- src/utils/miscellaneous.js
+++ src/utils/miscellaneous.js
@@ -1,4 +1,6 @@
 import objectAssign from 'core-js-pure/stable/object/assign';
+import objectEntries from 'core-js-pure/stable/object/entries';
+import { ZalgoPromise } from 'zalgo-promise';
 
 import { partial } from './functional';
 
@@ -24,4 +26,46 @@ export function createCallbackError(message, cb) {
     error.onEnd = cb;
 
     return error;
+}
+
+export function request(method, url, { data, headers } = {}) {
+    return new ZalgoPromise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = () => {
+            if (xhttp.readyState === 4) {
+                const responseHeaders = xhttp
+                    .getAllResponseHeaders()
+                    .split('\n')
+                    .reduce((accumulator, header) => {
+                        const [key, val] = header.trim().split(': ');
+                        return {
+                            ...accumulator,
+                            [key]: val
+                        };
+                    }, {});
+
+                switch (xhttp.status) {
+                    case 200:
+                        resolve({ headers: responseHeaders, data: xhttp.responseText });
+                        break;
+                    case 204:
+                        resolve({ headers: responseHeaders });
+                        break;
+                    default:
+                        reject(new Error('Request failed'));
+                }
+            }
+        };
+
+        xhttp.open(method, url, true);
+
+        if (headers) {
+            objectEntries(headers).forEach(([header, value]) => {
+                xhttp.setRequestHeader(header, value);
+            });
+        }
+
+        xhttp.send(typeof data === 'object' ? JSON.stringify(data) : data);
+    });
 }
