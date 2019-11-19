3a3388b4a22ecf76f3df038a1527769f8cfd45a9
sai roja ramayanapu <sramayanapu@paypal.com>
2019-11-18 19:27:03 -0500

    added ni amount logic banner

---
+++ src/locale/US/mutations/ni_qualifying.js
@@ -0,0 +1,100 @@
+import Logo from '../logos';
+import { basicMediaQuery, legacyNI } from './ni';
+
+export default {
+    'layout:text': [
+        [
+            'default',
+            {
+                styles: [
+                    basicMediaQuery(290),
+                    '.message__messaging { flex: 1 1 auto; }',
+                    '@media (max-width: 289px) { .message__disclaimer { display: block; } }'
+                ],
+                logo: Logo.PRIMARY.COLOR,
+                headline: ['xsmall', { tag: 'medium', br: ['months'] }],
+                disclaimer: 'xsmall'
+            }
+        ],
+        ['logo.type:primary', { messageWidth: [130, 320] }],
+        [
+            'logo.type:inline',
+            {
+                messageWidth: [200, 1000],
+                styles: [basicMediaQuery(280)],
+                logo: Logo.ALT_NO_PP.COLOR,
+                headline: [
+                    { tag: 'xsmall', replace: [['time.', 'time']] },
+                    { tag: 'medium', br: ['purchases'] }
+                ]
+            }
+        ],
+        [
+            'logo.type:none',
+            {
+                messageWidth: [180, 1000],
+                styles: [basicMediaQuery(280)],
+                logo: false,
+                headline: [
+                    {
+                        tag: 'xsmall',
+                        replace: [['time.', 'time']],
+                        br: ['time']
+                    },
+                    {
+                        tag: 'medium',
+                        br: ['purchases']
+                    }
+                ]
+            }
+        ],
+        [
+            'logo.type:alternative',
+            {
+                styles: [basicMediaQuery(520)],
+                logo: Logo.ALTERNATIVE.COLOR,
+                headline: ['xsmall', { tag: 'medium', br: ['months'] }]
+            }
+        ],
+        ['logo.type:primary && logo.position:top', { styles: [basicMediaQuery(210)] }],
+        ['logo.type:alternative && logo.position:top', { styles: [basicMediaQuery(210)] }],
+        ['text.color:white && logo.type:primary', { logo: Logo.PRIMARY.WHITE }],
+        ['text.color:white && logo.type:alternative', { logo: Logo.ALTERNATIVE.WHITE }],
+        ['text.color:white && logo.type:inline', { logo: Logo.ALT_NO_PP.WHITE }]
+    ],
+
+    'layout:flex': [
+        [
+            'default',
+            {
+                logo: Logo.PRIMARY.WHITE,
+                headline: ['xsmall', { tag: 'medium', br: ['months'] }],
+                disclaimer: 'xsmall'
+            }
+        ],
+        [
+            'ratio:1x1',
+            {
+                headline: ['xsmall', 'medium'],
+                styles: ['@media (min-width: 150px) { .message__headline { font-size: 8vw } }']
+            }
+        ],
+        [
+            'ratio:1x4',
+            {
+                headline: { tag: 'medium', br: ['months'] },
+                styles: [
+                    '.message__logo-container { margin-bottom: 30%; }',
+                    '.message__disclaimer span.multi:nth-of-type(1) { display: none; }',
+                    '.message__headline { font-size: 1.1rem }'
+                ],
+                disclaimer: ['xlarge', 'xsmall']
+            }
+        ],
+        ['color:gray', { logo: Logo.PRIMARY.COLOR }],
+        ['color:white', { logo: Logo.PRIMARY.COLOR }],
+        ['color:white-no-border', { logo: Logo.PRIMARY.COLOR }]
+    ],
+
+    'layout:legacy': legacyNI
+};
