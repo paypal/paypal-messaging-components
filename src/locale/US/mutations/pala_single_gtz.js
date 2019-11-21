ef0e22a34a7a06a1053878f6b3001b2ecb131282
sai roja ramayanapu <sramayanapu@paypal.com>
2019-11-20 21:31:38 -0500

    removed No money due today language

--- src/locale/US/mutations/pala_single_gtz.js
+++ src/locale/US/mutations/pala_single_gtz.js
@@ -41,15 +41,13 @@ export default {
             {
                 logo: Logo.PRIMARY.WHITE,
                 headline: { tag: 'small', br: ['of'] },
-                subHeadline: 'small',
                 disclaimer: 'xsmall'
             }
         ],
         [
             'ratio:1x4',
             {
-                headline: { br: ['payments'] },
-                subHeadline: { tag: 'small', br: ['money'] }
+                headline: { br: ['payments'] }
             }
         ],
         ['color:gray', { logo: Logo.PRIMARY.COLOR }],
