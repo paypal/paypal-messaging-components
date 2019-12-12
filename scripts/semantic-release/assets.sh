9b755f630ff4d433814124e7c0e72a41d3af9ee6
Nathan Schott <nschott3@gmail.com>
2019-12-12 13:03:46 -0500

    Refactor modal build script

--- scripts/semantic-release/assets.sh
+++ scripts/semantic-release/assets.sh
@@ -5,11 +5,11 @@ rm -rf ./dist
 
 npm run --silent build:standalone -- --bail --display none
 npm run --silent build:legacy -- --bail --display none
+npm run --silent build:components -- --bail --display none
 
-head -n 1 ./dist/messaging.js > ./dist/messaging@"$VERSION".js
-echo "//# sourceMappingURL=messaging@${VERSION}.js.map" >> ./dist/messaging@"$VERSION".js
-cp ./dist/messaging.js.map ./dist/messaging@"$VERSION".js.map
-
-head -n 1 ./dist/merchant.js > ./dist/merchant@"$VERSION".js
-echo "//# sourceMappingURL=merchant@${VERSION}.js.map" >> ./dist/merchant@"$VERSION".js
-cp ./dist/merchant.js.map ./dist/merchant@"$VERSION".js.map
+for fullfile in ./dist/*.js; do
+    filename=$(basename $fullfile .js)
+    head -n 1 ./dist/"$filename".js > ./dist/messaging@"$VERSION".js
+    echo "//# sourceMappingURL=$filename@$VERSION.js.map" >> ./dist/"$filename"@"$VERSION".js
+    cp ./dist/"$filename".js.map ./dist/"$filename"@"$VERSION".js.map
+done
--- webpack.config.components.js
+++ webpack.config.components.js
@@ -3,11 +3,13 @@ const { getWebpackConfig } = require('grumbler-scripts/config/webpack.config');
 const globals = require('./globals');
 
 module.exports = (env = {}) => {
-    const config = getWebpackConfig({
+    const MODAL_ZOID_COMPONENT = getWebpackConfig({
         entry: './src/messages/models/SmartModal/component.js',
-        filename: 'modal-interface.js',
+        filename: 'modal.js',
+        minify: true,
+        web: true,
         vars: globals(env)
     });
 
-    return config;
+    return [MODAL_ZOID_COMPONENT];
 };
