c9c80f0e3b1c46f1bcef5d16f24181ea19ad8fd4
Ryan Gilbert <gilbertstheman+github@gmail.com>
2019-12-30 11:28:22 -0500

    fix update script

--- scripts/snapshot-tests/run-tests.sh
+++ scripts/snapshot-tests/run-tests.sh
@@ -8,6 +8,7 @@ if [[ "${DIRTY_SNAPSHOTS}" != "1" ]]; then
 elif [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "develop" ]]; then
     npm run test:func -- -u
     
+    git checkout develop
     git add .
     git commit -m "update snapshots [skip ci]"
     git push
