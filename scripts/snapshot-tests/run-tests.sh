48f3b49bd7ac99d91044a42942f24b2cdda8a57f
Ryan Gilbert <gilbertstheman+github@gmail.com>
2019-12-30 14:14:11 -0500

    cleanup

--- scripts/snapshot-tests/run-tests.sh
+++ scripts/snapshot-tests/run-tests.sh
@@ -1,15 +1,10 @@
-
-cat .travis.yml
-
-# Uses GNU sed syntax
-sed -i -e 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
-
-cat .travis.yml
-
 if [[ "${DIRTY_SNAPSHOTS}" != "1" ]]; then 
     npm run test:func
 elif [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "develop" ]]; then
     npm run test:func -- -u
+
+    # Uses GNU sed syntax
+    sed -i -e 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
     
     git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 
