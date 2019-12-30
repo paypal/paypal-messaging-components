7203170f5cfdfa9a5017a73b2728a6536f7ab9bb
Ryan Gilbert <gilbertstheman+github@gmail.com>
2019-12-30 12:39:12 -0500

    update script

--- scripts/snapshot-tests/run-tests.sh
+++ scripts/snapshot-tests/run-tests.sh
@@ -8,6 +8,8 @@ if [[ "${DIRTY_SNAPSHOTS}" != "1" ]]; then
 elif [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "develop" ]]; then
     npm run test:func -- -u
     
+    git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+
     git checkout develop
     git add .
     git commit -m "update snapshots [skip ci]"
