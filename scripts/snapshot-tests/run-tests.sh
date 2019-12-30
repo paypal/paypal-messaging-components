a02b235319ceea3813fc65f5a9dd6fa02999c180
Ryan Gilbert <gilbertstheman+github@gmail.com>
2019-12-30 13:33:43 -0500

    fix snapshot update trigger

--- scripts/snapshot-tests/run-tests.sh
+++ scripts/snapshot-tests/run-tests.sh
@@ -5,12 +5,14 @@ echo $TRAVIS_PULL_REQUEST
 
 if [[ "${DIRTY_SNAPSHOTS}" != "1" ]]; then 
     npm run test:func
+
+    sed -i '' -e 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
+
+    git diff
 elif [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "develop" ]]; then
     npm run test:func -- -u
     
     git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-
-    sed -i '' 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
 
     git checkout develop
     git add .
