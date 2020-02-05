ab8dd6d59586be776e75c9561fffc05f5800c122
Nathan Schott <nschott3@gmail.com>
2020-02-05 13:41:54 -0500

    refactor: use payer id passed from server

--- src/modal/lib/hooks/applyNow.js
+++ src/modal/lib/hooks/applyNow.js
@@ -1,17 +1,21 @@
+import { useContext } from 'preact/hooks';
+
 import { useXProps } from './helpers';
 import useTransitionState from './transitionState';
+import { ServerContext } from '../context';
 
 export default clickTitle => {
-    const { onClick, payerId, clientId, amount, refId } = useXProps();
+    const { payerId } = useContext(ServerContext);
+    const { onClick, amount, refId } = useXProps();
     const [, handleClose] = useTransitionState();
 
     return () => {
         onClick(clickTitle);
         // TODO: Get finalized query param keys
         const win = window.open(
-            `https://www.paypal.com/ppcreditapply/da/us?cats_id=DA_AD_OTHER&actor=merchant&mktgrefid=${refId}&${
-                payerId ? `payer_id=${payerId}` : `client_id=${clientId}`
-            }${amount ? `&amount=${amount}` : ''}`
+            `https://www.paypal.com/ppcreditapply/da/us?cats_id=DA_AD_OTHER&actor=merchant&mktgrefid=${refId}&payer_id=${payerId}${
+                amount ? `&amount=${amount}` : ''
+            }`
         );
         const intervalId = setInterval(() => {
             if (win.closed) {
