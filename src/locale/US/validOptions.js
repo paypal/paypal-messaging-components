45dd2b2467f3d83a51ae97f6ceb2c190c3c76d7f
Merlin <merlinpatt@gmail.com>
2020-02-05 14:55:28 -0500

    Add salmon option

--- src/locale/US/validOptions.js
+++ src/locale/US/validOptions.js
@@ -13,7 +13,7 @@ export default {
         }
     },
     flex: {
-        color: [Types.STRING, ['blue', 'black', 'white', 'white-no-border', 'gray|grey']],
+        color: [Types.STRING, ['blue', 'black', 'white', 'white-no-border', 'gray|grey', 'salmon']],
         ratio: [Types.STRING, ['1x1', '1x4', '8x1', '20x1']]
     },
     legacy: {
--- src/messages/models/Template/styles/flex/index.js
+++ src/messages/models/Template/styles/flex/index.js
@@ -9,6 +9,7 @@ import colorBlue from './css/color--blue.css';
 import colorGray from './css/color--gray.css';
 import colorBlueRatio1x4 from './css/color--blue&&ratio--1x4.css';
 import colorBlack from './css/color--black.css';
+import colorSalmon from './css/color--salmon.css';
 import colorWhite from './css/color--white.css';
 import colorWhiteNoBorder from './css/color--white-no-border.css';
 
@@ -23,6 +24,7 @@ export default [
     ['color:blue', colorBlue],
     ['color:gray', colorGray],
     ['color:black', colorBlack],
+    ['color:salmon', colorSalmon],
     ['color:white', colorWhite],
     ['color:white-no-border', colorWhiteNoBorder],
 
