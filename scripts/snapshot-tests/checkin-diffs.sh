b067e061d0ee917b6d32f3c2e839cb21f49510c2
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-02-07 16:09:19 -0500

    Add github comment

--- scripts/snapshot-tests/checkin-diffs.sh
+++ scripts/snapshot-tests/checkin-diffs.sh
@@ -8,7 +8,7 @@ echo TRAVIS_PULL_REQUEST_SLUG
 echo $TRAVIS_PULL_REQUEST_SLUG
 
 
-if [[ "$TRAVIS_TEST_RESULT" != "0" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then 
+if [[ "$TRAVIS_TEST_RESULT" != "0" ]]; then 
     node ./tests/functional/utils/collectDiffs.js
 
     mv tests/functional/__diff_output__ ../snapshots
@@ -25,11 +25,17 @@ if [[ "$TRAVIS_TEST_RESULT" != "0" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]];
     COMMIT_DATE=$(date +"%Y-%m-%d %H:%M:%S")
     SNAPSHOT_COUNT=$(ls -1q ./snapshots | wc -l)
 
-    git commit -m "$COMMIT_DATE: $SNAPSHOT_COUNT failed snapshots"
+    if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+        git commit -m "$COMMIT_DATE: $SNAPSHOT_COUNT failed snapshots"
 
-    git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-    git push --set-upstream origin $FAILED_SNAPSHOT_BRANCH
+        git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+        git push --set-upstream origin $FAILED_SNAPSHOT_BRANCH
 
-    SNAPSHOT_URL="https://github.com/${TRAVIS_REPO_SLUG}/tree/${FAILED_SNAPSHOT_BRANCH}/snapshots"
-    echo "$SNAPSHOT_COUNT failed snapshots viewable at $SNAPSHOT_URL"
+        SNAPSHOT_URL="https://github.com/${TRAVIS_REPO_SLUG}/tree/${FAILED_SNAPSHOT_BRANCH}/snapshots"
+        echo "$SNAPSHOT_COUNT failed snapshots viewable at $SNAPSHOT_URL"
+    else
+        curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+            -d "{\"body\": \"$SNAPSHOT_COUNT failed snapshots should be viewable at $SNAPSHOT_URL\"}" \
+            "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+    fi
 fi
