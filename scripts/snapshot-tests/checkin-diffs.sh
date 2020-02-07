1063868c8ae20ffa3a3cc9fbfdc051ed24a0419d
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-02-07 13:37:11 -0500

    update script

--- scripts/snapshot-tests/checkin-diffs.sh
+++ scripts/snapshot-tests/checkin-diffs.sh
@@ -3,12 +3,12 @@ FAILED_SNAPSHOT_BRANCH=failed-snapshots
 if [[ "$TRAVIS_TEST_RESULT" != "0" ]]; then 
     node ./tests/functional/utils/collectDiffs.js
 
-    mv tests/functional/__diff_output__ ..
+    mv tests/functional/__diff_output__ ../snapshots
 
     git checkout -b $FAILED_SNAPSHOT_BRANCH
 
     rm -rf ./snapshots
-    mv ../__diff_output__ ./snapshots
+    mv ../snapshots .
 
     git add .
 
@@ -17,5 +17,6 @@ if [[ "$TRAVIS_TEST_RESULT" != "0" ]]; then
 
     git commit -m "$COMMIT_DATE: $SNAPSHOT_COUNT failed snapshots"
 
+    git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
     git push --set-upstream origin $FAILED_SNAPSHOT_BRANCH
 fi
