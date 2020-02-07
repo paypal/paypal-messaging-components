d8b0d2c7f5e7e0b4bb34a5a94bdd196a2e05476f
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-02-07 09:46:23 -0500

    Test git functionality

--- scripts/snapshot-tests/checkin-diffs.sh
+++ scripts/snapshot-tests/checkin-diffs.sh
@@ -3,10 +3,16 @@ FAILED_SNAPSHOT_BRANCH=failed-snapshots
 echo TRAVIS_TEST_RESULT
 echo $TRAVIS_TEST_RESULT
 
-# if [[ "$TRAVIS_TEST_RESULT" != "1" ]]; then 
+if [[ "$TRAVIS_TEST_RESULT" != "0" ]]; then 
     node ./tests/functional/utils/collectDiffs.js
-    cd tests/functional/__diff_output__
-    ls
 
-    # git checkout $FAILED_SNAPSHOT_BRANCH
-# fi
+    mv tests/functional/__diff_output__ ..
+
+    git checkout $FAILED_SNAPSHOT_BRANCH
+
+    mv ../__diff_output__ tests/functional
+
+    git add .
+
+    git status
+fi
