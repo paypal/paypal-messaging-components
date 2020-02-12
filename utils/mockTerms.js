15cd5fdcb0c73de1e2ba2de55b7d2af02a08f4e3
Nathan Schott <nschott3@gmail.com>
2020-02-12 13:46:03 -0500

    Fix mock terms for generic ezp

--- utils/mockTerms.js
+++ utils/mockTerms.js
@@ -25,29 +25,27 @@ module.exports = (country, amount) => {
     const terms = JSON.parse(fs.readFileSync(path.resolve(__dirname, './terms.json'), 'utf-8'));
     const toLocaleString = localize(country);
 
-    return amount
-        ? {
-              type: 'pala',
-              maxAmount: terms.maxAmount,
-              minAmount: terms.minAmount,
-              amount,
-              formattedAmount: toLocaleString(amount),
-              offers: terms.offers.map(({ term, apr, nominalRate }) => {
-                  const total = amount + amount * (apr * 0.01) * (term / 12);
-                  return {
-                      term,
-                      type: 'INST',
-                      apr: toLocaleString(apr),
-                      nominalRate: toLocaleString(nominalRate),
-                      minValue: toLocaleString(terms.minAmount),
-                      qualified: amount > terms.minAmount && amount < terms.maxAmount,
-                      monthly: toLocaleString(total / term),
-                      total: toLocaleString(total),
-                      totalInterest: toLocaleString(total - amount)
-                  };
-              }),
-              formattedMinAmount: toLocaleString(terms.minAmount),
-              formattedMaxAmount: toLocaleString(terms.maxAmount)
-          }
-        : {};
+    return {
+        type: amount ? 'pala' : 'generic',
+        maxAmount: terms.maxAmount,
+        minAmount: terms.minAmount,
+        amount,
+        formattedAmount: toLocaleString(amount),
+        offers: terms.offers.map(({ term, apr, nominalRate }) => {
+            const total = amount + amount * (apr * 0.01) * (term / 12);
+            return {
+                term,
+                type: 'INST',
+                apr: toLocaleString(apr),
+                nominalRate: toLocaleString(nominalRate),
+                minValue: toLocaleString(terms.minAmount),
+                qualified: amount > terms.minAmount && amount < terms.maxAmount,
+                monthly: toLocaleString(total / term),
+                total: toLocaleString(total),
+                totalInterest: toLocaleString(total - amount)
+            };
+        }),
+        formattedMinAmount: toLocaleString(terms.minAmount),
+        formattedMaxAmount: toLocaleString(terms.maxAmount)
+    };
 };
