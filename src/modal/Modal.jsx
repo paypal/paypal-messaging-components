086f476c8211321e5ad1612d03fdd7fc49f73c73
Nathan Schott <nschott3@gmail.com>
2020-02-25 12:43:24 -0500

    fix: use Sass for compatibility with latest grumbler scripts

--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -5,12 +5,12 @@ import { ServerContext } from './lib/context';
 import TransitionState from './lib/transition';
 import Container from './parts/Container';
 import Content from './content/Content';
-import styles from './styles/common.css';
+import styles from './styles/common.scss';
 
 const Modal = ({ serverData }) => (
     <ServerContext.Provider value={serverData}>
         <TransitionState>
-            <style>{styles}</style>
+            <style>{styles._getCss()}</style>
             <Container>
                 <Content />
             </Container>
