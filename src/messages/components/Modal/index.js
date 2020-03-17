5aefb6553d1b5bf4dda5b3ee5a7ec28954ce2a6f
Nathan Schott <nschott3@gmail.com>
2020-03-17 11:58:46 -0400

    fix: prevent multiple same modals from rendering

--- src/messages/components/Modal/index.js
+++ src/messages/components/Modal/index.js
@@ -1,6 +1,7 @@
 import startsWith from 'core-js-pure/stable/string/starts-with';
 import { ZalgoPromise } from 'zalgo-promise';
 
+import { memoizeOnProps } from '../../../utils';
 import Modal from './component';
 
 function getModalType(offerCountry, offerType) {
@@ -13,8 +14,38 @@ function getModalType(offerCountry, offerType) {
     }
 }
 
+const renderModal = memoizeOnProps(
+    ({ options, meta, track, wrapper, type }) => {
+        const { render, hide, updateProps } = Modal({
+            account: options.account,
+            country: meta.offerCountry,
+            currency: options.currency,
+            type,
+            amount: options.amount,
+            refId: meta.messageRequestId,
+            onCalculate: amount => track({ et: 'CLICK', event_type: 'click', link: 'Calculator', amount }),
+            onClick: linkName => {
+                if (options.onApply && linkName.includes('Apply Now')) {
+                    options.onApply();
+                }
+                track({ et: 'CLICK', event_type: 'click', link: linkName });
+            },
+            onClose: linkName => {
+                wrapper.firstChild.focus();
+                track({ et: 'CLICK', event_type: 'modal-close', link: linkName });
+            }
+        });
+
+        hide();
+        // The render promise will resolve before Preact renders and picks up changes
+        // via updateProps so a small delay is added after the initial "render" promise
+        return { renderProm: render('body').then(() => ZalgoPromise.delay(100)), updateProps };
+    },
+    ['options', 'meta', 'type']
+);
+
 export default {
-    init({ options, meta, events, track, wrapper }) {
+    init: ({ options, meta, events, track, wrapper }) => {
         // For legacy image banners, open a popup instead of the modal
         if (options._legacy && startsWith(meta.offerType, 'NI')) {
             events.on('click', evt => {
@@ -34,31 +65,7 @@ export default {
             });
         } else {
             const modalType = getModalType(meta.offerCountry, meta.offerType);
-
-            const { render, hide, updateProps } = Modal({
-                account: options.account,
-                country: meta.offerCountry,
-                currency: options.currency,
-                type: modalType,
-                amount: options.amount,
-                refId: meta.messageRequestId,
-                onCalculate: amount => track({ et: 'CLICK', event_type: 'click', link: 'Calculator', amount }),
-                onClick: linkName => {
-                    if (options.onApply && linkName.includes('Apply Now')) {
-                        options.onApply();
-                    }
-                    track({ et: 'CLICK', event_type: 'click', link: linkName });
-                },
-                onClose: linkName => {
-                    wrapper.firstChild.focus();
-                    track({ et: 'CLICK', event_type: 'modal-close', link: linkName });
-                }
-            });
-
-            hide();
-            // The render promise will resolve before Preact renders and picks up changes
-            // via updateProps so a small delay is added after the initial "render" promise
-            const renderProm = render('body').then(() => ZalgoPromise.delay(100));
+            const { renderProm, updateProps } = renderModal({ options, meta, track, wrapper, type: modalType });
 
             events.on('click', () => {
                 renderProm.then(() => {
