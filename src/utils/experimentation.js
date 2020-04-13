1ae6014703ee6b1fdd0a8d260bbbdce01ef684ea
Nathan Schott <nschott3@gmail.com>
2020-04-13 13:06:03 -0400

    Catch failed ramp request

--- src/utils/experimentation.js
+++ src/utils/experimentation.js
@@ -2,4 +2,8 @@ import { memoize } from './functional';
 import { getGlobalUrl } from './global';
 import { request } from './miscellaneous';
 
-export const getWhitelist = memoize(() => request('GET', getGlobalUrl('RAMP_WHITELIST')).then(res => res?.data ?? []));
+export const getWhitelist = memoize(() =>
+    request('GET', getGlobalUrl('RAMP_WHITELIST'))
+        .then(res => res?.data ?? [])
+        .catch(() => [])
+);
