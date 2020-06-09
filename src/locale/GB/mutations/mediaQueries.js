e74ba5ff783e011d29bad2ac0558d02f6594603a
jfurman <jfurman@paypal.com>
2020-06-08 23:19:50 -0400

    Fixed message breakpoints and fallback logic. Added support for GB to the message snapshot functional tests.

--- src/locale/GB/mutations/mediaQueries.js
+++ src/locale/GB/mutations/mediaQueries.js
@@ -1,8 +1,68 @@
+// Media query used to fallback to "Pay Later with Flex."
+export function fallbackMediaQuery(breakpoint) {
+    return `
+    .message__headline {
+        white-space: nowrap;
+    }
+
+    .message__headline > span:nth-child(2) {
+        display: none;
+    }
+
+    @media (max-width: ${breakpoint}px) {
+        .message__headline > span:first-child {
+            display: none;
+        }
+
+        .message__headline > span:nth-child(2) {
+            display: inline;
+        }
+        
+        .message__headline span.multi:nth-child(2) > span {
+            white-space: nowrap;
+        }
+
+        .locale--GB .message__headline > span:last-child {
+            display:none;
+        }
+    } 
+`;
+}
+
+// Changes "PayPal Flex" font color to white.
+export function whiteTextMediaQuery(breakpoint) {
+    return `
+    @media (max-width: ${breakpoint}px) {
+        .locale--GB .message__headline span.multi:nth-child(2) > span:first-child::after {
+            color: white;
+        }
+        .product-name::before {
+            color: white;
+        }
+        .product-name {
+            color: white;
+        }
+    } 
+`;
+}
+
+// Moves PayPal logo to position right for logo primary message config.
 export function gbPLContentMediaQuery(breakpoint) {
     return `
     @media (min-width: ${breakpoint}px) {
         .locale--GB .message__content {
             display: inline-flex;
+        }
+    }
+    `;
+}
+
+// Changes disclaimer to inline at specified breakpoints for the logo alternative message config.
+export function gbPLAltContentMediaQuery(minBreakpoint, maxBreakpoint) {
+    return `
+    @media (min-width: ${minBreakpoint}px) and (max-width: ${maxBreakpoint}px) {
+        .message__disclaimer {
+            display: inline;
         }
     }
     `;
