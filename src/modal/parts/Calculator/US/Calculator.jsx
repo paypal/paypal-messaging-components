870f740f7ce8f1e92d2361301553d14953d9c9b4
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-06-17 17:46:44 -0400

    Modal loading fixes

--- src/modal/parts/Calculator/US/Calculator.jsx
+++ src/modal/parts/Calculator/US/Calculator.jsx
@@ -12,14 +12,8 @@ const Calculator = () => {
         <Fragment>
             <div className="calculator">
                 <h2 className="calculator__title">Enter a purchase amount to calculate your monthly Easy Payments.</h2>
-                <form className="calculator__form" onSubmit={submit}>
-                    <input
-                        className="calculator__input"
-                        type="text"
-                        value={value}
-                        onInput={changeInput}
-                        style={{ opacity: isLoading ? '0.5' : '1' }}
-                    />
+                <form className={`calculator__form ${isLoading ? 'calculator__form--loading' : ''}`} onSubmit={submit}>
+                    <input className="calculator__input" type="text" value={value} onInput={changeInput} />
                     <Button className="calculator__button" type="submit" secondary>
                         Calculate
                     </Button>
