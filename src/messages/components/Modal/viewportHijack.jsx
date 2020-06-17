97f5566ce4786c3ed8eedb4ba29a08b2560c5cb6
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-06-17 10:51:09 -0400

    Code comments

--- src/messages/components/Modal/viewportHijack.jsx
+++ src/messages/components/Modal/viewportHijack.jsx
@@ -6,28 +6,26 @@ import { createState } from '../../../utils';
 export default function viewportHijack() {
     const [viewportState, setViewportState] = createState({});
 
+    // Create hijack viewport
+    const newViewport = (
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui, shrink-to-fit=no"
+        />
+    ).render(dom({ doc: document }));
+
     return [
         () => {
             // Save existing body style
             const bodyStyle = document.body.getAttribute('style');
 
-            // Create hijack viewport
-            const newViewport = document.createElement('meta');
-            newViewport.setAttribute('name', 'viewport');
-            newViewport.setAttribute(
-                'content',
-                'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui, shrink-to-fit=no'
-            );
-
-            // Save old viewport
-            const existingViewport = document.head.querySelector('meta[name="viewport"]');
-
             // If no viewport exists, some browsers will not respect the change of simply removing the hijack viewport
             // Add a blank viewport to replace the hijack in this case
-            const placeholderViewport = (<meta name="viewport" content="" />).render(dom({ doc: document }));
-            const oldViewport = existingViewport || placeholderViewport;
+            const oldViewport =
+                document.head.querySelector('meta[name="viewport"]') ||
+                (<meta name="viewport" content="" />).render(dom({ doc: document }));
 
-            if (existingViewport) {
+            if (oldViewport.parentNode) {
                 document.head.removeChild(oldViewport);
             }
 
@@ -38,12 +36,11 @@ export default function viewportHijack() {
 
             setViewportState({
                 bodyStyle,
-                newViewport,
                 oldViewport
             });
         },
         () => {
-            document.head.removeChild(viewportState.newViewport);
+            document.head.removeChild(newViewport);
             document.head.appendChild(viewportState.oldViewport);
 
             document.body.setAttribute('style', viewportState.bodyStyle || '');
