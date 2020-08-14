5fcd19b413fa85a0264dc623ff2195b9905b076d
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-08-14 14:05:28 -0400

    Refactor logos file

--- server/locale/US/logos.js
+++ server/locale/US/logos.js
@@ -1,74 +1,35 @@
+const ROOT_URL = 'https://www.paypalobjects.com/upstream/assets/logos/US';
+const TYPES = ['COLOR', 'WHITE', 'BLACK', 'GRAYSCALE'];
+const TYPE_MAP = { COLOR: 'fc', WHITE: 'wh', BLACK: 'mono', GRAYSCALE: 'grayscale' };
+const getSvgSrc = svgFileName => `${ROOT_URL}/${svgFileName}.svg`;
+
 export default {
-    PRIMARY: {
-        COLOR: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_fc_alt.svg',
-            dimensions: [573, 80]
-        },
-        WHITE: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_wh_alt.svg',
-            dimensions: [573, 80]
-        },
-        BLACK: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_mono_alt.svg',
-            dimensions: [573, 80]
-        },
-        GRAYSCALE: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_grayscale_alt.svg',
-            dimensions: [573, 80]
-        }
-    },
-    ALTERNATIVE: {
-        COLOR: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_fc_alt.svg',
-            dimensions: [573, 80]
-        },
-        WHITE: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_wh_alt.svg',
-            dimensions: [573, 80]
-        },
-        BLACK: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_mono_alt.svg',
-            dimensions: [573, 80]
-        },
-        GRAYSCALE: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_grayscale_alt.svg',
-            dimensions: [573, 80]
-        }
-    },
-    ALT_NO_PAYPAL: {
-        COLOR: {
-            src: 'https://localhost.paypal.com:8080/upstream/assets/logos/US/ppc_fc_alt_no_paypal.svg',
-            dimensions: [96, 24]
-        },
-        WHITE: {
-            src: 'https://localhost.paypal.com:8080/upstream/assets/logos/US/ppc_wh_alt_no_paypal.svg',
-            dimensions: [96, 24]
-        },
-        BLACK: {
-            src: 'https://localhost.paypal.com:8080/upstream/assets/logos/US/ppc_mono_alt_no_paypal.svg',
-            dimensions: [96, 24]
-        },
-        GRAYSCALE: {
-            src: 'https://localhost.paypal.com:8080/upstream/assets/logos/US/ppc_grayscale_alt_no_paypal.svg',
-            dimensions: [96, 24]
-        }
-    },
-    ALT_NO_PP: {
-        COLOR: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_fc_alt_noPP.svg',
-            dimensions: [477, 64]
-        },
-        WHITE: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_wh_alt_noPP.svg',
-            dimensions: [477, 64]
-        },
-        BLACK: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_mono_alt_noPP.svg',
-            dimensions: [477, 64]
-        },
-        GRAYSCALE: {
-            src: 'https://www.paypalobjects.com/upstream/assets/logos/US/ppc_grayscale_alt_noPP.svg',
-            dimensions: [477, 64]
-        }
-    }
+    PRIMARY: TYPES.reduce(
+        (object, type) => ({
+            ...object,
+            [type]: { dimensions: [573, 80], src: getSvgSrc(`ppc_${TYPE_MAP[type]}_alt`) }
+        }),
+        {}
+    ),
+    ALTERNATIVE: TYPES.reduce(
+        (object, type) => ({
+            ...object,
+            [type]: { dimensions: [573, 80], src: getSvgSrc(`ppc_${TYPE_MAP[type]}_alt`) }
+        }),
+        {}
+    ),
+    ALT_NO_PAYPAL: TYPES.reduce(
+        (object, type) => ({
+            ...object,
+            [type]: { dimensions: [401, 100], src: getSvgSrc(`ppc_${TYPE_MAP[type]}_alt_no_paypal`) }
+        }),
+        {}
+    ),
+    ALT_NO_PP: TYPES.reduce(
+        (object, type) => ({
+            ...object,
+            [type]: { dimensions: [477, 64], src: getSvgSrc(`ppc_${TYPE_MAP[type]}_alt_noPP`) }
+        }),
+        {}
+    )
 };
