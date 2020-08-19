811b16d9399c07ca119662289f19b7576b90d14e
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-08-19 02:15:14 -0400

    Remove waitFor domcontentloaded

--- tests/functional/spec/createBannerTest.js
+++ tests/functional/spec/createBannerTest.js
@@ -38,8 +38,8 @@ const waitForBanner = async timeout => {
     try {
         // console.log('waitFor load');
         // await page.waitForNavigation({ waitUntil: 'load' }); // consider nav done when the load event is fired
-        console.log('waitFor domcontentloaded');
-        await page.waitForNavigation({ waitUntil: 'domcontentloaded' }); // consider nav done when the DOMContentLoaded event is fired
+        // console.log('waitFor domcontentloaded');
+        // await page.waitForNavigation({ waitUntil: 'domcontentloaded' }); // consider nav done when the DOMContentLoaded event is fired
         console.log('waitFor networkidle0');
         await page.waitForNavigation({ waitUntil: 'networkidle0' }); // consider nav done when no more than 0 network connections for at least 500 ms
 
