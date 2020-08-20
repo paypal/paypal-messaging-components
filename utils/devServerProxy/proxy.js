9b56f7c0331d99ba5f15c607b07525ad806a1feb
Julia Furman <jfurman@paypal.com>
2020-08-20 09:58:57 -0400

    Rename pi4 proxy accounts

--- utils/devServerProxy/proxy.js
+++ utils/devServerProxy/proxy.js
@@ -17,8 +17,8 @@ const devAccountMap = {
     DEV0000000PSG: ['US', 'EZP', 'pala_single_gtz'],
     DEV0000000PMZ: ['US', 'EZP', 'pala_multi_eqz'],
     DEV0000000PMG: ['US', 'EZP', 'pala_multi_gtz'],
-    DEV000000PAY4: ['US', 'PAY4', 'pay_in_four'],
-    DEV00000PAY4Q: ['US', 'PAY4', 'pay_in_four_q'],
+    DEV0000000PI4: ['US', 'PI4', 'pi4'],
+    DEV000000PI4Q: ['US', 'PI4Q', 'pi4q'],
 
     DEV0000000IAZ: ['DE', 'INST', 'inst_any_eqz'],
     DEV0000000IAG: ['DE', 'INST', 'inst_any_gtz'],
