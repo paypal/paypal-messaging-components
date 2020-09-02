f1338330f27cdf51e4fc35011ee338bbb4e36629
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-09-02 02:44:34 -0400

    Fix logo mutations

--- server/locale/US/mutations/common.js
+++ server/locale/US/mutations/common.js
@@ -6,13 +6,19 @@ import Logo from '../logos';
  */
 
 export const textLogoMutations = [
-    ['text.color:white && logo.type:primary', { logo: Logo.SINGLE_LINE.WHITE }],
+    ['text.color:white && logo.type:primary', { logo: [Logo.SINGLE_LINE_NO_PAYPAL.WHITE, Logo.SINGLE_LINE.WHITE] }],
     ['text.color:white && logo.type:alternative', { logo: Logo.SINGLE_LINE_NO_PAYPAL.WHITE }],
     ['text.color:white && logo.type:inline', { logo: Logo.SINGLE_LINE_NO_PP.WHITE }],
-    ['text.color:monochrome && logo.type:primary', { logo: Logo.SINGLE_LINE.BLACK }],
+    [
+        'text.color:monochrome && logo.type:primary',
+        { logo: [Logo.SINGLE_LINE_NO_PAYPAL.BLACK, Logo.SINGLE_LINE.BLACK] }
+    ],
     ['text.color:monochrome && logo.type:alternative', { logo: Logo.SINGLE_LINE_NO_PAYPAL.BLACK }],
     ['text.color:monochrome && logo.type:inline', { logo: Logo.SINGLE_LINE_NO_PP.BLACK }],
-    ['text.color:grayscale && logo.type:primary', { logo: Logo.SINGLE_LINE.GRAYSCALE }],
+    [
+        'text.color:grayscale && logo.type:primary',
+        { logo: [Logo.SINGLE_LINE_NO_PAYPAL.GRAYSCALE, Logo.SINGLE_LINE.GRAYSCALE] }
+    ],
     ['text.color:grayscale && logo.type:alternative', { logo: Logo.SINGLE_LINE_NO_PAYPAL.GRAYSCALE }],
     ['text.color:grayscale && logo.type:inline', { logo: Logo.SINGLE_LINE_NO_PP.GRAYSCALE }]
 ];
