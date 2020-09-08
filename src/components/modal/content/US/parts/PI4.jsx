e7928e92432abfdb54cf7b4805a91b4be560528b
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-09-08 15:12:32 -0400

    Update tabs appearance

--- src/components/modal/content/US/parts/PI4.jsx
+++ src/components/modal/content/US/parts/PI4.jsx
@@ -1,8 +1,8 @@
 /** @jsx h */
-import { h, Fragment } from 'preact';
+import { h } from 'preact';
 
 import { useServerData } from '../../../lib';
-import PieChart from './PieChart';
+import Icon from '../../../parts/Icon';
 
 const instructions = [
     'Pay in 4 is not available to residents of Wisconsin, North Dakota, Missouri, or any U.S. Territories.',
@@ -12,9 +12,6 @@ const instructions = [
 
 export default () => {
     const { terms } = useServerData();
-
-    // For now, only PI4 INST offer should be shown in this modal
-    const offer = terms.offers.find(ofr => ofr.type === 'INST' && ofr.qualified);
 
     return (
         <section className="content-body">
@@ -26,36 +23,13 @@ export default () => {
                     on purchases from ${terms.minAmount}-${terms.maxAmount}.
                 </p>
 
-                <div className="payment-breakdown">
-                    {offer ? (
-                        <Fragment>
-                            <div>
-                                <PieChart filledPercent={25} />
-                                <b>${offer.periodic}</b>
-                                <div>Today</div>
-                            </div>
-                            <div>
-                                <PieChart filledPercent={50} />
-                                <b>${offer.periodic}</b>
-                                <div>Week 2</div>
-                            </div>
-                            <div>
-                                <PieChart filledPercent={75} />
-                                <b>${offer.periodic}</b>
-                                <div>Week 4</div>
-                            </div>
-                            <div>
-                                <PieChart filledPercent={100} />
-                                <b>${offer.periodic}</b>
-                                <div>Week 6</div>
-                            </div>
-                        </Fragment>
-                    ) : null}
+                <div className="call-to-action">
+                    <p>
+                        Check out securely with <b>PayPal</b>
+                        <br /> and choose <b>Pay in 4</b>
+                    </p>
+                    <Icon name="secure" />
                 </div>
-
-                <p>
-                    Check out with <b>PayPal</b> and choose <b>Pay Later</b>
-                </p>
             </div>
 
             <hr className="divider" />
