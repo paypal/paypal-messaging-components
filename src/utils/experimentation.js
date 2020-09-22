92db81c42419dec1ab799d069ae8421161e64106
Nathan Schott <nschott3@gmail.com>
2020-09-22 14:19:16 -0400

    refactor: experiment returns treatment type

--- src/utils/experimentation.js
+++ src/utils/experimentation.js
@@ -1,21 +1,40 @@
+import isArray from 'core-js-pure/stable/array/is-array';
+import arrayIncludes from 'core-js-pure/stable/array/includes';
 import { ZalgoPromise } from 'zalgo-promise';
 
 import { memoize } from './functional';
 import { getGlobalUrl } from './global';
 import { request } from './miscellaneous';
 
-export const getExclusionList = memoize(() =>
+export const Treatment = {
+    CONTROL: 'CONTROL',
+    TEST: 'TEST'
+};
+
+const fallback = {
+    type: 'inclusion',
+    list: []
+};
+
+const getExperiment = memoize(() =>
     __ENV__ === 'local'
-        ? ZalgoPromise.resolve([])
-        : request('GET', getGlobalUrl('RAMP_EXCLUSION_LIST'))
-              .then(res => res?.data ?? [])
-              .catch(() => [])
+        ? ZalgoPromise.resolve(fallback)
+        : request('GET', getGlobalUrl('RAMP_EXPERIMENT'))
+              .then(res => res?.data ?? fallback)
+              .catch(() => fallback)
 );
 
-export const getInclusionList = memoize(() =>
-    __ENV__ === 'local'
-        ? ZalgoPromise.resolve([])
-        : request('GET', getGlobalUrl('RAMP_INCLUSION_LIST'))
-              .then(res => res?.data ?? [])
-              .catch(() => [])
+export const getExperimentTreatment = memoize(id =>
+    getExperiment().then(({ type, list }) => {
+        const ids = isArray(id) ? id : [id];
+
+        switch (type) {
+            case 'inclusion':
+                return ids.some(i => arrayIncludes(list, i)) ? Treatment.TEST : Treatment.CONTROL;
+            case 'exclusion':
+                return ids.some(i => arrayIncludes(list, i)) ? Treatment.CONTROL : Treatment.TEST;
+            default:
+                return Treatment.CONTROL;
+        }
+    })
 );
