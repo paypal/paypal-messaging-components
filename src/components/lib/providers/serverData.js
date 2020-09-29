ab545c219646bed6f17e349a25bb92cd30a06205
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-09-29 13:18:22 -0400

    Handle forward compat for modal

--- src/components/lib/providers/serverData.js
+++ src/components/lib/providers/serverData.js
@@ -5,7 +5,18 @@ import { useState, useContext } from 'preact/hooks';
 const ServerData = createContext({});
 
 export const ServerDataProvider = ({ children, data }) => {
-    const [serverData, setServerData] = useState(data);
+    // Needed to forward-support changes to modal content
+    // TODO: remove with new modal
+    const processedData = {
+        ...data
+    };
+    if (processedData.products) {
+        processedData.products = processedData.products
+            .map(prod => (typeof prod === 'string' ? prod : prod.meta.product))
+            .filter(prod => prod !== 'GPL');
+    }
+
+    const [serverData, setServerData] = useState(processedData);
 
     return <ServerData.Provider value={{ ...serverData, setServerData }}>{children}</ServerData.Provider>;
 };
