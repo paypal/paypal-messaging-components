4fa955a94359c2acfad622611f0f0b49fb1f835e
Nathan Schott <nschott3@gmail.com>
2020-09-29 15:26:57 -0400

    fix: pass offerType to validator and remove default preset

--- server/locale/US/GPL/validOptions.js
+++ server/locale/US/GPL/validOptions.js
@@ -10,13 +10,15 @@ export default {
             color: [Types.STRING, ['black', 'white', 'monochrome', 'grayscale|greyscale']],
             size: [Types.NUMBER, [12, 10, 11, 13, 14, 15, 16]],
             fontFamily: [Types.STRING]
-        }
+        },
+        preset: [Types.STRING, [undefined, 'smallest']]
     },
     flex: {
         color: [
             Types.STRING,
             ['blue', 'black', 'white', 'white-no-border', 'gray|grey', 'monochrome', 'grayscale|greyscale']
         ],
-        ratio: [Types.STRING, ['1x1', '1x4', '8x1', '20x1']]
+        ratio: [Types.STRING, ['1x1', '1x4', '8x1', '20x1']],
+        preset: [Types.STRING, [undefined, 'smallest']]
     }
 };
