46a1d4dd5ec72392b3f15ab140cf6534f52fa4b2
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-10-07 12:41:49 -0400

    Add top overlay

--- src/components/modal/content/US/parts/ContentWrapper.jsx
+++ src/components/modal/content/US/parts/ContentWrapper.jsx
@@ -5,13 +5,17 @@ import Content from './Content';
 import Header from '../../../parts/Header';
 import Container from '../../../parts/Container';
 import Icon from '../../../parts/Icon';
+import { useTransitionState } from '../../../lib';
 
 const ContentWrapper = () => {
+    const [, handleClose] = useTransitionState();
     const headerRef = useRef();
     const contentWrapper = useRef();
 
     return (
         <Container contentWrapper={contentWrapper} contentMaxWidth={640}>
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+            <div className="top-overlay" onClick={() => handleClose('Modal Overlay')} />
             <div className="content-wrapper" ref={contentWrapper}>
                 <div className="content-background">
                     <Header wrapperRef={headerRef}>
