b4ad55bb6fd298030e3d0f43d65164ddc32988d0
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-10-08 09:01:28 -0400

    Test: Update diff collection

--- tests/functional/utils/collectDiffs.js
+++ tests/functional/utils/collectDiffs.js
@@ -1,49 +1,58 @@
-/* eslint-disable no-console */
 const path = require('path');
 const fs = require('fs');
+const readline = require('readline');
 const imgur = require('imgur');
 
-const DIFF_DIR = path.resolve(__dirname, '../__diff_output__');
+const DIFF_FOLDER = '__diff_output__';
+const DIFF_DIR = path.resolve(__dirname, `../${DIFF_FOLDER}`);
+const DIFF_FOLDERS_LIST = path.resolve(__dirname, '../../../diff_folders.log');
 
-function collectDiffs() {
-    const searchFolders = (folder, pathTo) => {
-        const fullPath = path.resolve(pathTo, folder);
-        const contents = fs.readdirSync(fullPath);
-
-        contents.forEach(name => {
-            if (fs.statSync(path.resolve(fullPath, name)).isDirectory()) {
-                searchFolders(name, fullPath);
-            } else if (folder === '__diff_output__') {
-                const file = path.resolve(fullPath, name);
-                const configName = path
-                    .relative(path.resolve(__dirname, '../snapshots'), pathTo)
-                    .replace(/:/g, '-')
-                    .replace(/\//g, '__');
-
-                const subDir = file.includes('/modal/') ? 'modal' : 'banner';
-                fs.renameSync(file, path.resolve(DIFF_DIR, subDir, `${configName}__${name}`));
-            }
-        });
-    };
-
-    try {
-        fs.rmdirSync(DIFF_DIR);
-    } catch (e) {
-        if (e.code === 'ENOTEMPTY') {
-            console.log(e);
-        }
-    }
+const collectDiffs = async () => {
+    const snapshotPath = path.resolve(__dirname, '../snapshots');
 
     fs.mkdirSync(DIFF_DIR);
     fs.mkdirSync(path.resolve(DIFF_DIR, 'modal'));
     fs.mkdirSync(path.resolve(DIFF_DIR, 'banner'));
 
-    searchFolders('snapshots', path.resolve(__dirname, '..'));
-}
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream(DIFF_FOLDERS_LIST),
+        output: null,
+        console: false
+    });
 
-async function uploadToImgur(subDir) {
+    console.group('COLLECTING DIFFS'); // eslint-disable-line no-console
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const fullPath of readInterface) {
+        const contents = fs.readdirSync(fullPath);
+
+        console.group(`MOVING SNAPSHOTS in ${fullPath}`); // eslint-disable-line no-console
+        console.info(contents.length ? `${contents.join('\n  ')}` : `None found`); // eslint-disable-line no-console
+
+        contents.forEach(name => {
+            const file = path.resolve(fullPath, name);
+            const configName = path
+                .relative(snapshotPath, fullPath.replace(DIFF_FOLDER, ''))
+                .replace(/:/g, '-')
+                .replace(/\//g, '__');
+
+            const subDir = file.includes('/modal/') ? 'modal' : 'banner';
+            const newFile = path.resolve(DIFF_DIR, subDir, `${configName}__${name}`);
+            console.info(`Mv: ${file}\n  To: ${newFile}`); // eslint-disable-line no-console
+            fs.renameSync(file, newFile);
+        });
+
+        console.groupEnd(); // eslint-disable-line no-console
+    }
+    console.groupEnd(); // eslint-disable-line no-console
+};
+
+const uploadToImgur = async subDir => {
     const folder = path.resolve(DIFF_DIR, subDir);
     const snapshots = fs.readdirSync(folder);
+
+    console.group(`UPLOADING SNAPSHOTS for ${subDir}`); // eslint-disable-line no-console
+    console.info(snapshots.length ? `${snapshots.join('\n  ')}` : `None found`); // eslint-disable-line no-console
+    console.groupEnd(); // eslint-disable-line no-console
 
     if (snapshots.length > 0) {
         const album = await imgur.createAlbum();
@@ -52,23 +61,21 @@ async function uploadToImgur(subDir) {
             snapshots.map(fileName => imgur.uploadFile(path.resolve(folder, fileName), album.data.deletehash, fileName))
         );
 
-        console.log('\n');
-        console.log(
-            `\u001b[31m${result.length} failed ${subDir} snapshots uploaded and viewable at https://imgur.com/a/${album.data.id}`
-        );
-        console.log('\n');
+        const albumUrl = `https://imgur.com/a/${album.data.id}`;
+        // eslint-disable-next-line no-console
+        console.info(`\n\u001b[31m${result.length} failed ${subDir} snapshots uploaded and viewable at ${albumUrl}\n`);
     } else {
-        console.log(`No snapshots found in ${folder}`);
+        console.info(`No snapshots found in ${folder}`); // eslint-disable-line no-console
     }
 
     return snapshots.length;
-}
-
-collectDiffs();
+};
 
 (async () => {
-    const [bannerDiffs, modalDiffs] = await Promise.all([uploadToImgur('modal'), uploadToImgur('banner')]).catch(e =>
-        console.log(e)
+    await collectDiffs();
+
+    const [bannerDiffs, modalDiffs] = await Promise.all([uploadToImgur('modal'), uploadToImgur('banner')]).catch(
+        error => console.error(error) // eslint-disable-line no-console
     );
 
     if (bannerDiffs + modalDiffs > 0) {
