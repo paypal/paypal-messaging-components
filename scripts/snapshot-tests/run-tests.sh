d67d6405da7ad00bd073929f500b36325ad83ef1
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-10-08 09:03:32 -0400

    Test: update bash snapshot script

--- scripts/snapshot-tests/run-tests.sh
+++ scripts/snapshot-tests/run-tests.sh
@@ -1,17 +1,62 @@
-if [[ "$DIRTY_SNAPSHOTS" != "1" ]]; then 
-    npm run test:func
+#!/bin/bash
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+# set -Eevxo
+
+OUTPUT_FILE=test_output.log
+ERROR_FILE=test_errors.log
+DIFF_FOLDERS_LIST=diff_folders.log
+DIFF_FILES_LIST=diff_files.log
+
+showFailures () {
+    failedCount=$(grep failed $OUTPUT_FILE | wc -l)
+    if [[ $failedCount -gt 0 ]]; then
+        echo ''
+        echo 'FAILURES FOUND';
+        node ./tests/functional/utils/collectFailedTests.js
+
+        echo ''
+        snapCount=$(grep -h __diff_output__ $ERROR_FILE | wc -l)
+        echo "FULL LIST OF FAILED SNAPSHOTS ($snapCount found)"
+        grep -h __diff_output__ $ERROR_FILE -B 1
+        exit 1
+    fi
+}
+
+if [[ "$DIRTY_SNAPSHOTS" != "1" ]]; then
+    npm run test:func 2> $ERROR_FILE | tee $OUTPUT_FILE
+
+    rm -r ./tests/functional/__diff_output__
+    find ./tests/functional/snapshots -type d | grep -h __diff_output__ > $DIFF_FOLDERS_LIST
+    diffFolderCount=$(cat $DIFF_FOLDERS_LIST | wc -l)
+    echo 'DIFF FOLDERS FOUND'
+    cat $DIFF_FOLDERS_LIST
+    echo ''
+
+    find ./tests/functional/ -type f -name '*png' | grep -h __diff_output__
+    diffFileCount=$(cat $DIFF_FILES_LIST | wc -l)
+    echo "DIFF FILES FOUND ($diffFileCount found)"
+    cat $DIFF_FILES_LIST
+    echo ''
 
     node ./tests/functional/utils/collectDiffs.js
-elif [[ "$TRAVIS_PULL_REQUEST" = "false" ]] && [[ "$TRAVIS_BRANCH" = "develop" ]]; then
-    npm run test:func -- -u
 
-    # Uses GNU sed syntax
-    sed -i -e 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
-    
-    git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+    showFailures
+else
+    if [[ "$TRAVIS_PULL_REQUEST" = "false" ]] && [[ "$TRAVIS_BRANCH" = "develop" ]]; then
+        npm run test:func -- -u
 
-    git checkout develop
-    git add .
-    git commit -m "chore(snapshots): update snapshots [skip ci]"
-    git push
+        # Uses GNU sed syntax
+        sed -i -e 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
+
+        git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+
+        git checkout develop
+        git add .
+        git commit -m "chore(snapshots): update snapshots [skip ci]"
+        git push
+    else
+        npm run test:func:nosnaps 2> $ERROR_FILE | tee $OUTPUT_FILE
+
+        showFailures
+    fi
 fi
