104b49b67ccaf280b8c8c55ce880a0575caa8952
Nathan Schott <nschott@paypal.com>
2020-10-22 15:43:18 -0400

    Revert removing old utils

--- src/utils/elements.js
+++ src/utils/elements.js
@@ -77,6 +77,105 @@ export const getElement = curry(
 );
 
 /**
+ * Create a new style element and prepend it to the container
+ * @param {HTMLElement} container Container element to prepend new style
+ * @param {String} text Text content of the style element
+ * @returns {void}
+ */
+export const prependStyle = curry((container, text) => {
+    const elem = document.createElement('style');
+    elem.textContent = text;
+    container.insertBefore(elem, container.firstChild);
+});
+
+/**
+ * Append text data into the container element
+ * @param {HTMLElement} container Container element to append text
+ * @param {any} obj Various inputs representing text to be appended
+ * @returns {void}
+ */
+export const appendText = curry((container, obj) => {
+    if (Array.isArray(obj)) {
+        obj.forEach(elem => container.appendChild(elem));
+    } else if (obj instanceof HTMLElement) {
+        container.appendChild(obj);
+    } else if (typeof obj === 'string') {
+        const span = document.createElement('span');
+        span.innerHTML = obj;
+        container.appendChild(span);
+    } else if (obj === false) {
+        container.parentNode.removeChild(container);
+    }
+});
+
+/**
+ * Append text data into the container element
+ * @param {HTMLElement} container Container element to append text
+ * @param {any} obj Various inputs representing text to be appended
+ * @returns {void}
+ */
+export const prependText = curry((container, obj) => {
+    if (Array.isArray(obj)) {
+        [...obj].reverse().forEach(elem => container.insertBefore(elem, container.firstChild));
+    } else if (obj instanceof HTMLElement) {
+        container.insertBefore(obj, container.firstChild);
+    } else if (typeof obj === 'string') {
+        const span = document.createElement('span');
+        span.innerHTML = obj;
+        container.insertBefore(span, container.firstChild);
+    } else if (obj === false) {
+        container.parentNode.removeChild(container);
+    }
+});
+
+/**
+ * Append a new image into the container element
+ * @param {HTMLElement} container Container element to append image
+ * @param {String} url Image src attribute
+ * @param {String} alt Image alt attribute
+ * @param {String} srcset Image srcset attribute
+ * @returns {void}
+ */
+export const appendImage = curry((container, url, alt = 'PayPal Credit', srcset = '') => {
+    if (Array.isArray(url)) {
+        url.forEach(logo => appendImage(container, logo, alt));
+    } else if (typeof url === 'string') {
+        const logo = new Image();
+        logo.alt = alt;
+        logo.className = 'message__logo';
+        logo.src = url;
+
+        if (srcset) {
+            logo.srcset = srcset;
+        }
+
+        container.appendChild(logo);
+    } else if (typeof url === 'object') {
+        const {
+            src,
+            dimensions: [width, height]
+        } = url;
+
+        const logo = new Image();
+        logo.src = src;
+        logo.alt = alt;
+
+        const svgWrapper = document.createElement('div');
+        svgWrapper.className = 'message__logo message__logo--svg';
+
+        const canvas = document.createElement('canvas');
+        canvas.height = height;
+        canvas.width = width;
+
+        svgWrapper.appendChild(logo);
+        svgWrapper.appendChild(canvas);
+        container.appendChild(svgWrapper);
+    } else {
+        container.parentNode.removeChild(container);
+    }
+}, 2); // Need to manually set curry arity because of default parameters and transpiling
+
+/**
  * Ensure element is ready to be worked with. Necessary since
  * creating an iframe is asynchronous
  * @param {HTMLElement} element HTML Element
