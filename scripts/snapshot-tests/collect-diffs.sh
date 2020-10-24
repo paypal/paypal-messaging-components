cc143963cf269f7206571574e5406940e6602cf6
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-10-24 19:38:17 -0400

    Fix missing file

--- scripts/snapshot-tests/collect-diffs.sh
+++ scripts/snapshot-tests/collect-diffs.sh
@@ -8,16 +8,20 @@ set -ev
 DIFF_FOLDERS_LIST=diff_folders.log
 DIFF_FILES_LIST=diff_files.log
 
+touch DIFF_FOLDERS_LIST
+
 echo ''
 rm -rf ./tests/functional/__diff_output__
-find ./tests/functional/snapshots -type d | grep __diff_output__ || [[ $? == 1 ]]
+find ./tests/functional/snapshots -type d | grep __diff_output__ || [[ $? == 1 ]] > $DIFF_FOLDERS_LIST
 diffFolderCount=$(wc -l < $DIFF_FOLDERS_LIST || [[ $? == 1 ]] )
 echo "DIFF FOLDERS FOUND ($diffFolderCount found)"
 cat $DIFF_FOLDERS_LIST
 echo ''
 
+touch DIFF_FILES_LIST
+
 echo ''
-find ./tests/functional/ -type f -name '*png' | grep -h __diff_output__ > $DIFF_FILES_LIST || [[ $? == 1 ]]
+find ./tests/functional/ -type f -name '*png' | grep -h __diff_output__ || [[ $? == 1 ]] > $DIFF_FILES_LIST
 diffFileCount=$(wc -l < $DIFF_FILES_LIST || [[] $? == 1 ]] )
 echo "DIFF FILES FOUND ($diffFileCount found)"
 cat $DIFF_FILES_LIST
