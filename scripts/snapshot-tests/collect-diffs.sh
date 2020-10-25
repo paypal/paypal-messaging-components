e2ad837028652b0eeeb946060fbfa54cefd8e601
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-10-25 10:44:54 -0400

    Fix script

--- scripts/snapshot-tests/collect-diffs.sh
+++ scripts/snapshot-tests/collect-diffs.sh
@@ -8,27 +8,27 @@ set -ev
 DIFF_FOLDERS_LIST=diff_folders.log
 DIFF_FILES_LIST=diff_files.log
 
+touch $DIFF_FOLDERS_LIST
+touch $DIFF_FILES_LIST
+
 echo ''
 rm -rf ./tests/functional/__diff_output__
-find ./tests/functional/snapshots -type d | grep __diff_output__ || [[ $? == 1 ]] > $DIFF_FOLDERS_LIST
-diffFolderCount=$(wc -l < $DIFF_FOLDERS_LIST || [[ $? == 1 ]] )
+find ./tests/functional/snapshots -type d | (grep __diff_output__ || [[ $? == 1 ]]) > $DIFF_FOLDERS_LIST
+diffFolderCount=$(wc -l < $DIFF_FOLDERS_LIST)
 echo "DIFF FOLDERS FOUND ($diffFolderCount found)"
 cat $DIFF_FOLDERS_LIST
 echo ''
 
 echo ''
-find ./tests/functional/ -type f -name '*png' | grep -h __diff_output__ || [[ $? == 1 ]] > $DIFF_FILES_LIST
-diffFileCount=$(wc -l < $DIFF_FILES_LIST || [[] $? == 1 ]] )
+find ./tests/functional/ -type f -name '*png' | (grep -h __diff_output__ || [[ $? == 1 ]]) > $DIFF_FILES_LIST
+diffFileCount=$(wc -l < $DIFF_FILES_LIST)
 echo "DIFF FILES FOUND ($diffFileCount found)"
 cat $DIFF_FILES_LIST
 echo ''
 
-node ./tests/functional/utils/collectDiffs.js
+# node ./tests/functional/utils/collectDiffs.js
 
 if [[ $diffFileCount -gt 0 ]]; then
+    echo 'Diffs found, exiting with failure'
     exit 1
 fi
-
-# TODO
-## spec/US/custom-and-legacy all legacy times out at waitForBanner
-## # https://travis-ci.org/github/paypal/paypal-messaging-components/jobs/738571266#L467
