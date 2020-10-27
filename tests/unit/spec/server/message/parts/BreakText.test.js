b9ddbfd0146cf0714d040400e4c11e0f54f25982
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-10-27 15:31:30 -0400

    <BreakText /> tests

--- tests/unit/spec/server/message/parts/BreakText.test.js
+++ tests/unit/spec/server/message/parts/BreakText.test.js
@@ -1,4 +1,28 @@
+/** @jsx h */
+import { h } from 'preact';
+import { render } from '@testing-library/preact';
+
+import BreakText from 'server/message/parts/BreakText';
+
 describe('<BreakText />', () => {
-    test.todo('handles breaks in the middle of text');
-    test.todo('does not nest if break is at end of text');
+    const textParts = [['No Interest if paid in full in 6 months'], ['on purchases of $99+.', 'weak']];
+
+    test('handles breaks in the middle of text', () => {
+        const { getByText } = render(<BreakText textParts={textParts} options={{ tag: 'small', br: ['paid'] }} />);
+
+        expect(getByText('No Interest if paid')).toBeInTheDocument();
+        expect(getByText('No Interest if paid')).toHaveClass('br');
+        expect(getByText('in full in 6 months')).toBeInTheDocument();
+        expect(getByText('in full in 6 months')).toHaveClass('br');
+        expect(getByText('on purchases of $99+.')).toBeInTheDocument();
+        expect(getByText('on purchases of $99+.')).toHaveClass('weak');
+    });
+
+    test('does not nest if break is at end of text', () => {
+        const { getByText } = render(<BreakText textParts={textParts} options={{ tag: 'small', br: ['months'] }} />);
+
+        expect(getByText('No Interest if paid in full in 6 months')).toBeInTheDocument();
+        expect(getByText('No Interest if paid in full in 6 months')).toHaveClass('br');
+        expect(getByText('No Interest if paid in full in 6 months').parentElement).not.toHaveClass('br');
+    });
 });
