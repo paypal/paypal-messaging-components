4d93ff05fcd342a05dce7c9989eacdff9332dcaa
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-11-17 17:06:56 -0500

    Add env condition for logs

--- tests/functional/spec/utils/logScreenshot.js
+++ tests/functional/spec/utils/logScreenshot.js
@@ -1,6 +1,6 @@
+// to enable, set TEST_DEBUG in the CI or local environment
 const logScreenshot = ({ name, viewport }) => {
-    // eslint-disable-next-line no-constant-condition
-    if (false) {
+    if (process.env.TEST_DEBUG) {
         // eslint-disable-next-line no-console
         console.log(`Taking screenshot of [${name}] with dimensions ${JSON.stringify(viewport)}`);
     }
--- tests/functional/spec/utils/logTestName.js
+++ tests/functional/spec/utils/logTestName.js
@@ -1,10 +1,8 @@
-// const isComparingSnapshots = process.env.DIRTY_SNAPSHOTS == 0; // eslint-disable-line eqeqeq
-
-// Outputs current test so CI does not stall
+// to enable, set TEST_DEBUG in the CI or local environment
 const logTestName = ({ account, viewport, groupString, testNameParts, testName }) => {
     const name = testName || `${account} ${groupString} ${testNameParts}`;
-    // eslint-disable-next-line no-constant-condition
-    if (false) {
+
+    if (process.env.TEST_DEBUG) {
         // eslint-disable-next-line no-console
         console.log(`Running test [${name}] with viewport [${JSON.stringify(viewport)}]`);
     }
