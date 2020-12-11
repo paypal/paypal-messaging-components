6aa1c274a830311500087b57680acb48a55c3bc2
Julia Furman <jfurman@paypal.com>
2020-12-11 10:13:07 -0500

    Remove extraneous commented out code

--- server/locale/FR/styles/text/index.js
+++ server/locale/FR/styles/text/index.js
@@ -1,19 +1,14 @@
-// import base from './base.css';
 import logoPrimary from './logo--primary.css';
 import logoAlternative from './logo--alternative.css';
 import logoTop from './logo--top.css';
-// import logoRight from './logo--right.css';
 import logoInline from './logo--inline.css';
 import logoNone from './logo--none.css';
-// import textWhite from './text--white.css';
 import textMonochrome from './text--monochrome.css';
 import textGrayscale from './text--grayscale.css';
 
 export default [
-    // ['default', base],
     ['logo.type:primary', logoPrimary],
     ['logo.position:top', logoTop],
-    // ['logo.position:right', logoRight],
 
     ['logo.type:alternative', logoAlternative],
 
@@ -21,7 +16,6 @@ export default [
 
     ['logo.type:none', logoNone],
 
-    // ['text.color:white', textWhite],
     ['text.color:monochrome', textMonochrome],
     ['text.color:grayscale', textGrayscale]
 ];
