96b66e31cc3a9e665ed108e0376e284f48bcaa63
Nathan Schott <nschott@paypal.com>
2020-12-18 14:57:40 -0500

    Move message and modal content to its own package

--- utils/devServerProxy/proxy.js
+++ utils/devServerProxy/proxy.js
@@ -55,8 +55,8 @@ export default (app, server, compiler) => {
 
             const banner =
                 preferredCreditType !== 'NI'
-                    ? fs.readFileSync(`content/banners/${country}/${offer}.json`, 'utf-8')
-                    : fs.readFileSync(`content/banners/US/ni.json`, 'utf-8');
+                    ? fs.readFileSync(`packages/content/banners/${country}/${offer}.json`, 'utf-8')
+                    : fs.readFileSync(`packages/content/banners/US/ni.json`, 'utf-8');
 
             return JSON.parse(populateTemplate(morsVars, banner));
         }
@@ -181,7 +181,7 @@ export default (app, server, compiler) => {
         const [country, productNames] = devAccountMap[account] ?? ['US', ['ni']];
 
         const productsJSON = productNames.map(product =>
-            fs.readFileSync(`content/modals/${country}/${product}.json`, 'utf-8').toString()
+            fs.readFileSync(`packages/content/modals/${country}/${product}.json`, 'utf-8').toString()
         );
 
         const terms = getTerms(country, Number(amount));
