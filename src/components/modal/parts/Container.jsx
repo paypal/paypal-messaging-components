1c79ee7e7947ddaa9588995e8c575cfd90f7b084
Ryan Gilbert <gilbertstheman+github@gmail.com>
2021-01-05 15:58:36 -0500

    refactor: forward tracking information  (#396)

--- src/components/modal/parts/Container.jsx
+++ src/components/modal/parts/Container.jsx
@@ -7,7 +7,14 @@ import { useTransitionState, ScrollProvider, useServerData, useXProps, useDidUpd
 import Overlay from './Overlay';
 
 const Container = ({ children, contentWrapper, contentMaxWidth, contentMaxHeight }) => {
-    const { type, products, setServerData } = useServerData();
+    const {
+        type,
+        products,
+        setServerData,
+        meta: {
+            trackingDetails: { payload: trackingPayload }
+        }
+    } = useServerData();
     const { onReady, currency, amount, payerId, clientId, merchantId, buyerCountry } = useXProps();
     const [transitionState] = useTransitionState();
     const [loading, setLoading] = useState(false);
@@ -23,7 +30,7 @@ const Container = ({ children, contentWrapper, contentMaxWidth, contentMaxHeight
 
     useEffect(() => {
         if (typeof onReady === 'function') {
-            onReady({ type, products: products.map(({ meta }) => meta.product) });
+            onReady({ type, products: products.map(({ meta }) => meta.product), trackingPayload });
         }
     }, []);
 
