10f462a62ad4664a1766c77c3af51803ab6d5c76
Julia Furman <jfurman@paypal.com>
2021-01-27 18:41:44 -0500

    GPLNQ purchase range message

---
+++ server/locale/US/GPL/mutations/gplnq_range.js
@@ -0,0 +1,163 @@
+import Logo from '../../../../message/logos';
+import {
+    textWrap,
+    messageLogoWidth,
+    altNoWrap,
+    setLogoTop,
+    xSmallFallback,
+    logo20x1
+} from '../../../../message/mediaQueries';
+import { flexLogoMutations, textLogoMutations } from '../../../../message/logoMutations';
+
+const flex = [
+    [
+        'default',
+        {
+            logo: Logo.PP_PAYPAL.WHITE,
+            headline: [
+                {
+                    tag: 'xsmall'
+                },
+                {
+                    tag: 'medium'
+                }
+            ],
+            disclaimer: ['default'],
+            styles: [
+                '.message__headline .tag--medium > span:nth-child(2):after { content: "."; }',
+                '.message__headline .tag--medium .weak { display: none; }'
+            ]
+        }
+    ],
+    [
+        'ratio:20x1',
+        {
+            styles: [
+                logo20x1(),
+                `.message__headline .tag--medium.multi .weak {
+                    display: inline-block;
+                    transform: translateX(-100%);
+                    font-weight: 500;
+                    margin: 0px;
+                }
+                `
+            ]
+        }
+    ],
+    [
+        'ratio:8x1',
+        {
+            headline: [
+                {
+                    tag: 'xsmall'
+                },
+                {
+                    tag: 'medium',
+                    br: ['payments']
+                }
+            ],
+            styles: [
+                '.message__headline .tag--medium > span:nth-child(2):after { content: "."; }',
+                '.message__headline .tag--medium .weak { display: none; }',
+                '@media (min-aspect-ratio: 80/11) { .message__disclaimer { margin-left: 0;} }'
+            ]
+        }
+    ],
+    ['color:white-no-border', { logo: Logo.PP_PAYPAL.COLOR }],
+    ...flexLogoMutations
+];
+
+export default {
+    'layout:flex': flex,
+    'layout:text': [
+        [
+            'default',
+            ({ textSize }) => ({
+                styles: [
+                    textWrap(textSize * 45, textSize, 'US'),
+                    xSmallFallback(textSize * 14),
+                    messageLogoWidth(false, textSize * 4, textSize * 1.25),
+                    setLogoTop(textSize * 20)
+                ],
+                logo: Logo.PP_PAYPAL.COLOR,
+                headline: [
+                    {
+                        tag: 'medium',
+                        br: ['of']
+                    },
+                    { tag: 'xsmall' }
+                ],
+                disclaimer: ['default']
+            })
+        ],
+        [
+            'logo.type:primary && logo.position:right',
+            ({ textSize }) => ({
+                styles: [
+                    xSmallFallback(textSize * 16),
+                    setLogoTop(textSize * 40),
+                    messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25)
+                ]
+            })
+        ],
+        [
+            'logo.type:primary && logo.position:top',
+            ({ textSize }) => ({
+                styles: [
+                    xSmallFallback(textSize * 14.75 + 10),
+                    messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25)
+                ]
+            })
+        ],
+        [
+            'logo.type:alternative',
+            ({ textSize }) => ({
+                styles: [
+                    `@media screen and (max-width: ${textSize * 10.6}px) { .message__content { white-space: nowrap; }}`,
+                    textWrap(textSize * 37, textSize, 'US'),
+                    xSmallFallback(textSize * 15.4),
+                    altNoWrap(textSize * 10.6),
+                    messageLogoWidth(textSize * 1.75, textSize * 4, textSize * 1.25)
+                ],
+                logo: Logo.PP_PAYPAL.COLOR[0]
+            })
+        ],
+        [
+            'logo.type:none',
+            ({ textSize }) => ({
+                styles: [xSmallFallback(textSize * 18)],
+                logo: false,
+                headline: [
+                    {
+                        tag: 'medium',
+                        br: ['on'],
+                        replace: [['purchases.', 'purchases']]
+                    },
+                    {
+                        tag: 'xsmall.2',
+                        replace: [['later.', 'later']]
+                    }
+                ]
+            })
+        ],
+        [
+            'logo.type:inline',
+            ({ textSize }) => ({
+                styles: [xSmallFallback(textSize * 18), `.message__logo { width: ${textSize * 4}px }`],
+                logo: Logo.NO_PP_MONOGRAM.COLOR,
+                headline: [
+                    {
+                        tag: 'medium',
+                        br: ['on'],
+                        replace: [['purchases.', 'purchases']]
+                    },
+                    {
+                        tag: 'xsmall.2',
+                        replace: [['later.', 'later']]
+                    }
+                ]
+            })
+        ],
+        ...textLogoMutations
+    ]
+};
