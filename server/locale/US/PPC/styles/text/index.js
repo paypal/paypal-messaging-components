a449bdac15517c60a2a62bd2e4f86b3c73ebf467
Julia Furman <jfurman@paypal.com>
2021-02-09 08:47:38 -0500

    Fix primary PPC logo alignment and wrapping for position right

--- server/locale/US/PPC/styles/text/index.js
+++ server/locale/US/PPC/styles/text/index.js
@@ -2,10 +2,12 @@ import base from './base.css';
 import textMonochrome from './text--monochrome.css';
 import textGrayscale from './text--grayscale.css';
 import logoInline from './logo--inline.css';
+import logoPrimaryPostionRight from './logo--primary-position--right.css';
 
 export default [
     ['default', base],
     ['text.color:monochrome', textMonochrome],
     ['text.color:grayscale', textGrayscale],
-    ['logo.type:inline', logoInline]
+    ['logo.type:inline', logoInline],
+    ['logo.type:primary && logo.position:right', logoPrimaryPostionRight]
 ];
