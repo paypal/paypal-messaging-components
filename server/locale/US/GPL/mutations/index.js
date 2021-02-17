46058b59d5b756833d84202d7062118e8476be52
Julia F <mpscqw@gmail.com>
2021-02-17 10:53:15 -0500

    feat: add GPLNQ purchase range message (#434)

--- server/locale/US/GPL/mutations/index.js
+++ server/locale/US/GPL/mutations/index.js
@@ -3,6 +3,7 @@
 /* eslint-disable camelcase */
 import gpl_no_amount from './gpl';
 import gpl_non_qual from './gplnq';
+import gplnq_range from './gplnq_range';
 import gplq from './gplq';
 
 export default function getMutations(id, type) {
@@ -11,6 +12,8 @@ export default function getMutations(id, type) {
             return gplq[type];
         case 'GPLNQ':
             return gpl_non_qual[type];
+        case 'GPLNQ_RANGE':
+            return gplnq_range[type];
         case 'GPL':
         default:
             return gpl_no_amount[type];
