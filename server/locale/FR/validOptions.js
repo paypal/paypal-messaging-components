d81abb4f03ec8b5b7c93ad965dc178c63f8b2954
jadutter <4691511+jadutter@users.noreply.github.com>
2021-03-31 16:34:08 -0400

    added text align as a valid option for FR messaging, and added align parameters to functional tests

--- server/locale/FR/validOptions.js
+++ server/locale/FR/validOptions.js
@@ -9,6 +9,7 @@ export default {
         text: {
             color: [Types.STRING, ['black', 'white', 'monochrome', 'grayscale|greyscale']],
             size: [Types.NUMBER, [12, 10, 11, 13, 14, 15, 16]],
+            align: [Types.STRING, ['left', 'right', 'center']],
             fontFamily: [Types.STRING]
         },
         preset: [Types.STRING, [undefined, 'smallest']]
