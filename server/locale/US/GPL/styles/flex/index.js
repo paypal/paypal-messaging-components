0485cefbbd36042b1df102b55e2fdfa3f8f7145d
Rene Osman <rosman@paypal.com>
2021-04-27 12:19:18 -0400

    updated US GPL colors

--- server/locale/US/GPL/styles/flex/index.js
+++ server/locale/US/GPL/styles/flex/index.js
@@ -2,6 +2,7 @@ import base from './base.css';
 import colorBlack from './color--black.css';
 import colorGray from './color--gray.css';
 import colorWhite from './color--white.css';
+import colorBlue from './color--blue.css';
 import colorWhiteNoBorder from './color--white-no-border.css';
 import colorMonochrome from './color--monochrome.css';
 import colorGrayscale from './color--grayscale.css';
@@ -12,7 +13,7 @@ import ratio8x1 from './ratio--8x1.css';
 import ratio20x1 from './ratio--20x1.css';
 
 export default [
-    ['default', base],
+    ['default', [base, colorBlue].join('\n')],
     ['ratio:1x1', ratio1x1],
     ['ratio:1x4', ratio1x4],
     ['ratio:8x1', [ratio6x1, ratio8x1].join('\n')],
