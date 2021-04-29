12b4b92a81c7ce878bee67fff9a76cf6c295b9a3
Rene Osman <rosman@paypal.com>
2021-04-29 11:53:46 -0400

    updated default css to non gradient color. updated de to still show gradient

--- server/locale/DE/styles/flex/index.js
+++ server/locale/DE/styles/flex/index.js
@@ -1,4 +1,5 @@
 import base from './base.css';
+import colorBlue from './color--blue.css';
 import ratio1x1 from './ratio--1x1.css';
 import ratio1x4 from './ratio--1x4.css';
 import ratio6x1 from './ratio--6x1.css';
@@ -6,7 +7,7 @@ import ratio8x1 from './ratio--8x1.css';
 import ratio20x1 from './ratio--20x1.css';
 
 export default [
-    ['default', base],
+    ['default', [base, colorBlue].join('\n')],
     ['ratio:1x1', ratio1x1],
     ['ratio:1x4', ratio1x4],
     ['ratio:8x1', [ratio6x1, ratio8x1].join('\n')],
