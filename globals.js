534cc582fce9a213c63dc94bf9fac68f76d3fee0
Nathan Schott <nschott@paypal.com>
2021-05-07 16:03:26 -0400

    build: stage bundle support

--- globals.js
+++ globals.js
@@ -24,28 +24,15 @@ module.exports = (env = { TARGET: 'sdk' }) => ({
         __VERSION__: env.VERSION || version,
         __DEMO__: !!env.demo,
         __TARGET__: env.TARGET.toUpperCase(),
+        __STAGE_TAG__: env.STAGE_TAG,
+        __TEST_ENV__: env.TEST_ENV,
         __DOMAIN__: {
             __LOCAL__: `https://localhost.paypal.com:${PORT}`,
             __STAGE__: 'https://www.msmaster.qa.paypal.com',
             __SANDBOX__: 'https://www.sandbox.paypal.com',
-            __PRODUCTION__: 'https://www.paypal.com',
-
-            // Manual endpoint override example:
-            // __MODAL__: {
-            //     __LOCAL__: 'https://localhost.paypal.com:8443'
-            // },
-            // __LOGGER_A__: {
-            //     __LOCAL__: 'https://www.msmaster.qa.paypal.com'
-            // },
-            __RAMP_EXPERIMENT__: {
-                __LOCAL__: 'https://www.paypalobjects.com',
-                __STAGE__: 'https://www.paypalobjects.com',
-                __SANDBOX__: 'https://www.paypalobjects.com',
-                __PRODUCTION__: 'https://www.paypalobjects.com'
-            }
+            __PRODUCTION__: 'https://www.paypal.com'
         },
         __URI__: {
-            __RAMP_EXPERIMENT__: '/upstream/assets/messaging/modal/ramp-experiment-ssr.json',
             __MESSAGE_A__: '/credit-presentment/messages',
             __MESSAGE_B__: '/credit-presentment/smart/message',
             __MODAL__: '/credit-presentment/smart/modal',
