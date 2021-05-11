4d0d80293ab8f3c534b5489a2205cdc4a01e1639
yeul <mpscqw@gmail.com>
2021-05-11 15:48:38 -0400

    move promise back to overflow observer

--- src/utils/observers.js
+++ src/utils/observers.js
@@ -67,15 +67,6 @@ const getIntersectionObserverPolyfill = () => {
         typeof window.IntersectionObserver === 'undefined'
             ? dynamicImport('https://polyfill.io/v3/polyfill.js?features=IntersectionObserver')
             : undefined
-    ).then(() =>
-        ZalgoPromise.all([
-            // Ensure window has loaded otherwise root can be miscalculated and default to null (viewport)
-            // and cause messages below the fold to hide
-            // e.g. https://www.escortradar.com/products/escort_redline_360c
-            awaitWindowLoad,
-            // Ensure a message has rendered so that the [data-pp-id] querySelector below is not null
-            awaitFirstRender
-        ])
     );
 };
 
@@ -105,107 +96,118 @@ export const getViewportIntersectionObserver = createGlobalVariableGetter('__vie
 );
 
 export const getOverflowObserver = createGlobalVariableGetter('__intersection_observer__', () =>
-    getIntersectionObserverPolyfill().then(() => {
-        const firstContainer = getGlobalState()
-            .messagesMap.keys()
-            .next().value;
-        // A single page app could cause an issue here if the root element is
-        // determined to be inside the main single page app code
-        const root = getRoot(firstContainer);
-        // eslint-disable-next-line compat/compat
-        return new IntersectionObserver(
-            (entries, observer) => {
-                const { messagesMap } = getGlobalState();
+    getIntersectionObserverPolyfill()
+        .then(() =>
+            ZalgoPromise.all([
+                // Ensure window has loaded otherwise root can be miscalculated and default to null (viewport)
+                // and cause messages below the fold to hide
+                // e.g. https://www.escortradar.com/products/escort_redline_360c
+                awaitWindowLoad,
+                // Ensure a message has rendered so that the [data-pp-id] querySelector below is not null
+                awaitFirstRender
+            ])
+        )
+        .then(() => {
+            const firstContainer = getGlobalState()
+                .messagesMap.keys()
+                .next().value;
+            // A single page app could cause an issue here if the root element is
+            // determined to be inside the main single page app code
+            const root = getRoot(firstContainer);
+            // eslint-disable-next-line compat/compat
+            return new IntersectionObserver(
+                (entries, observer) => {
+                    const { messagesMap } = getGlobalState();
 
-                entries.forEach(entry => {
-                    const iframe = entry.target;
-                    const container = iframe.parentNode.parentNode;
-                    // If the library has been cleaned up by an SDK destroy, the container
-                    // may not exist in the current SDK script messageMap. In this scenario
-                    // we will short circuit on the state.render check
-                    const { state } = messagesMap.get(container) || {};
+                    entries.forEach(entry => {
+                        const iframe = entry.target;
+                        const container = iframe.parentNode.parentNode;
+                        // If the library has been cleaned up by an SDK destroy, the container
+                        // may not exist in the current SDK script messageMap. In this scenario
+                        // we will short circuit on the state.render check
+                        const { state } = messagesMap.get(container) || {};
 
-                    // Only run in the context of a render call, and NOT when resizing the
-                    // element for other reasons such as window resizing
-                    if (!state?.renderStart) {
-                        observer.unobserve(iframe);
-                        return;
-                    }
-
-                    const index = container.getAttribute('data-pp-id');
-                    const minWidth = Number(iframe.getAttribute('data-width'));
-                    const minHeight = Number(iframe.getAttribute('data-height'));
-                    const duration = getCurrentTime() - state.renderStart;
-
-                    let isIntersectingFallback;
-                    // TODO: Further investigate why in some edge-cases the entry values are all 0.
-                    // If this is the case we run our own calculations as a fallback.
-                    // e.g. https://www.auto-protect-shop.de/nanolex-ultra-glasversiegelung-set/a-528
-                    if (entry.rootBounds?.width === 0 && entry.rootBounds.height === 0) {
-                        isIntersectingFallback = elementContains(root, iframe);
-                    }
-
-                    /**
-                     * If the message is intersecting/partially obscured AND the message isn't offscreen,
-                     * or the message is too small, run overflow detection to hide the message.
-                     *
-                     * Else, ensure the message is visible.
-                     */
-                    if (
-                        ((entry.intersectionRatio < 0.9 && elementIsOffscreen(iframe) === false) ||
-                            // Round up for decimal values
-                            Math.ceil(iframe.getBoundingClientRect().width) < minWidth) &&
-                        !isIntersectingFallback
-                    ) {
-                        // Attempt to fallback and failing that, hide the message.
-                        if (container.getAttribute('data-pp-style-preset') === 'smallest') {
-                            logger.warn(state.renderComplete ? 'update_hidden' : 'hidden', {
-                                description: `PayPal Message has been hidden. Fallback message must be visible and requires minimum dimensions of ${minWidth}px x ${minHeight}px. Current container is ${entry.intersectionRect.width}px x ${entry.intersectionRect.height}px.`,
-                                container,
-                                index,
-                                duration
-                            });
-                            logger.track({
-                                index,
-                                et: 'CLIENT_IMPRESSION',
-                                event_type: 'message_hidden'
-                            });
-                            state.renderComplete = true;
-                            delete state.renderStart;
-                        } else {
-                            container.setAttribute('data-pp-style-preset', 'smallest');
-                            logger.warn(state.renderComplete ? 'update_overflow' : 'overflow', {
-                                description: `PayPal Message attempting fallback. Message must be visible and requires minimum dimensions of ${minWidth}px x ${minHeight}px. Current container is ${entry.intersectionRect.width}px x ${entry.intersectionRect.height}px.`,
-                                container,
-                                index,
-                                duration
-                            });
+                        // Only run in the context of a render call, and NOT when resizing the
+                        // element for other reasons such as window resizing
+                        if (!state?.renderStart) {
+                            observer.unobserve(iframe);
+                            return;
                         }
 
-                        iframe.style.setProperty('opacity', '0', 'important');
-                        iframe.style.setProperty('pointer-events', 'none', 'important');
+                        const index = container.getAttribute('data-pp-id');
+                        const minWidth = Number(iframe.getAttribute('data-width'));
+                        const minHeight = Number(iframe.getAttribute('data-height'));
+                        const duration = getCurrentTime() - state.renderStart;
 
-                        observer.unobserve(iframe);
-                    } else {
-                        // Reset if previously hidden
-                        iframe.style.setProperty('opacity', 1);
-                        iframe.style.setProperty('pointer-events', null);
+                        let isIntersectingFallback;
+                        // TODO: Further investigate why in some edge-cases the entry values are all 0.
+                        // If this is the case we run our own calculations as a fallback.
+                        // e.g. https://www.auto-protect-shop.de/nanolex-ultra-glasversiegelung-set/a-528
+                        if (entry.rootBounds?.width === 0 && entry.rootBounds.height === 0) {
+                            isIntersectingFallback = elementContains(root, iframe);
+                        }
 
-                        logger.info(state.renderComplete ? 'update_visible' : 'visible', {
-                            index,
-                            duration
-                        });
+                        /**
+                         * If the message is intersecting/partially obscured AND the message isn't offscreen,
+                         * or the message is too small, run overflow detection to hide the message.
+                         *
+                         * Else, ensure the message is visible.
+                         */
+                        if (
+                            ((entry.intersectionRatio < 0.9 && elementIsOffscreen(iframe) === false) ||
+                                // Round up for decimal values
+                                Math.ceil(iframe.getBoundingClientRect().width) < minWidth) &&
+                            !isIntersectingFallback
+                        ) {
+                            // Attempt to fallback and failing that, hide the message.
+                            if (container.getAttribute('data-pp-style-preset') === 'smallest') {
+                                logger.warn(state.renderComplete ? 'update_hidden' : 'hidden', {
+                                    description: `PayPal Message has been hidden. Fallback message must be visible and requires minimum dimensions of ${minWidth}px x ${minHeight}px. Current container is ${entry.intersectionRect.width}px x ${entry.intersectionRect.height}px.`,
+                                    container,
+                                    index,
+                                    duration
+                                });
+                                logger.track({
+                                    index,
+                                    et: 'CLIENT_IMPRESSION',
+                                    event_type: 'message_hidden'
+                                });
+                                state.renderComplete = true;
+                                delete state.renderStart;
+                            } else {
+                                container.setAttribute('data-pp-style-preset', 'smallest');
+                                logger.warn(state.renderComplete ? 'update_overflow' : 'overflow', {
+                                    description: `PayPal Message attempting fallback. Message must be visible and requires minimum dimensions of ${minWidth}px x ${minHeight}px. Current container is ${entry.intersectionRect.width}px x ${entry.intersectionRect.height}px.`,
+                                    container,
+                                    index,
+                                    duration
+                                });
+                            }
 
-                        state.renderComplete = true;
-                        delete state.renderStart;
+                            iframe.style.setProperty('opacity', '0', 'important');
+                            iframe.style.setProperty('pointer-events', 'none', 'important');
 
-                        observer.unobserve(iframe);
-                    }
-                });
-            },
-            {
-                root
-            }
-        );
-    })
+                            observer.unobserve(iframe);
+                        } else {
+                            // Reset if previously hidden
+                            iframe.style.setProperty('opacity', 1);
+                            iframe.style.setProperty('pointer-events', null);
+
+                            logger.info(state.renderComplete ? 'update_visible' : 'visible', {
+                                index,
+                                duration
+                            });
+
+                            state.renderComplete = true;
+                            delete state.renderStart;
+
+                            observer.unobserve(iframe);
+                        }
+                    });
+                },
+                {
+                    root
+                }
+            );
+        })
 );
