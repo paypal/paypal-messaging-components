66276f6ad23098bfd135a2fd57b70d5022b63060
jadutter <4691511+jadutter@users.noreply.github.com>
2021-06-01 16:38:19 -0400

    initial commit for changing prerender iframe to reserve space for the message iframe

--- src/zoid/message/containerTemplate.jsx
+++ src/zoid/message/containerTemplate.jsx
@@ -3,12 +3,85 @@ import { node, dom } from 'jsx-pragmatic/src';
 import { EVENT } from 'zoid/src';
 
 import { getOverflowObserver, createTitleGenerator } from '../../utils';
+import prerenderTemplate from './prerenderTemplate';
 
 const getTitle = createTitleGenerator();
 
+import getParentStyles from '../../../server/getParentStyles.js';
+
+const getBaseStyles = ({ uid, style: { layout, text: textOptions, ratio: ratioOption = '1x4' } }) => {
+    let cssStyles = ``;
+    const ratioMap = {
+        '1x1': `
+            max-height: 300px;
+            min-height: 120px;
+            max-width: 300px;
+            min-width: 120px;
+        `,
+        '1x4': `
+            max-height: 1920px;
+            min-height: 120px;
+            max-width: 160px;
+            min-width: 160px;
+        `,
+        '8x1': `
+            max-width: 768px;
+            min-width: 250px;
+        `,
+        '20x1': `
+            max-width: 1169px;
+            min-width: 350px;
+        `
+    };
+    if (layout !== 'flex') {
+        const { size: textSize = 12 } = textOptions ?? {};
+        cssStyles = `
+        min-height: ${textSize * 3}px;
+        max-height: ${textSize * 6}px;
+        min-width: 100px;
+        `;
+    } else {
+        cssStyles = ratioMap[ratioOption];
+    }
+
+    return `
+        #${uid} {
+            display: block;
+            width: 100%;
+            height: 100%;
+            position: relative;
+            box-sizing: border-box;
+            
+        }
+        #${uid} iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }
+        #${uid} > iframe:nth-of-type(1) {
+            z-index: 99;
+            height: 100%;
+            ${cssStyles}
+        }
+        #${uid} > iframe:nth-of-type(2) {
+            z-index: 98;
+            height: 100%;
+            min-height: 10px;
+        }
+    `;
+};
+
 export default ({ uid, frame, prerenderFrame, doc, event, props, container }) => {
+    const removePrerender = getRemovePrerender(prerenderFrame);
+
+    prerenderFrame.srcdoc = prerenderTemplate({ doc, props });
+
     event.on(EVENT.RENDERED, () => {
         prerenderFrame.parentNode.removeChild(prerenderFrame);
+        // setTimeout(() => {
+        //     prerenderFrame.parentNode.removeChild(prerenderFrame);
+        // }, 3000);
     });
 
     const setupAutoResize = el => {
@@ -24,13 +97,11 @@ export default ({ uid, frame, prerenderFrame, doc, event, props, container }) =>
                     // Attributes used by the overflow observer
                     el.setAttribute('data-width', width);
                     el.setAttribute('data-height', height);
-
                     if (typeof height === 'number') {
                         // Auto resize height for non-layout flex messages
                         el.style.setProperty('height', `${height}px`);
                     }
                 }
-
                 if (el.__hasResizedBefore__) {
                     // The styles event will fire first before the resize event for the initial render
                     event.once('styles', () => {
@@ -48,17 +119,9 @@ export default ({ uid, frame, prerenderFrame, doc, event, props, container }) =>
             }
         });
     };
+    const baseStyles = getBaseStyles({ ...props, uid });
 
-    const baseStyles = `
-        #${uid} > iframe {
-            width: 100%;
-            height: 0;
-        }
-
-        #${uid} > iframe:nth-of-type(2) {
-            display: none;
-        }
-    `;
+    console.log('baseStyles', props?.style?.layout ?? 'text', baseStyles);
 
     event.on('styles', ({ styles }) => {
         if (typeof styles === 'string') {
@@ -66,7 +129,7 @@ export default ({ uid, frame, prerenderFrame, doc, event, props, container }) =>
 
             style.textContent = `
                 ${baseStyles}
-                ${styles.replace(/\.pp-flex[^\s:,.#]*/g, `#${uid}`)}
+                ${styles.replace(/\.pp-(text|flex)[^\s:,.#]*/g, `#${uid}`)}
             `;
         }
     });
@@ -74,8 +137,8 @@ export default ({ uid, frame, prerenderFrame, doc, event, props, container }) =>
     return (
         <span id={uid}>
             <style>{baseStyles}</style>
-            <node el={frame} title={messageTitle} onRender={setupAutoResize} />
             <node el={prerenderFrame} title={`Prerender ${messageTitle}`} />
+            <node el={frame} title={messageTitle} onRender={setupAutoResize} />
         </span>
     ).render(dom({ doc }));
 };
---
+++ src/zoid/message/prerenderTemplate.jsx
@@ -0,0 +1,24 @@
+/** @jsx node */
+import { node, dom } from 'jsx-pragmatic/src';
+
+export default ({ uid, frame, prerenderFrame, doc, event, props, container, jsxDom }) => {
+    const { style: layout } = props;
+    const styleContent = `
+    html {
+        width: 100%;
+        height: 100%;
+        color: #000;
+    }
+    `;
+    const createEl = ({ doc }) =>
+        (
+            <html>
+                <style>{styleContent}</style>
+                <body>
+                    <span>Loading...</span>
+                </body>
+            </html>
+        ).render(dom({ doc }));
+    const el = createEl({ doc });
+    return el.innerHTML;
+};
