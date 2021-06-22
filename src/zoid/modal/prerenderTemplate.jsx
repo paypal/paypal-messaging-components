454aef5cdba33c5342caf60c0c48c5ec54880895
Rene Osman <rosman@paypal.com>
2021-06-22 18:49:41 -0400

    fixed shifting of modal, updated border radius in prerender

--- src/zoid/modal/prerenderTemplate.jsx
+++ src/zoid/modal/prerenderTemplate.jsx
@@ -1,53 +1,74 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/style-prop-object */
+/* eslint-disable no-param-reassign */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/no-unknown-property */
 /** @jsx node */
 import { node, dom } from 'jsx-pragmatic/src';
 import { Spinner } from '@paypal/common-components';
+import { ZalgoPromise } from 'zalgo-promise/src';
 
 export default ({ uid, doc, props, state }) => {
     const styles = `
-      .overlay{
-        position: fixed;
-        left: 0;
-        top: 0;
-        width: 100%; 
-        height: 100%;
-      }
-      .modal-content {
-        position: relative;
-        background-color: #fefefe;
-        margin: auto;
-        border: 1px solid #888;
-        max-width: 638px;
-        height: 100%;
-        border-radius: 10px;
-        right: 10px;
-        box-shadow: 0 5px 15px 0 rgb(0 0 0 / 50%);
-        transition: transform 350ms ease, box-shadow 350ms ease;
-        transform: translateY(100%);
-      }
-      .show-modal{
-        transform: translateY(0);
-      }
-      .spinner{
-          position: relative !important;
-      }
-      .close-button > button {
-          background-image: url("data:image/svg+xml,%3Csvg width='36' height='36' viewBox='0 0 36 36' fill='transparent' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 0L0 12' transform='translate(12 12)' stroke='%232C2E2F' stroke-width='2' stroke-linecap='round'/%3E%3Cpath d='M0 0L12 12' transform='translate(12 12)' stroke='%232C2E2F' stroke-width='2' stroke-linecap='round' /%3E%3C/svg%3E");
-          width: 38px;
-        height: 38px;
-        display: block;
-        padding: 0;
-        border: none;
-        cursor: pointer;
-        position: absolute;
-        top: 12px;
-        right: 8px;
-        z-index: 50;
-      }
+        .overlay{
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 100%; 
+            height: 100%;
+        }
+        .modal-content {
+            position: relative;
+            background-color: #fefefe;
+            margin: auto;
+            border: 1px solid #888;
+            max-width: 640px;
+            height: 100%;
+            border-top-right-radius: 10px;
+            border-top-left-radius: 10px;
+            right: 9px;
+            box-shadow: 0 5px 15px 0 rgb(0 0 0 / 50%);
+            transition: transform 350ms ease, box-shadow 350ms ease;
+            transform: translateY(100%);
+        }
+        .show-modal{
+            transform: translateY(0);
+        }
+        .spinner{
+            position: relative !important;
+        }
+        .close-button > button {
+            background-image: url("data:image/svg+xml,%3Csvg width='36' height='36' viewBox='0 0 36 36' fill='transparent' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 0L0 12' transform='translate(12 12)' stroke='%232C2E2F' stroke-width='2' stroke-linecap='round'/%3E%3Cpath d='M0 0L12 12' transform='translate(12 12)' stroke='%232C2E2F' stroke-width='2' stroke-linecap='round' /%3E%3C/svg%3E");
+            width: 38px;
+            height: 38px;
+            display: block;
+            padding: 0;
+            border: none;
+            cursor: pointer;
+            position: absolute;
+            top: 12px;
+            right: 8px;
+            z-index: 50;
+        }
+
+        .error{
+            position: absolute;
+            top: 60%;
+            left: 35%;
+            font-size: 25px;
+        }
+
+        @media (max-width: 639px), (max-height: 539px){
+            .modal-content {
+                margin-top: 84px;
+                height: calc(100% - 84px);
+                scrollbar-width: none;
+                right: 1px;
+            }       
+        }
         
     `;
     const toggleShow = boolean => {
@@ -64,8 +85,27 @@ export default ({ uid, doc, props, state }) => {
         state.prerenderDetails.prerenderElement.classList.add(`${uid}-invisible`);
         state.prerenderDetails.prerenderElement.classList.remove(`${uid}-visible`);
         document.querySelector(`#${uid}`).style.display = 'none';
+        document.getElementById(`${uid}-top`).style.opacity = 0;
         toggleShow(false);
     };
+    const checkForErrors = () => {
+        ZalgoPromise.delay(10000).then(() => {
+            // get parent iframe with modal content
+            const parentFrame = state.prerenderDetails.frameElement.contentDocument;
+            // check to see if modal content class exists
+            if (!parentFrame.getElementsByClassName('modal-wrapper').length) {
+                // looks like there is an error if modal content class does not exist.
+                // assign variable to state and access in UI
+                state.error = 'Error loading Modal';
+                state.prerenderDetails.prerenderElement.contentDocument.getElementsByClassName(
+                    'error'
+                )[0].style.display = 'block';
+                state.prerenderDetails.prerenderElement.contentDocument.getElementsByClassName('error')[0].textContent =
+                    state.error;
+            }
+        });
+    };
+
     // no way to check if prerenderer is fully loaded so wait 100ms before attempting to see if the elements exist. This timeout only happens on first render
     setTimeout(() => {
         toggleShow(true);
@@ -74,16 +114,19 @@ export default ({ uid, doc, props, state }) => {
         <html lang="en">
             <head>
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta content="width=device-width, initial-scale=1" name="viewport" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </head>
             <style>{styles}</style>
-            <body>
+            <body onRender={checkForErrors}>
                 <div class="modal">
                     <div class="overlay" onClick={handlePrerenderClose} />
+                    <div class="top-overlay" />
                     <div class="modal-content">
                         <div class="close-button">
                             <button onClick={handlePrerenderClose} type="button" />
                         </div>
+                        <div class="error" style="display: none"></div>
                         <Spinner nonce={props.nonce} />
                     </div>
                 </div>
