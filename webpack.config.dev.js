2447f2b4dfd9ca71def8c1642dac05a3826d055f
Ryan Gilbert <gilbertstheman+github@gmail.com>
2021-06-24 15:18:31 -0400

    add missing globals for dev (#544)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -41,7 +41,9 @@ module.exports = (env = {}) => {
                       __PATH__: `/${FILE_NAME}.js`,
                       __NAMESPACE__: 'paypal',
                       __VERSION__: '1.0.55',
-                      __COMPONENTS__: ['messages']
+                      __COMPONENTS__: ['messages'],
+                      __PAYPAL_DOMAIN__: `${PROTOCOL}://${HOSTNAME}:${PORT}`,
+                      __PAYPAL_API_DOMAIN__: `${PROTOCOL}://${HOSTNAME}:${PORT}`
                   }
               });
 
