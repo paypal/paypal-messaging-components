0e4cb1d9d15a3603a2c681dbccdb700533f189b1
rosman21 <rosman21@gmail.com>
2021-08-17 11:26:19 -0400

    removed unnecessary options for flex banners

--- server/locale/DE/Pi30/validOptions.js
+++ server/locale/DE/Pi30/validOptions.js
@@ -16,12 +16,8 @@ export default {
         preset: [Types.STRING, [undefined, 'smallest']]
     },
     flex: {
-        color: [
-            Types.STRING,
-            ['blue', 'black', 'white', 'white-no-border', 'gray|grey', 'monochrome', 'grayscale|greyscale']
-        ],
+        color: [Types.STRING, ['blue', 'black', 'white', 'gray|grey', 'monochrome', 'grayscale|greyscale']],
         ratio: [Types.STRING, ['1x1', '1x4', '8x1', '20x1']],
-        preset: [Types.STRING, [undefined, 'smallest']],
         text: {
             fontFamily: [Types.STRING],
             fontSource: [Types.ANY]
