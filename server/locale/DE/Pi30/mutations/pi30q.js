b105e2d25f47c6bd3bd8b6eb00943283a6313601
rosman21 <rosman21@gmail.com>
2021-09-02 19:23:53 -0400

    modified messages based on excel file

--- server/locale/DE/Pi30/mutations/pi30q.js
+++ server/locale/DE/Pi30/mutations/pi30q.js
@@ -8,18 +8,86 @@ import {
     xSmallNoWrap,
     addPeriod
 } from '../../../../message/mediaQueries';
-import { textLogoMutations } from '../../../../message/logoMutations';
+import { flexLogoMutations, textLogoMutations } from '../../../../message/logoMutations';
+
+const headlineBreaks = [
+    {
+        sizes: ['xsmall'],
+        breaks: ['kaufen,']
+    },
+    {
+        sizes: ['medium'],
+        breaks: ['nach']
+    }
+].reduce((acc, item) => {
+    const { sizes, breaks } = item;
+    sizes.forEach(size => {
+        acc.push({
+            tag: size,
+            br: breaks
+        });
+    });
+    return acc;
+}, []);
+
+const flex = [
+    [
+        'default',
+        {
+            logo: Logo.PP_PAYPAL.WHITE,
+            headline: [
+                {
+                    tag: 'xsmall'
+                },
+                {
+                    tag: 'medium'
+                }
+            ],
+            disclaimer: ['default']
+        }
+    ],
+    [
+        'ratio:1x1',
+        {
+            styles: [addPeriod()],
+            headline: [...headlineBreaks]
+        }
+    ],
+    [
+        'ratio:1x4',
+        {
+            styles: [addPeriod()],
+            headline: [...headlineBreaks]
+        }
+    ],
+    [
+        'ratio:8x1',
+        {
+            styles: [addPeriod()],
+            headline: [...headlineBreaks]
+        }
+    ],
+    [
+        'ratio:20x1',
+        {
+            styles: [addPeriod()],
+            headline: [...headlineBreaks]
+        }
+    ],
+    ...flexLogoMutations
+];
 
 export default {
+    'layout:flex': flex,
     'layout:text': [
         [
             'default',
             ({ textSize }) => ({
                 styles: [
                     textWrap(textSize * 55, textSize, 'DE'),
-                    xSmallFallback(textSize * 8.5),
+                    xSmallFallback(textSize * 13),
                     messageLogoWidth(false, textSize * 4, textSize * 1.25),
-                    setLogoTop(textSize * 15),
+                    setLogoTop(textSize * 20),
                     xSmallNoWrap(textSize * 7.7),
                     addPeriod(),
                     `@media screen and 
@@ -34,7 +102,7 @@ export default {
                         tag: 'medium',
                         br: ['nach']
                     },
-                    { tag: 'xsmall', br: [','] }
+                    { tag: 'xsmall', br: ['später'] }
                 ],
                 disclaimer: ['default']
             })
@@ -43,21 +111,35 @@ export default {
             'logo.type:primary && logo.position:right',
             ({ textSize }) => ({
                 styles: [
-                    xSmallFallback(textSize * 8),
-                    setLogoTop(textSize * 26.5),
+                    textWrap(textSize * 55, textSize, 'DE'),
+                    xSmallFallback(textSize * 12.5),
+                    setLogoTop(textSize * 26),
                     messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25),
-                    addPeriod()
-                ]
+                    addPeriod(),
+                    xSmallNoWrap(textSize * 8.3),
+                    `@media screen and 
+                    (max-width: ${textSize * 18.67}px) {
+                        .message__headline > .tag--medium > span > span.br:nth-child(2) {white-space: nowrap;}
+                    }`
+                ],
+                headline: [{ tag: 'medium', br: ['bei'] }, { tag: 'xsmall' }]
             })
         ],
         [
             'logo.type:primary && logo.position:top',
             ({ textSize }) => ({
                 styles: [
-                    xSmallFallback(textSize * 9.5),
+                    textWrap(textSize * 55, textSize, 'DE'),
+                    xSmallFallback(textSize * 12.5),
                     messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25),
-                    addPeriod()
-                ]
+                    addPeriod(),
+                    xSmallNoWrap(textSize * 9),
+                    `@media screen and 
+                    (max-width: ${textSize * 18.67}px) {
+                        .message__headline > .tag--medium > span > span.br:nth-child(2) {white-space: nowrap;}
+                    }`
+                ],
+                headline: [{ tag: 'medium', br: ['bei'] }, { tag: 'xsmall' }]
             })
         ],
         [
@@ -76,7 +158,7 @@ export default {
                         12.5}px) { .locale--DE .message__messaging { white-space: nowrap;}}`
                 ],
                 headline: [
-                    { tag: 'medium', br: ['nach'] },
+                    { tag: 'medium', br: ['bie'] },
                     { tag: 'xsmall', br: ['später'] }
                 ],
                 logo: Logo.PP_PAYPAL.COLOR[0]
@@ -85,12 +167,17 @@ export default {
         [
             'logo.type:none',
             ({ textSize }) => ({
-                styles: [xSmallFallback(textSize * 14)],
+                styles: [
+                    textWrap(textSize * 55, textSize, 'DE'),
+                    xSmallFallback(textSize * 13),
+                    xSmallNoWrap(textSize * 8),
+                    `.locale--DE .message__messaging .tag--medium span.br {white-space:normal;}`
+                    // `.locale--DE .message__messaging .tag--medium span.br:nth-child(2) {white-space:nowrap;}`
+                ],
                 logo: false,
                 headline: [
                     {
-                        tag: 'medium',
-                        br: ['bei']
+                        tag: 'medium.text'
                     },
                     {
                         tag: 'xsmall',
@@ -104,14 +191,16 @@ export default {
             ({ textSize }) => ({
                 styles: [
                     xSmallFallback(textSize * 14),
+                    xSmallNoWrap(textSize * 13.8),
                     `.message__logo { width: ${textSize * 4}px }`,
-                    `.message__logo-container::after { content: '.'; }`
+                    `.message__logo-container::after { content: '.'; }`,
+                    `.locale--DE .message__messaging .tag--medium span.br {white-space:normal;}`,
+                    `.locale--DE .message__messaging .tag--medium span.br:nth-child(2) {white-space:nowrap;}`
                 ],
                 logo: Logo.NO_PP_MONOGRAM.COLOR,
                 headline: [
                     {
-                        tag: 'medium',
-                        br: ['bei']
+                        tag: 'medium.text'
                     },
                     {
                         tag: 'xsmall',
