23f9284668a158d4fd784aba51a2c8e0d2f18a60
jadutter <4691511+jadutter@users.noreply.github.com>
2021-10-04 13:46:44 -0400

    added some starting css

--- src/components/modal/v2/parts/views/NI/Content.jsx
+++ src/components/modal/v2/parts/views/NI/Content.jsx
@@ -1,31 +1,39 @@
 /** @jsx h */
 import { h, Fragment } from 'preact';
 import { useState } from 'preact/hooks';
-// import Button from '../../Button';
+import Button from '../../Button';
 import Instructions from '../../Instructions';
+import styles from './styles/index.scss';
 
-export const NI = ({
-    // subHeadline,
-    instructions,
-    // terms,
-    buttonText
-    // disclaimer,
-    // footer
-}) => {
+export const NI = ({ instructions, terms, buttonText, disclaimer, footer }) => {
     const [expandedState] = useState(false);
     return (
         <Fragment>
+            <style>{styles._getCss()}</style>
             <div className="content__container">
                 <main className="main">
                     <div className="content__body">
                         <Instructions instructions={instructions} expandedState={expandedState} />
-                        <div className="button">{buttonText}</div>
-                        {/*
-                        <div className={`content__row disclosure ${expandedState ? '' : 'collapsed'}`}>
-                            {disclosure}
-                        </div>
-                        */}
+                        <ul className="content__row terms">
+                            {terms.map(item => (
+                                <li className="terms-item">{item}</li>
+                            ))}
+                        </ul>
+                        <Button className="content__row">{buttonText}</Button>
+                        <div className="content__row disclaimer">{disclaimer}</div>
                     </div>
+                    <ul className=" footer">
+                        {footer.map(content => {
+                            const line = content.map(item => {
+                                if (Array.isArray(item)) {
+                                    const [text, link] = item;
+                                    return <a href={link}>{text}</a>;
+                                }
+                                return <span>{item}</span>;
+                            });
+                            return <li className="footer-item">{line}</li>;
+                        })}
+                    </ul>
                 </main>
             </div>
         </Fragment>
