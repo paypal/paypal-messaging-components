914e7dcb1761c8bc563a135d180551eb01bd33c6
Grant Black <grablack@paypal.com>
2021-10-25 22:20:58 -0400

    refactor

--- src/components/modal/v2/parts/Donut.jsx
+++ src/components/modal/v2/parts/Donut.jsx
@@ -1,44 +1,13 @@
 /** @jsx h */
 /* eslint-disable react/no-unknown-property */
 import { Fragment, h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
-import { debounce } from 'belter/src';
-
-const green500 = '#00CF92';
-const grey300 = '#DBD8D0';
-const grey600 = '#888';
 
 const generateDonutData = (currentNum = 0, numOfPayments = 4) => {
-    return [{ color: green500, percentage: (currentNum / numOfPayments) * 100 }];
-};
-
-const getWindowDimensions = () => {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        screenWidth: width,
-        screenheight: height
-    };
-};
-
-const useWindowDimensions = () => {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
+    return [
+        {
+            percentage: (currentNum / numOfPayments) * 100
         }
-
-        window.addEventListener(
-            'resize',
-            debounce(() => handleResize())
-        );
-
-        return () =>
-            window.removeEventListener(
-                'resize',
-                debounce(() => handleResize())
-            );
-    }, []);
-    return windowDimensions;
+    ];
 };
 
 const Donut = ({
@@ -51,7 +20,6 @@ const Donut = ({
     style = { fontSize: '0.375rem' },
     children,
     segmentStrokeWidth = 4,
-    baseColor = grey300,
     currentNum = 0,
     timeStamp,
     periodicPayment,
@@ -63,39 +31,35 @@ const Donut = ({
     if (!segStrokeWidth) {
         segStrokeWidth = strokeWidth;
     }
-    let width = '82px';
-    let height = '82px';
-
-    const { screenWidth } = useWindowDimensions();
-
-    if (screenWidth < 400) {
-        width = '60px';
-        height = '60px';
-    }
 
     return (
-        <div className="donut__single_payment">
+        <div className={`donut__single_payment ${qualifying === 'true' && 'donut__qualifying_payment'}`}>
             <svg
                 aria-hidden
-                width={width}
-                height={height}
                 viewBox={viewBox}
                 className="donut"
                 style={style}
                 xmlns="http://www.w3.org/2000/svg"
                 // {...rest}
             >
-                <circle cx={cx} cy={cy} r={radius} fill="transparent" stroke={baseColor} stroke-width={strokeWidth} />
+                <circle
+                    cx={cx}
+                    cy={cy}
+                    r={radius}
+                    fill="transparent"
+                    className="donut__background"
+                    stroke-width={strokeWidth}
+                />
                 <g stroke-width={segStrokeWidth} fill="transparent" stroke-linecap={strokeLinecap}>
                     <DonutSegments cx={cx} cy={cy} radius={radius} data={data} />
                 </g>
                 {children && (
-                    <text x={cx} y={cy + 2} text-anchor="middle" fill={grey600}>
-                        {qualifying && children}
+                    <text x={cx} y={cy} text-anchor="middle">
+                        {qualifying === 'true' && children}
                     </text>
                 )}
             </svg>
-            {periodicPayment && periodicPayment !== '-' && <span>{periodicPayment}</span>}
+            {qualifying === 'true' && periodicPayment !== '-' && <span>{periodicPayment}</span>}
             <span className="donut__timestamp">{timeStamp}</span>
         </div>
     );
@@ -103,7 +67,7 @@ const Donut = ({
 
 const DonutSegments = ({ cx, cy, radius, data }) => {
     let offset = 25; // To start from 12'O clock
-    const segments = data.map(({ color, percentage }) => {
+    const segments = data.map(({ percentage }) => {
         const strokeDasharray = `${percentage} ${100 - percentage}`;
         const strokeDashoffset = offset;
         offset = 100 - percentage + offset;
@@ -114,7 +78,7 @@ const DonutSegments = ({ cx, cy, radius, data }) => {
                 cx={cx}
                 cy={cy}
                 r={radius}
-                stroke={color}
+                className="donut__percent"
                 stroke-dasharray={strokeDasharray}
                 stroke-dashoffset={strokeDashoffset}
             />
