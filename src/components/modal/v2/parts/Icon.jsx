10af31a1ea8bacb9916ed0e4544f882c6ab9a640
yeul <mpscqw@gmail.com>
2021-10-25 21:03:23 -0400

    update Icon component with header background

--- src/components/modal/v2/parts/Icon.jsx
+++ src/components/modal/v2/parts/Icon.jsx
@@ -52,13 +52,108 @@ const Icon = ({ name, color }) => {
                     />
                 </svg>
             );
-        case 'pay-monthly-image':
+        case 'pay-monthly-temp-image':
             return (
                 <img
-                    alt="temporary"
+                    alt="pp-lt"
                     style={{ width: '100%' }}
-                    src="https://preview.redd.it/7c2gaheiowu31.png?auto=webp&s=5581d6fd2c0769db69c5e71e1d9938866aabb7fc"
+                    src="https://www.paypalobjects.com/marketing/web/us/en/credit-line-and-card-services/new/pp-credit-and-cards-hero.png"
                 />
+            );
+        case 'header-background':
+            return (
+                <svg width="1920" height="1380" viewBox="0 0 1920 1380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clipPath="url(#clip0)">
+                        <rect width="1920" height="1379" transform="translate(0 0.5)" fill="#1040C1" />
+                        <mask
+                            id="mask0"
+                            style={{ maskType: 'alpha' }}
+                            maskUnits="userSpaceOnUse"
+                            x="0"
+                            y="0"
+                            width="1920"
+                            height="1378"
+                        >
+                            <rect y="0.5" width="1920" height="1377" fill="#38AF88" />
+                        </mask>
+                        <g mask="url(#mask0)">
+                            <rect
+                                opacity="0.5"
+                                width="915.857"
+                                height="1623.41"
+                                transform="matrix(0.970524 0.241004 -0.438762 0.898603 1422.9 -79.2495)"
+                                fill="#1072EB"
+                            />
+                            <rect
+                                opacity="0.5"
+                                width="1103.73"
+                                height="2258.79"
+                                transform="matrix(0.0750469 -0.99718 0.997737 0.067235 -338 1307.5)"
+                                fill="#1040C1"
+                            />
+                            <rect
+                                opacity="0.5"
+                                width="900.752"
+                                height="1693.94"
+                                transform="matrix(0.970524 0.241004 -0.438762 0.898603 0.238281 -240.688)"
+                                fill="#142C8E"
+                            />
+                            <rect
+                                opacity="0.5"
+                                width="351.272"
+                                height="2301"
+                                transform="matrix(0.0432526 -0.999064 0.998507 0.0546328 -25 151.285)"
+                                fill="#1040C1"
+                            />
+                        </g>
+                        <mask
+                            id="mask1"
+                            style={{ maskType: 'alpha' }}
+                            maskUnits="userSpaceOnUse"
+                            x="0"
+                            y="0"
+                            width="1920"
+                            height="1378"
+                        >
+                            <rect y="0.5" width="1920" height="1377" fill="#38AF88" />
+                        </mask>
+                        <g mask="url(#mask1)">
+                            <rect
+                                opacity="0.5"
+                                width="915.857"
+                                height="1623.41"
+                                transform="matrix(0.970524 0.241004 -0.438762 0.898603 1422.9 -79.2495)"
+                                fill="#1072EB"
+                            />
+                            <rect
+                                opacity="0.5"
+                                width="1103.73"
+                                height="2258.79"
+                                transform="matrix(0.0750469 -0.99718 0.997737 0.067235 -338 1307.5)"
+                                fill="#1040C1"
+                            />
+                            <rect
+                                opacity="0.5"
+                                width="900.752"
+                                height="1693.94"
+                                transform="matrix(0.970524 0.241004 -0.438762 0.898603 0.238281 -240.688)"
+                                fill="#142C8E"
+                            />
+                            <rect
+                                opacity="0.5"
+                                width="351.272"
+                                height="2301"
+                                transform="matrix(0.0432526 -0.999064 0.998507 0.0546328 -25 151.285)"
+                                fill="#1040C1"
+                            />
+                        </g>
+                    </g>
+                    <defs>
+                        <clipPath id="clip0">
+                            <rect width="1920" height="1379" fill="white" transform="translate(0 0.5)" />
+                        </clipPath>
+                    </defs>
+                </svg>
             );
         default:
             return null;
