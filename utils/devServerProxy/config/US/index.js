59ec9bef3f506171ef03ebc8ef892473b29499e2
Merlin (they/them) Patterson <421960+merlinpatt@users.noreply.github.com>
2021-12-03 08:16:23 -0500

    Move US dev accounts into folder

---
+++ utils/devServerProxy/config/US/index.js
@@ -0,0 +1,2 @@
+export { DEV_US_MULTI } from './multi';
+export { DEV_US_PRODUCT_LIST } from './productList';
---
+++ utils/devServerProxy/config/US/multi.js
@@ -0,0 +1,70 @@
+export const DEV_US_MULTI = {
+    country: 'US',
+    modalViews: [
+        {
+            template: 'product_list.json',
+            product: 'PRODUCT_LIST'
+        },
+        {
+            template: 'long_term.json',
+            offersTemplate: 'long_term.json',
+            product: 'PAY_LATER_LONG_TERM'
+        }
+    ],
+    messageThresholds: [
+        {
+            amount: 0,
+            template: 'gplnq.json',
+            product: 'PAY_LATER_SHORT_TERM'
+        }
+    ],
+    offers: {
+        PAYPAL_CREDIT_NO_INTEREST: [
+            {
+                totalPayments: 6,
+                apr: 0,
+                nominalRate: 0,
+                minAmount: 99
+            }
+        ],
+        PAY_LATER_SHORT_TERM: [
+            {
+                totalPayments: 4,
+                apr: 0,
+                nominalRate: 0,
+                minAmount: 30,
+                maxAmount: 1500
+            }
+        ],
+        PAY_LATER_LONG_TERM: [
+            {
+                totalPayments: 6,
+                apr: 0,
+                nominalRate: 0,
+                minAmount: 199,
+                maxAmount: 2999.99
+            },
+            {
+                totalPayments: 12,
+                apr: 0,
+                nominalRate: 0,
+                minAmount: 199,
+                maxAmount: 20000
+            },
+            {
+                totalPayments: 24,
+                apr: 0,
+                nominalRate: 0,
+                minAmount: 500,
+                maxAmount: 20000
+            },
+            {
+                totalPayments: 36,
+                apr: 0,
+                nominalRate: 0,
+                minAmount: 3000,
+                maxAmount: 20000
+            }
+        ]
+    }
+};
