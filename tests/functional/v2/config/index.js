17270042c383e0c08324ad991cb4e1a576112a88
yeul <mpscqw@gmail.com>
2021-12-20 02:32:58 -0500

    support universal modals with updated functional test infrastructure and coverage

---
+++ tests/functional/v2/config/index.js
@@ -0,0 +1,1 @@
+export * from './US/content.config';
---
+++ tests/functional/v2/utils/setup.js
@@ -0,0 +1,71 @@
+// This file contains various utility functions used to setup the universal modal functional test configs.
+
+/**
+ * Screen dimensions used in modal functional tests.
+ */
+export const screenDimensions = {
+    desktop: { width: 1920, height: 1440 },
+    mobile: { width: 375, height: 1250 }
+};
+
+/**
+ * Assembles provided config file into an array of objects.
+ */
+export const getPermutations = config => {
+    const configArr = [];
+
+    const viewports = ['desktop', 'mobile'];
+
+    Object.entries(config).forEach(([payerId, { country, amounts, minAmount = '', maxAmount = '' }]) => {
+        const account = payerId;
+        amounts.forEach(({ value, message, expectedValue, modalContent, ...rest }) => {
+            viewports.forEach(viewport => {
+                configArr.push({
+                    country,
+                    account,
+                    minAmount,
+                    maxAmount,
+                    amount: Number(value),
+                    message,
+                    expectedValue,
+                    modalContent,
+                    viewport,
+                    ...rest
+                });
+            });
+        });
+    });
+
+    return configArr;
+};
+
+/**
+ * @param {Array<string>} configs Which country config files to use (i.e. US).
+ * @param {Array<string>} accounts Which accounts from the config to use (i.e. DEV_US_MULTI).
+ * @returns {Array<object>} Filtered array of config permutations that meet input criteria.
+ */
+export const filterPermutations = (configs, accounts) => {
+    let filteredPermutations = [];
+    let configList = [];
+
+    configs.forEach(config => {
+        configList = [...configList, ...getPermutations(config)];
+    });
+
+    accounts.forEach(accountName => {
+        filteredPermutations = [
+            ...filteredPermutations,
+            ...configList.filter(({ account }) => account === accountName)
+        ];
+    });
+
+    return filteredPermutations.map(permutation => [permutation.country, permutation.account, permutation]);
+};
+
+export const testNameParts = (country, integration, account, amount, viewport) => {
+    const testName = expect
+        .getState()
+        .currentTestName.split(' - ')[1]
+        .replace(/\s/g, '_');
+    return `${country}-${integration}-${account}-${amount}-${testName}-${viewport}`;
+};
