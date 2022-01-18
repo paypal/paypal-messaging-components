d1ce584e8f59dab20877aa19366c4f2cf9a2fccd
Merlin (they/them) Patterson <421960+merlinpatt@users.noreply.github.com>
2022-01-18 14:16:33 -0500

    Make path more generic

--- .github/scripts/getTestPathPatterns.sh
+++ .github/scripts/getTestPathPatterns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # lists all functional test paths and then removes text leaving only {locale}/{type}/{subtype}
-validTestPaths=$(ls ./tests/functional/spec/*/*/*.test.js | sed -E 's/(.*spec\/|.test.js)//g')
+validTestPaths=$(find ./tests/functional/spec -name '*.test.js' | grep -v non-snapshot-tests | sed -E 's/(^.*spec\/|.test.js$)//g')
 # transforms the list into a comma separated json array
 testPathPatterns="[ \"$(echo $validTestPaths | tr '\n' ',' | sed 's/ /,/g;s/,$//;s/,/","/g' )\" ]"
 
