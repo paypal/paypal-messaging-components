04f79247b8b9c3623b68c7d240c7f17ea62b1a3d
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-01-26 09:04:09 -0500

    Update src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_sq.js
    Co-authored-by: Julia F. <mpscqw@gmail.com>

--- src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_sq.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_sq.js
@@ -95,6 +95,7 @@ export default {
                         .message__headline > .tag--default > span.br:first-child { white-space: normal; } 
                     }`,
                     altNoWrap(textSize * 10.6),
+                    textWrap(textSize * 32, textSize, 'US'),
                     messageLogoWidth(textSize * 1.75, textSize * 4, textSize * 1.25)
                 ],
                 logo: Logo.PP_PAYPAL.COLOR[0]
