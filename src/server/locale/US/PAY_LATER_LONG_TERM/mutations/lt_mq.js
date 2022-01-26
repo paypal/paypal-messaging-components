b74362f0af549e9d8fb522eda6aa45b04b647c4c
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-01-26 09:00:47 -0500

    Update src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_mq.js
    Co-authored-by: Julia F. <mpscqw@gmail.com>

--- src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_mq.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_mq.js
@@ -80,6 +80,7 @@ export default {
             ({ textSize }) => ({
                 styles: [
                     altNoWrap(textSize * 10.6),
+                    textWrap(textSize * 32, textSize, 'US'),
                     messageLogoWidth(textSize * 1.75, textSize * 4, textSize * 1.25)
                 ],
                 logo: Logo.PP_PAYPAL.COLOR[0]
