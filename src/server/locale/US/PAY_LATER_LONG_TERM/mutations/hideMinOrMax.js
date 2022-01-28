034de4e2059b387a412d9531477ddf6bd3cc098d
Merlin (they/them) Patterson <421960+merlinpatt@users.noreply.github.com>
2022-01-28 15:41:16 -0500

    Switch no amount to hide max

--- src/server/locale/US/PAY_LATER_LONG_TERM/mutations/hideMinOrMax.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/mutations/hideMinOrMax.js
@@ -3,8 +3,8 @@ const hideMinOrMax = ({ amount: amountString, min: minString, max: maxString }) 
     const min = Number(minString);
     const max = Number(maxString);
 
-    if (amount < min) return '.message__headline .max { display: none; }';
-    if (amount > max || !amount) return '.message__headline .min { display: none; }';
+    if (amount < min || !amount) return '.message__headline .max { display: none; }';
+    if (amount > max) return '.message__headline .min { display: none; }';
     return '';
 };
 
