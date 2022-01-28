409ed1da39c8c54f0e0b27a0e39135d1bfe63599
Merlin (they/them) Patterson <421960+merlinpatt@users.noreply.github.com>
2022-01-28 14:58:33 -0500

    Convert amount to string

--- src/server/locale/US/PAY_LATER_LONG_TERM/mutations/hideMinOrMax.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/mutations/hideMinOrMax.js
@@ -1,4 +1,5 @@
-const hideMinOrMax = ({ amount, min: minString, max: maxString }) => {
+const hideMinOrMax = ({ amount: amountString, min: minString, max: maxString }) => {
+    const amount = Number(amountString);
     const min = Number(minString);
     const max = Number(maxString);
 
