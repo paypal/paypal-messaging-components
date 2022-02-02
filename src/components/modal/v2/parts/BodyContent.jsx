27ff8f3bb5db00a2bbffbc910fab640afa43c620
Merlin (they/them) Patterson <421960+merlinpatt@users.noreply.github.com>
2022-02-02 13:42:11 -0500

    Lint

--- src/components/modal/v2/parts/BodyContent.jsx
+++ src/components/modal/v2/parts/BodyContent.jsx
@@ -49,7 +49,8 @@ const BodyContent = () => {
 
     useDidUpdateEffect(() => {
         scrollTo(0); // Reset scroll position to top when view changes
-        window.document.querySelector('#close-btn')?.focus();
+        const closeButton = window.document.querySelector('#close-btn');
+        if (closeButton) closeButton.focus();
     }, [viewName]);
 
     useDidUpdateEffect(() => {
