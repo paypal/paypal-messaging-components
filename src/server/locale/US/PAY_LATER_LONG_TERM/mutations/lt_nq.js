b7d1f5c448961108859ae79ca51c188c1f08eb97
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-02-14 10:56:38 -0500

    Update src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_nq.js
    Co-authored-by: Julia F. <mpscqw@gmail.com>

--- src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_nq.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_nq.js
@@ -93,13 +93,7 @@ export default {
         [
             'logo.type:primary && logo.position:top',
             ({ textSize }) => ({
-                styles: [
-                    `@media screen and (max-width: ${textSize * 18.5}px) { 
-                        .message__headline > .tag--medium > span.br:first-child { white-space: normal; } 
-                    }`,
-                    xSmallFallback(textSize * 10.75),
-                    messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25)
-                ]
+                styles: [xSmallFallback(textSize * 13), messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25)]
             })
         ],
         [
