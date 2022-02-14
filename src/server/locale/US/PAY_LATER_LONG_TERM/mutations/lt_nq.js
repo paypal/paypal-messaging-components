06f8a7ecc1fd0bf8e6d830f7c21877f9c32cd999
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-02-14 10:56:49 -0500

    Update src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_nq.js
    Co-authored-by: Julia F. <mpscqw@gmail.com>

--- src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_nq.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/mutations/lt_nq.js
@@ -100,17 +100,11 @@ export default {
             'logo.type:alternative',
             ({ textSize }) => ({
                 styles: [
-                    `@media screen and (max-width: ${textSize * 10.6}px) { 
-                        .message__headline > .tag--medium > span.br:first-child { white-space: normal; } 
-                    }`,
                     xSmallFallback(textSize * 11.5),
                     altNoWrap(textSize * 10.6),
                     messageLogoWidth(textSize * 1.75, textSize * 4, textSize * 1.25),
-                    `.locale--US .message__headline > .tag--medium > span { white-space: normal; }`,
-                    `.locale--US .message__headline { white-space: normal; }`,
-                    `@media screen and (max-width: ${textSize * 11.5}px) {
-                        .message__headline .tag--xsmall > span { white-space: normal; }
-                    }`
+                    `.locale--US .message__messaging span.br {white-space: normal}`,
+                    `.message__headline .tag--xsmall > span {white-space: normal}`
                 ],
                 logo: Logo.PP_PAYPAL.COLOR[0]
             })
