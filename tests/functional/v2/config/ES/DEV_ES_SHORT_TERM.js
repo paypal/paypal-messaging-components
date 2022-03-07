92271431f644895b6823b7606331513736e15267
Dan Haas <danzhaas@gmail.com>
2022-03-07 13:25:01 -0500

    corrects errors in ES short term test config

--- tests/functional/v2/config/ES/DEV_ES_SHORT_TERM.js
+++ tests/functional/v2/config/ES/DEV_ES_SHORT_TERM.js
@@ -8,30 +8,30 @@ export const DEV_ES_SHORT_TERM = {
         {
             value: '0.00',
             message: 'Non-qualifying Pay in 3',
-            expectedValue: 'Paga en 3 plazos sin intereses tus compras de 30 € a 2.000 €',
+            expectedValue: 'Paga en 3 plazos sin intereses tus compras de 30,00 € a 2.000,00 €',
             modalContent: {
                 subheadline:
-                    'Disponible para las compras de 30 € a 2.000 €. Sin comisiones de apertura ni por pago atrasado.',
+                    'Disponible para las compras de 30,00 € a 2.000,00 €. Sin comisiones de apertura ni por pago atrasado.',
                 periodicPayment: null
             }
         },
         {
             value: '30.00',
             message: 'Qualifying Pay in 3',
-            expectedValue: 'Paga en 3 plazos de 10.00 €, sin intereses',
+            expectedValue: 'Paga en 3 plazos de 10,00 €, sin intereses',
             modalContent: {
                 subheadline:
-                    'Divide tu compra de 30.00 € en 3 plazos, sin comisiones de apertura ni por pago atrasado.',
-                periodicPayment: '10.00 €'
+                    'Divide tu compra de 30,00 € en 3 plazos, sin comisiones de apertura ni por pago atrasado.',
+                periodicPayment: '10,00 €'
             }
         },
         {
             value: '2000.01',
             message: 'Non-qualifying Pay in 3',
-            expectedValue: 'Paga en 3 plazos sin intereses tus compras de 30 € a 2.000 €',
+            expectedValue: 'Paga en 3 plazos sin intereses tus compras de 30,00 € a 2.000,00 €',
             modalContent: {
                 subheadline:
-                    'Disponible para las compras de 30 € a 2.000 €. Sin comisiones de apertura ni por pago atrasado.',
+                    'Disponible para las compras de 30,00 € a 2.000,00 €. Sin comisiones de apertura ni por pago atrasado.',
                 periodicPayment: ''
             }
         }
