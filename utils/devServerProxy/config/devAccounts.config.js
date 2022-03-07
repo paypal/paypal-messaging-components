9d112d5676795b7ebc37c786d107514b52b0e42d
Dan Haas <danzhaas@gmail.com>
2022-03-07 16:42:54 -0500

    fixes ES text and flex snapshot functionality

--- utils/devServerProxy/config/devAccounts.config.js
+++ utils/devServerProxy/config/devAccounts.config.js
@@ -47,7 +47,7 @@ export default {
     DEV000000AUPL: ['AU', ['gpl'], 'gpl'],
     DEV00000AUPLQ: ['AU', ['gpl'], 'gplq'],
 
-    DEV0000ESPLNA: ['ES', ['gpl'], 'short_term_no_amount'],
-    DEV00000ESPLQ: ['ES', ['gpl'], 'short_term_q'],
-    DEV0000ESPLNQ: ['ES', ['gpl'], 'short_term_nq']
+    DEV0000ESPLNA: ['ES', ['short_term'], 'short_term_no_amount'],
+    DEV00000ESPLQ: ['ES', ['short_term'], 'short_term_q'],
+    DEV0000ESPLNQ: ['ES', ['short_term'], 'short_term_nq']
 };
