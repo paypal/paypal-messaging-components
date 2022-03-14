957f4c9c6a0a3515b4bdcc25a64dbf1b061bb4bc
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-03-14 11:29:36 -0400

    Fix FR test config

--- tests/functional/v2/config/FR/DEV_FR_SHORT_TERM.js
+++ tests/functional/v2/config/FR/DEV_FR_SHORT_TERM.js
@@ -7,31 +7,30 @@ export const DEV_FR_SHORT_TERM = {
     amounts: [
         {
             value: '0.00',
-            message: 'Non-qualifying Pay in 3',
-            expectedValue: 'Paga en 3 plazos sin intereses tus compras de 30,00 € a 2.000,00 €',
+            message: 'Non-qualifying Pay in 4',
+            expectedValue: 'Disponible pour les achats de 30,00 € à 2.000,00 €',
             modalContent: {
                 subheadline:
-                    'Disponible para las compras de 30,00 € a 2.000,00 €. Sin comisiones de apertura ni por pago atrasado.',
+                    'Disponible pour les achats de 30,00 € à 2.000,00 €. Pas de frais de dossier ni de frais de retard.',
                 periodicPayment: null
             }
         },
         {
             value: '30.00',
-            message: 'Qualifying Pay in 3',
-            expectedValue: 'Paga en 3 plazos de 10,00 €, sin intereses',
+            message: 'Qualifying Pay in 4',
+            expectedValue: 'Payez votre achat de 30,00 €',
             modalContent: {
-                subheadline:
-                    'Divide tu compra de 30,00 € en 3 plazos, sin comisiones de apertura ni por pago atrasado.',
+                subheadline: 'Payez votre achat de 30,00 € en 4 fois sans frais de dossier ni frais de retard.',
                 periodicPayment: '10,00 €'
             }
         },
         {
             value: '2000.01',
-            message: 'Non-qualifying Pay in 3',
-            expectedValue: 'Paga en 3 plazos sin intereses tus compras de 30,00 € a 2.000,00 €',
+            message: 'Non-qualifying Pay in 4',
+            expectedValue: 'Disponible pour les achats de 30,00 € à 2.000,00 €',
             modalContent: {
                 subheadline:
-                    'Disponible para las compras de 30,00 € a 2.000,00 €. Sin comisiones de apertura ni por pago atrasado.',
+                    'Disponible pour les achats de 30,00 € à 2.000,00 €. Pas de frais de dossier ni de frais de retard.',
                 periodicPayment: ''
             }
         }
