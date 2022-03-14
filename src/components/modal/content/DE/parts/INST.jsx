0d19dd8413856199c6fd381c1cfec06b541d6883
Grant Black <grablack@paypal.com>
2022-03-14 15:24:03 -0400

    update content to use

--- src/components/modal/content/DE/parts/INST.jsx
+++ src/components/modal/content/DE/parts/INST.jsx
@@ -6,7 +6,7 @@ import Calculator from './Calculator';
 import { useContent } from '../../../lib';
 
 const INST = () => {
-    const { carousel } = useContent('PAYPAL_CREDIT_INSTALLMENTS');
+    const { carousel } = useContent('PAY_LATER_SHORT_TERM');
 
     return (
         <section className="content-body">
