39e4f4100eec5f12f7c8b93fde008f85c47e657c
paypal-rosman <rosman@paypal.com>
2022-03-23 15:55:47 -0400

    spelling correction

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -102,7 +102,7 @@ const Message = function({ markup, meta, parentStyles, warnings }) {
         onProps(xprops => {
             const shouldRerender = Object.keys(props)
                 // specific situation where this variable would compare null to undefined casuing it to render as true.
-                // to migtigae that we are filtering all xprops and props values that are equal to null or undefined
+                // to mitigate that we are filtering all xprops and props values that are equal to null or undefined
                 // thus only checking values that are not null or undefined and determing if we need to rerender or not. 
                 // Rene 03/23/22
                 .filter(
