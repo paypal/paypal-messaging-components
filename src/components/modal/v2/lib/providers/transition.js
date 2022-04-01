2218c2f628acfd1962b78a5bf53bff5d3b98ad85
yeul <mpscqw@gmail.com>
2022-04-01 16:53:17 -0400

    fix view switching bug on universal modal

--- src/components/modal/v2/lib/providers/transition.js
+++ src/components/modal/v2/lib/providers/transition.js
@@ -42,7 +42,7 @@ export const TransitionStateProvider = ({ children }) => {
                     { threshold: 1 }
                 );
 
-                observer.observe(document.body);
+                observer.unobserve(document.body);
             });
         }
     }, []);
