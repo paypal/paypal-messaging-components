9c6fbf17ab9aa1d064b162b22dedc3b6563cb580
yeul <mpscqw@gmail.com>
2022-04-15 15:24:13 -0400

    US long term flex color option parity with other US products

--- src/server/locale/US/PAY_LATER_LONG_TERM/validOptions.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/validOptions.js
@@ -15,7 +15,10 @@ export default {
         }
     },
     flex: {
-        color: [Types.STRING, ['blue', 'black', 'white', 'gray|grey', 'monochrome', 'grayscale|greyscale']],
+        color: [
+            Types.STRING,
+            ['blue', 'black', 'white', 'white-no-border', 'gray|grey', 'monochrome', 'grayscale|greyscale']
+        ],
         ratio: [Types.STRING, ['1x1', '1x4', '8x1', '20x1']],
         text: {
             fontFamily: [Types.STRING],
