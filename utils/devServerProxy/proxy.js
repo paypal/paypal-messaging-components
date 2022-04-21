ec5ac9a4ae1a37fd0af327e02b8ea8acd8251f13
Ryan Gilbert <rygilbert@paypal.com>
2022-04-21 13:45:32 -0600

    Mock treatments endpoint in local dev server

--- utils/devServerProxy/proxy.js
+++ utils/devServerProxy/proxy.js
@@ -7,6 +7,32 @@ export default (app, server, compiler) => {
 
     app.post('/credit-presentment/log', (req, res) => res.send(''));
 
+    app.get('/credit-presentment/experiments/hash', (req, res) => res.send('1daf92517fb7620b02add6943517ae0a5ca8f0a0'));
+
+    app.get('/credit-presentment/experiments/local', (req, res) =>
+        res.send(`
+            <!DOCTYPE html>
+            <head>
+            </head>
+            <body>
+                <script>
+                    function sendTreatments () {
+                        window.parent.postMessage({
+                            treatmentsHash: '1daf92517fb7620b02add6943517ae0a5ca8f0a0',
+                            deviceID: 'uid_e495bda3c2_mtk6mzg6mju'
+                        }, '*');
+                    }
+                        
+                    function getTreatments () {
+                        sendTreatments();
+                    }
+
+                    window.addEventListener('load', getTreatments);
+                </script>
+            </body>
+        `)
+    );
+
     // Support versioned URLs
     app.get('/versioned/:component', (req, res) => {
         const { component } = req.params;
