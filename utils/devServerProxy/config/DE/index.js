d25e533304cda415b13723026fb92f2bcc5b12de
yeul <mpscqw@gmail.com>
2022-04-25 15:25:51 -0400

    tweaks to base DE proxy and file names

--- utils/devServerProxy/config/DE/index.js
+++ utils/devServerProxy/config/DE/index.js
@@ -9,50 +9,15 @@ export default {
                 product: 'PRODUCT_LIST'
             },
             {
-                template: 'pay_in_thirty.json',
+                template: 'pay_in_1.json',
                 product: 'PAY_LATER_PAY_IN_1'
             },
             {
-                template: 'ratenzahlung.json',
-                product: 'PAYPAL_CREDIT_INSTALLMENTS'
+                template: 'long_term.json',
+                product: 'PAY_LATER_LONG_TERM'
             }
         ],
-        messageThresholds: [
-            {
-                amount: 0,
-                template: 'gplq_eqz.json',
-                product: 'PAY_LATER_SHORT_TERM'
-            },
-            {
-                amount: 30,
-                template: 'gplq_eqz.json',
-                product: 'PAY_LATER_SHORT_TERM'
-            },
-            {
-                amount: 1500.01,
-                template: 'gplq_eqz.json',
-                product: 'PAYPAL_CREDIT_NO_INTEREST'
-            }
-        ],
-        offers: {
-            PAY_LATER_SHORT_TERM: [
-                {
-                    totalPayments: 4,
-                    apr: 0,
-                    nominalRate: 0,
-                    minAmount: 30,
-                    maxAmount: 1500
-                }
-            ],
-            PAYPAL_CREDIT_NO_INTEREST: [
-                {
-                    totalPayments: 6,
-                    apr: 0,
-                    nominalRate: 0,
-                    minAmount: 99,
-                    maxAmount: 9999.99
-                }
-            ]
-        }
+        messageThresholds: [],
+        offers: {}
     }
 };
