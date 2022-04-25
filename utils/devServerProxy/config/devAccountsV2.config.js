e876a4356d910872bc473269a799faf54d23ca02
yeul <mpscqw@gmail.com>
2022-04-25 15:27:17 -0400

    update import

--- utils/devServerProxy/config/devAccountsV2.config.js
+++ utils/devServerProxy/config/devAccountsV2.config.js
@@ -1,19 +1,19 @@
 import US from './US';
-import DE from './DE';
 import GB from './GB';
 import AU from './AU';
 import ES from './ES';
 import IT from './IT';
 import FR from './FR';
+import DE from './DE';
 
 // Note that these values are solely mock structures for development purposes
 // that are not guaranteed to be reflective of what is in production
 export default {
     ...US,
-    ...DE,
     ...GB,
     ...AU,
     ...ES,
     ...IT,
-    ...FR
+    ...FR,
+    ...DE
 };
