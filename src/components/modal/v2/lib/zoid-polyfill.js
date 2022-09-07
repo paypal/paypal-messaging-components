f26c7383b1cc18c5941e79b0c6ebe6bd8167d1c5
Nathan Schott <nschott@paypal.com>
2022-09-07 14:57:37 -0400

    feat: native modal communication

--- src/components/modal/v2/lib/zoid-polyfill.js
+++ src/components/modal/v2/lib/zoid-polyfill.js
@@ -1,21 +1,10 @@
+import { isIosWebview, isAndroidWebview } from '@krakenjs/belter/src';
 import { logger } from '../../../../utils';
 
-export default function polyfillZoid() {
-    const props = window.location.search
-        .slice(1)
-        .split('&')
-        .reduce((acc, query) => {
-            const [key, value] = query.split('=');
+const IOS_INTERFACE_NAME = 'paypalMessageModalCallbackHandler';
+const ANDROID_INTERFACE_NAME = 'PayPalMessageModalInterface';
 
-            if (value) {
-                const propName = key.replace(/_([a-z])/g, (_, p1) => p1.toUpperCase());
-
-                acc[propName] = value;
-            }
-
-            return acc;
-        }, {});
-
+const setupBrowser = props => {
     window.xprops = {
         // We will never recieve new props via this integration style
         onProps: () => {},
@@ -105,4 +94,113 @@ export default function polyfillZoid() {
         // Specified props via query params
         ...props
     };
+};
+
+const setupWebview = props => {
+    const postMessage = (() => {
+        if (window.webkit?.messageHandlers?.[IOS_INTERFACE_NAME]) {
+            return window.webkit.messageHandlers[IOS_INTERFACE_NAME].postMessage.bind(
+                window.webkit.messageHandlers[IOS_INTERFACE_NAME]
+            );
+        }
+
+        if (window[ANDROID_INTERFACE_NAME]) {
+            return window[ANDROID_INTERFACE_NAME].postMessage.bind(window[ANDROID_INTERFACE_NAME]);
+        }
+
+        // THis scenario should only ever occur when developing locally
+        return payload => console.warn('postMessage:', JSON.parse(payload));
+    })();
+
+    const propListeners = new Set();
+    const sendCallbackMessage = (name, ...args) => postMessage(JSON.stringify({ name, args }));
+    // Functions called from the native app
+    window.actions = {
+        updateProps: newProps => {
+            if (newProps && typeof newProps === 'object') {
+                Array.from(propListeners.values()).forEach(listener => {
+                    listener({ ...window.xprops, ...newProps });
+                });
+
+                Object.assign(window.xprops, newProps);
+            }
+        }
+    };
+    window.xprops = {
+        onProps: listener => propListeners.add(listener),
+        onReady: ({ products, meta, deviceID }) => {
+            const { offer, partnerAttributionId } = props;
+            const { tracking_details: trackingDetails } = meta;
+
+            sendCallbackMessage('onReady', {
+                et: 'CLIENT_IMPRESSION',
+                event_type: 'modal-render',
+                device_id: deviceID,
+                bn_code: partnerAttributionId,
+                products: `${products.join('_').toLowerCase()}:${offer ? offer.toLowerCase() : products[0]}`,
+                integration_type: props.integrationType ?? __MESSAGES__.__TARGET__,
+                ...trackingDetails
+            });
+        },
+        onClick: ({ linkName, src }) => {
+            sendCallbackMessage('onClick', {
+                et: 'CLICK',
+                event_type: 'click',
+                linkName,
+                src: src ?? linkName
+            });
+        },
+        onCalculate: ({ value }) => {
+            sendCallbackMessage('onCalculate', {
+                et: 'CLICK',
+                event_type: 'click',
+                link: 'Calculator',
+                src: 'Calculator',
+                amount: value
+            });
+        },
+        onShow: () => {
+            sendCallbackMessage('onShow', {
+                et: 'CLIENT_IMPRESSION',
+                event_type: 'modal-open',
+                src: 'Show'
+            });
+        },
+        onClose: ({ linkName }) => {
+            sendCallbackMessage('onClose', {
+                et: 'CLICK',
+                event_type: 'modal-close',
+                link: linkName
+            });
+        },
+        // Overridable defaults
+        integrationType: __MESSAGES__.__TARGET__,
+        // Specified props via query params
+        ...props
+    };
+};
+
+export default function polyfillZoid() {
+    const props = window.location.search
+        .slice(1)
+        .split('&')
+        .reduce((acc, query) => {
+            const [key, value] = query.split('=');
+
+            if (value) {
+                const propName = key.replace(/_([a-z])/g, (_, p1) => p1.toUpperCase());
+
+                acc[propName] = value;
+            }
+
+            return acc;
+        }, {});
+
+    const { userAgent } = window.navigator;
+
+    if (isIosWebview(userAgent) || isAndroidWebview(userAgent)) {
+        setupWebview(props);
+    } else {
+        setupBrowser(props);
+    }
 }
