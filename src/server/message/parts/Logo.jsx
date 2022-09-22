2dd248470046cc4f5322487e4ea2ab78e84d185a
Erika Tidwell <etidwell@paypal.com>
2022-09-22 11:49:04 -0400

    task 1

--- src/server/message/parts/Logo.jsx
+++ src/server/message/parts/Logo.jsx
@@ -10,7 +10,8 @@ const Logos = ({ mutations }) => {
         <div className="message__logo-container" aria-label="PayPal Credit">
             {logos.map(({ src, dimensions: [width, height] }) => (
                 <div className="message__logo message__logo--svg">
-                    <img src={src} role="presentation" alt="PayPal Credit" aria-hidden="true" />
+                    {/* task 1 */}
+                    <img src={src} alt="PayPal Credit" />
                     <canvas height={height} width={width} />
                 </div>
             ))}
