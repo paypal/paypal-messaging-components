722c889c3567d335ff63879b1f51b9bc0aa439f6
Erika Tidwell <etidwell@paypal.com>
2022-11-01 09:46:36 -0400

    removed installments folder

--- src/server/locale/DE/Ratenzahlung/styles/text/index.js
+++
@@ -1,9 +0,0 @@
-import base from './base.css';
-import logoInline from './logo--inline.css';
-import textWhite from './text--white.css';
-
-export default [
-    ['default', base],
-    ['logo.type:inline', logoInline],
-    ['text.color:white', textWhite]
-];
