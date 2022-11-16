c7d91e18e44b1d67ae06a7419c9ada18dffeedea
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2022-11-16 14:41:58 -0500

    fix: NQ US Pay Monthly message in Alternative style not wrapping (DTCRCMERC-1861) (#882)

--- src/server/locale/US/PAY_LATER_LONG_TERM/mutations/long_term_nq_eqz.js
+++ src/server/locale/US/PAY_LATER_LONG_TERM/mutations/long_term_nq_eqz.js
@@ -100,11 +100,11 @@ export default {
             'logo.type:alternative',
             ({ textSize }) => ({
                 styles: [
-                    xSmallFallback(textSize * 12.5),
+                    `@media screen and (max-width: ${textSize * 10.6}px) { .message__content { white-space: nowrap; }}`,
+                    xSmallFallback(textSize * 14.25),
                     altNoWrap(textSize * 10.6),
-                    messageLogoWidth(textSize * 1.75, textSize * 4, textSize * 1.25),
-                    `.locale--US .message__messaging span.br {white-space: normal}`,
-                    `.message__headline .tag--xsmall > span {white-space: normal}`
+                    textWrap(textSize * 32, textSize, 'US'),
+                    messageLogoWidth(textSize * 1.75, textSize * 4, textSize * 1.25)
                 ],
                 logo: Logo.PP_PAYPAL.COLOR[0]
             })
