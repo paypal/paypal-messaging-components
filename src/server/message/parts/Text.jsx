b4f32d849cc705d450cc8a72a85536e3409edb8d
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-11-17 11:35:10 -0500

    Add style to custom messaging

--- src/server/message/parts/Text.jsx
+++ src/server/message/parts/Text.jsx
@@ -5,13 +5,13 @@ import { h, Fragment } from 'preact';
 const Text = ({ children, className, spaced }) => {
     // if (children.)
     let text = <span className={className}>{children}</span>;
+
     const hasHtml = typeof children === 'string' && children.includes('<span');
-    console.log({ hasHtml });
     if (hasHtml) {
-        console.log(children);
         // eslint-disable-next-line react/no-danger
         text = <span className={className} dangerouslySetInnerHTML={{ __html: children }} />;
     }
+
     return spaced ? <>{text} </> : text;
 };
 
