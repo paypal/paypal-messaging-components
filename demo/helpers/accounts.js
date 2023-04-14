18b7e7af3ca9575b7ead21c82d7781c8719dfc10
Erika Tidwell <etidwell@paypal.com>
2023-04-14 16:05:19 -0400

    fix added account file back in

--- demo/helpers/accounts.js
+++ demo/helpers/accounts.js
@@ -1,20 +1,20 @@
 // prettier-ignore
 const accounts = [
     // US
-    // 'DEV00000000NI',
-    // 'DEV0000000NIQ',
-    // 'DEV000NINONUS',
-    // 'DEV00NINONUSQ',
-    // 'DEV0000000EAZ',
-    // 'DEV0000000EAG',
-    // 'DEV0000000PSZ',
-    // 'DEV0000000PSG',
-    // 'DEV0000000PMZ',
-    // 'DEV0000000PMG',
+    'DEV00000000NI',
+    'DEV0000000NIQ',
+    'DEV000NINONUS',
+    'DEV00NINONUSQ',
+    'DEV0000000EAZ',
+    'DEV0000000EAG',
+    'DEV0000000PSZ',
+    'DEV0000000PSG',
+    'DEV0000000PMZ',
+    'DEV0000000PMG',
     // pay later short term
-    // 'DEV0000000GPL',
-    // 'DEV00000GPLNQ',
-    // 'DEV000000GPLQ',
+    'DEV0000000GPL',
+    'DEV00000GPLNQ',
+    'DEV000000GPLQ',
     // pay later long term
     // 'DEV0USGENERIC',
     // 'DEV00USLTNQEZ',
@@ -49,8 +49,8 @@ const accounts = [
     // 'DEV00000GBPLQ',
 
     // FR
-    'DEV000000FRPL',
-    'DEV00000FRPLQ'
+    // 'DEV000000FRPL',
+    // 'DEV00000FRPLQ'
 
     // ES
     // 'DEV00000ESPLQ',
