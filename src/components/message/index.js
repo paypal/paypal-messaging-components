78799617db4f08a5130fa533b9d5ccdfa86053b0
Grant Black <grablack@paypal.com>
2023-05-23 15:23:23 -0400

    needs timeout because of map

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,6 +1,6 @@
 import createMessage from './Message';
 
-export async function setupMessage({ markup, meta, parentStyles, warnings }) {
-    const message = await createMessage({ markup, meta, parentStyles, warnings });
-    await document.body.appendChild(message);
+export function setupMessage({ markup, meta, parentStyles, warnings }) {
+    const message = createMessage({ markup, meta, parentStyles, warnings });
+    document.body.appendChild(message);
 }
