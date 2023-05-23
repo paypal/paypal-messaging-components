486f891407fa0cfb295eee8c1a70e1543e4ea8f3
Grant Black <grablack@paypal.com>
2023-05-23 12:46:39 -0400

    add await

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -2,5 +2,5 @@ import createMessage from './Message';
 
 export async function setupMessage({ markup, meta, parentStyles, warnings }) {
     const message = await createMessage({ markup, meta, parentStyles, warnings });
-    document.body.appendChild(message);
+    await document.body.appendChild(message);
 }
