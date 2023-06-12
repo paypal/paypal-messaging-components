b4ddb583f854626f1bfeb218d4315685973e4839
Merlin (they/them) <124289716+merlinpaypal@users.noreply.github.com>
2023-06-12 09:59:07 -0400

    Fix loose warning

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,8 @@ module.exports = {
     extends: '@krakenjs/babel-config-grumbler/babelrc-browser',
     env: {
         test: {
-            presets: [['@babel/env', { targets: { node: 'current' } }]]
+            presets: [['@babel/env', { targets: { node: 'current' } }]],
+            plugins: ['@babel/plugin-transform-private-property-in-object', { loose: true }]
         }
     }
 };
