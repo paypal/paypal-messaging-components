9b1a2408db4fffd150033dcd25c1d5eb02f5422f
Grant Black <85238976+grablack@users.noreply.github.com>
2023-06-14 08:48:42 -0400

    refactor: chunk multi-message rendering to prevent singular long task (#939)

--- src/library/controllers/message/interface.js
+++ src/library/controllers/message/interface.js
@@ -59,145 +59,166 @@ export default (options = {}) => ({
 
         return awaitTreatments.then(() =>
             ZalgoPromise.all(
-                validContainers.map(container => {
-                    try {
-                        const merchantOptions = objectMerge(
-                            getGlobalState().config,
-                            objectMerge(options, getInlineOptions(container))
-                        );
+                validContainers.map((container, mapIndex) => {
+                    return new ZalgoPromise((resolve, reject) => {
+                        // return resolved render and updateProps
+                        const renderOrUpdateMessage = () => {
+                            try {
+                                const merchantOptions = objectMerge(
+                                    getGlobalState().config,
+                                    objectMerge(options, getInlineOptions(container))
+                                );
 
-                        if (!container.hasAttribute('data-pp-id')) {
-                            container.setAttribute('data-pp-id', nextIndex());
-                        }
-
-                        const index = container.getAttribute('data-pp-id');
-                        const {
-                            account,
-                            merchantId,
-                            customerId,
-                            currency,
-                            amount,
-                            placement,
-                            style,
-                            offer,
-                            buyerCountry,
-                            ignoreCache,
-                            onClick,
-                            onRender,
-                            onApply,
-                            channel,
-                            ecToken,
-                            cspNonce
-                        } = merchantOptions;
-
-                        // Explicitly select props to pass in to avoid unintentionally sending
-                        // in props meant for only either the message or modal (e.g. onClick)
-                        const commonProps = {
-                            account,
-                            merchantId,
-                            customerId,
-                            currency,
-                            amount,
-                            buyerCountry,
-                            ignoreCache,
-                            channel,
-                            ecToken,
-                            cspNonce
-                        };
-                        const modalProps = {
-                            ...commonProps,
-                            onApply
-                        };
-                        const messageProps = {
-                            ...commonProps,
-                            index,
-                            placement,
-                            style,
-                            offer,
-                            onClick,
-                            onApply,
-                            onReady: (...args) => {
-                                if (typeof onRender === 'function') {
-                                    onRender(...args);
+                                if (!container.hasAttribute('data-pp-id')) {
+                                    container.setAttribute('data-pp-id', nextIndex());
                                 }
-                            },
-                            // Used in the computed callback props
-                            getContainer: () => container
+
+                                const index = container.getAttribute('data-pp-id');
+                                const {
+                                    account,
+                                    merchantId,
+                                    customerId,
+                                    currency,
+                                    amount,
+                                    placement,
+                                    style,
+                                    offer,
+                                    buyerCountry,
+                                    ignoreCache,
+                                    onClick,
+                                    onRender,
+                                    onApply,
+                                    channel,
+                                    ecToken,
+                                    cspNonce
+                                } = merchantOptions;
+
+                                // Explicitly select props to pass in to avoid unintentionally sending
+                                // in props meant for only either the message or modal (e.g. onClick)
+                                const commonProps = {
+                                    account,
+                                    merchantId,
+                                    customerId,
+                                    currency,
+                                    amount,
+                                    buyerCountry,
+                                    ignoreCache,
+                                    channel,
+                                    ecToken,
+                                    cspNonce
+                                };
+                                const modalProps = {
+                                    ...commonProps,
+                                    onApply
+                                };
+                                const messageProps = {
+                                    ...commonProps,
+                                    index,
+                                    placement,
+                                    style,
+                                    offer,
+                                    onClick,
+                                    onApply,
+                                    onReady: (...args) => {
+                                        if (typeof onRender === 'function') {
+                                            onRender(...args);
+                                        }
+                                    },
+                                    // Used in the computed callback props
+                                    getContainer: () => container
+                                };
+
+                                // Account can be undefined in the case where the attribute observer fires and there
+                                // is no global account, such as when rendering entirely using the JavaScript API
+                                if (account) {
+                                    messageProps.modal = Modal(modalProps);
+                                }
+
+                                if (!messagesMap.has(container)) {
+                                    const { render, state, updateProps, clone } = getMessageComponent()(messageProps);
+
+                                    state.renderStart = renderStart;
+                                    state.style = messageProps.style;
+
+                                    messagesMap.set(container, { render, updateProps, state, clone });
+
+                                    getAttributeObserver().observe(container, { attributes: true });
+
+                                    ppDebug(
+                                        `{
+                            clientID: ${account},
+                            merchantID: ${merchantId},
+                            customerID: ${customerId},
+                            offer: ${offer},
+                            currency: ${currency},
+                            ignoreCache: ${ignoreCache},
+                            channel: ${channel},
+                            ecToken: ${ecToken},
+                    
+                            index: data-pp-id="${index}",
+                            style: ${JSON.stringify(style)},
+                            amount: ${amount},
+                            buyerCountry: ${buyerCountry},
+                            placement: ${placement},
+                    
+                            renderStart: ${new Date(renderStart).toLocaleString()},
+                            renderMessageTime: ${new Date().toLocaleString()}
+                            }`
+                                    );
+
+                                    return render(container)
+                                        .then(() => globalEvent.trigger('render'))
+                                        .then(resolve);
+                                }
+
+                                const { updateProps, state } = messagesMap.get(container);
+
+                                if (state.renderComplete) {
+                                    state.renderStart = renderStart;
+                                }
+
+                                // Merge new styles into previous styles
+                                // Especially useful when combining inline attribute styles with JS API styles
+                                if (state.style && messageProps.style) {
+                                    const totalStyle = objectMerge(state.style, messageProps.style);
+                                    state.style = totalStyle;
+                                    messageProps.style = totalStyle;
+                                }
+
+                                // Filter out undefined to prevent overwriting previous values
+                                const updatedMessageProps = objectEntries(messageProps).reduce(
+                                    (acc, [key, val]) =>
+                                        typeof val === 'undefined' ? acc : Object.assign(acc, { [key]: val }),
+                                    {}
+                                );
+
+                                return updateProps(updatedMessageProps)
+                                    .then(() => globalEvent.trigger('render'))
+                                    .then(resolve);
+                            } catch (err) {
+                                // We only want console.warn to be called once
+                                // check for the known error offer_validation_error and if it’s not that error then we should throw the error again so it gets surfaced
+                                //  catch that known/safe error, but allow any other errors to escape through
+                                if (err.message === 'offer_validation_error') {
+                                    return resolve();
+                                }
+                                return reject(err);
+                            }
                         };
 
-                        // Account can be undefined in the case where the attribute observer fires and there
-                        // is no global account, such as when rendering entirely using the JavaScript API
-                        if (account) {
-                            messageProps.modal = Modal(modalProps);
+                        if (mapIndex === 0) {
+                            renderOrUpdateMessage();
+                            return;
                         }
 
-                        if (!messagesMap.has(container)) {
-                            const { render, state, updateProps, clone } = getMessageComponent()(messageProps);
-
-                            state.renderStart = renderStart;
-                            state.style = messageProps.style;
-
-                            messagesMap.set(container, { render, updateProps, state, clone });
-
-                            getAttributeObserver().observe(container, { attributes: true });
-
-                            ppDebug(
-                                `{
-                    clientID: ${account},
-                    merchantID: ${merchantId},
-                    customerID: ${customerId},
-                    offer: ${offer},
-                    currency: ${currency},
-                    ignoreCache: ${ignoreCache},
-                    channel: ${channel},
-                    ecToken: ${ecToken},
-            
-                    index: data-pp-id="${index}",
-                    style: ${JSON.stringify(style)},
-                    amount: ${amount},
-                    buyerCountry: ${buyerCountry},
-                    placement: ${placement},
-            
-                    renderStart: ${new Date(renderStart).toLocaleString()},
-                    renderMessageTime: ${new Date().toLocaleString()}
-                    }`
-                            );
-
-                            return render(container).then(() => globalEvent.trigger('render'));
+                        if (window.requestIdleCallback) {
+                            // check that requestIdleCallback is available
+                            window.requestIdleCallback(renderOrUpdateMessage);
+                        } else {
+                            // basic fallback for Safari
+                            setTimeout(renderOrUpdateMessage, mapIndex * 20);
                         }
-
-                        const { updateProps, state } = messagesMap.get(container);
-
-                        if (state.renderComplete) {
-                            state.renderStart = renderStart;
-                        }
-
-                        // Merge new styles into previous styles
-                        // Especially useful when combining inline attribute styles with JS API styles
-                        if (state.style && messageProps.style) {
-                            const totalStyle = objectMerge(state.style, messageProps.style);
-                            state.style = totalStyle;
-                            messageProps.style = totalStyle;
-                        }
-
-                        // Filter out undefined to prevent overwriting previous values
-                        const updatedMessageProps = objectEntries(messageProps).reduce(
-                            (acc, [key, val]) =>
-                                typeof val === 'undefined' ? acc : Object.assign(acc, { [key]: val }),
-                            {}
-                        );
-
-                        return updateProps(updatedMessageProps).then(() => globalEvent.trigger('render'));
-                    } catch (err) {
-                        // We only want console.warn to be called once
-                        // check for the known error offer_validation_error and if it’s not that error then we should throw the error again so it gets surfaced
-                        //  catch that known/safe error, but allow any other errors to escape through
-                        if (err.message === 'offer_validation_error') {
-                            return undefined;
-                        }
-
-                        throw err;
-                    }
+                    });
                 })
             )
         );
