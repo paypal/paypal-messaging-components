a770b6ff6bd369b04e171ee31abb7a5b41968460
Erika Tidwell <etidwell@paypal.com>
2023-08-16 16:34:19 -0400

    unit test adjustment

--- tests/unit/spec/src/components/modal/v2/lib/utils.test.js
+++ tests/unit/spec/src/components/modal/v2/lib/utils.test.js
@@ -6,13 +6,16 @@ jest.mock('src/components/modal/lib/utils', () => ({
 }));
 
 describe('Date function should return correct date format based on country', () => {
-    // /^d{1,2}\/\d{1,2}\/\d{4}$/ = regualar expression for MM/DD/YYYY format
-    // /^\d{1,2}\/\d{1,2}\/\d{4}$/ = regular expression for DD/MM/YYYY format
-    test.each([
-        ['US', /^d{1,2}\/\d{1,2}\/\d{4}$/],
-        ['AU', /^\d{1,2}\/\d{1,2}\/\d{4}$/]
-    ])('should formate the date correctly', (country, expectFormat) => {
-        const result = formatDateByCountry(country);
-        expect(result.toMatch(expectFormat));
+    it('US country date should be formatted MM/DD/YYYY', () => {
+        const result = formatDateByCountry('US');
+        const expectFormat = /^d{1,2}\/\d{1,2}\/\d{4}$/;
+        // regual expression for MM/DD/YYYY format
+        expect(result).toMatch(expectFormat);
+    });
+    it('All other countries date should be formatted DD/MM/YYYY', () => {
+        const result = formatDateByCountry('AU');
+        const expectedFormat = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+        // regular expression for DD/MM/YYYY format
+        expect(result).toMatch(expectedFormat);
     });
 });
