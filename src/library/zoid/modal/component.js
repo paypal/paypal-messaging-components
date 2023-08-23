79f99a723a17e0c4992f495904e4f9647cdc16b8
Surekha Weinberg <s.kaniget@gmail.com>
2023-08-23 11:37:29 -0700

    log a MORS event type for modals

--- src/library/zoid/modal/component.js
+++ src/library/zoid/modal/component.js
@@ -197,6 +197,14 @@ export default createGlobalVariableGetter('__paypal_credit_modal__', () =>
                             index,
                             refIndex,
                             et: 'CLIENT_IMPRESSION',
+                            event_type: 'MORS',
+                            src
+                        });
+
+                        logger.track({
+                            index,
+                            refIndex,
+                            et: 'CLIENT_IMPRESSION',
                             event_type: 'modal-open',
                             src
                         });
