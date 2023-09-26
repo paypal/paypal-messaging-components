507709e21bf2ae7020db423c5be2a4fc93eff705
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2023-09-26 10:48:36 -0400

    feat: Screen reader /mo for messages and modals (DTCRCMERC-1941) (#980)

--- src/components/modal/v2/parts/OfferCard.jsx
+++ src/components/modal/v2/parts/OfferCard.jsx
@@ -5,11 +5,13 @@ const OfferCard = ({ offer: { content, meta } }) => {
     const { termsLabel } = content;
     const aprRemoveTrailingZeros = meta?.apr.replace(/\D00$/, '');
     const aprFieldTitle = aprRemoveTrailingZeros === '0' ? termsLabel?.zeroApr : termsLabel?.nonZeroApr;
+    const offerHeaderField = termsLabel?.offerHeader;
 
     return (
         <div className="offer__container">
             <div className="offer__row">
-                <strong className="offer__field-header">{termsLabel.offerHeader}</strong>
+                {/* eslint-disable-next-line react/no-danger */}
+                <strong className="offer__field-header" dangerouslySetInnerHTML={{ __html: offerHeaderField }} />
             </div>
             <div className="offer__row">
                 <div className="offer__field-col">
