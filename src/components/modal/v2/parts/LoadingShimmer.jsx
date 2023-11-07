8793698841b403dbde1f8af2760717962f5d9290
yeul <mpscqw@gmail.com>
2023-11-07 09:34:10 -0500

    update loadingshimmer to div

--- src/components/modal/v2/parts/LoadingShimmer.jsx
+++ src/components/modal/v2/parts/LoadingShimmer.jsx
@@ -17,10 +17,10 @@ const LoadingShimmer = ({ numOffers = 3, offerCountry }) => {
                     return (
                         <div id={index} className="accordion__container shimmer">
                             <div className="accordion__row">
-                                <button className="accordion__header-container" type="button" disabled>
+                                <div className="accordion__header-container loading">
                                     <div className="offer__field-loading" style={{ width: '60%' }} />
                                     <div className="offer__field-loading" style={{ width: '30%' }} />
-                                </button>
+                                </div>
                             </div>
                         </div>
                     );
