481bfb5b833155dc6c4759a0c66cccc2294b8cfa
Surekha Weinberg <suweinberg@paypal.com>
2024-02-05 12:25:54 -0800

    remove checkout props

--- src/components/modal/v2/parts/Header.jsx
+++ src/components/modal/v2/parts/Header.jsx
@@ -12,11 +12,7 @@ const Header = ({
     qualifyingSubheadline,
     closeButtonLabel = 'Close',
     viewName,
-    useV4Design,
-    isQLDesign,
-    preapprovalHeadline,
-    preapprovalSubHeadline,
-    isPreapproved = 'false'
+    useV4Design
 }) => {
     const { country } = useServerData();
     const [, handleClose] = useTransitionState();
@@ -36,26 +32,6 @@ const Header = ({
     // Used to specifically target styles to a specific country
     const countryClassName = country?.toLowerCase();
 
-    // condition header content on design
-    const displayHeadline =
-        // eslint-disable-next-line no-nested-ternary
-        isQLDesign && headline !== 'Pay Monthly'
-            ? headline.slice(0, 8)
-            : isPreapproved
-            ? preapprovalHeadline
-            : headline;
-    const index = qualifyingSubheadline.length - 48;
-    const qlQualifyingSubheadlineTail = qualifyingSubheadline.slice(index);
-    let qlQualifyingSubheadline = qualifyingSubheadline.replace(/your purchase of /g, '').slice(0, index - 18);
-    qlQualifyingSubheadline = `${qlQualifyingSubheadline} interest-free payments ${qlQualifyingSubheadlineTail}`;
-
-    // eslint-disable-next-line no-nested-ternary
-    const displayQualifyingSubHeadline = isQLDesign
-        ? qlQualifyingSubheadline
-        : isPreapproved
-        ? preapprovalSubHeadline
-        : qualifyingSubheadline.replace(/(\s?EUR)/g, ' €');
-
     // IMPORTANT: These elements cannot be nested inside of other elements.
     // They are using very precise CSS position sticky rules that require this
     // specific adjacent DOM structure
@@ -65,91 +41,61 @@ const Header = ({
                 <div
                     className={`header__background-wrapper header__background-wrapper--gradient ${
                         useV4Design ? 'v4Design' : ''
-                    } ${isQLDesign ? 'qLDesign' : ''}`}
-                />
-            </div>
-            <div aria-hidden="true" className="header__fixed-wrapper">
-                <div
-                    className={`header__background-wrapper ${useV4Design ? 'v4Design' : ''}  ${
-                        isQLDesign ? 'qLDesign' : ''
                     }`}
                 />
             </div>
-            <div className={`header__icons ${isQLDesign ? 'qLDesign' : ''}`}>
+            <div aria-hidden="true" className="header__fixed-wrapper">
+                <div className={`header__background-wrapper ${useV4Design ? 'v4Design' : ''}`} />
+            </div>
+            <div className="header__icons">
                 <div className={`logo__wrapper ${isScrolled ? 'logo__wrapper--scroll' : ''}`}>
                     <div className="pp-logo">
-                        {/* eslint-disable-next-line no-nested-ternary */}
-                        {isQLDesign && headline !== 'Pay Monthly' ? (
-                            <Icon name="qLDesign__pi4_logo" />
-                        ) : isQLDesign ? (
-                            <Icon name="qLDesign__pmo_logo" />
-                        ) : (
-                            <Icon name={logo} />
-                        )}
+                        <Icon name={logo} />
                     </div>
                 </div>
                 {!isLander && (
                     // We don't need to render an 'x' button if the target is a lander since you will close via a
                     // merchant-provided close button from their own iframe, or by closing the window in the case of a webpage.
                     <button
-                        className={`close ${isQLDesign ? 'qLDesign' : ''}`}
+                        className="close"
                         aria-label={closeButtonLabel}
                         type="button"
                         id="close-btn"
                         aria-keyshortcuts="escape"
                         onClick={() => handleClose('Close Button')}
                     >
-                        {isQLDesign ? <Icon name="qLDesign__close" /> : <Icon name="close" />}
+                        <Icon name="close" />
                     </button>
                 )}
                 <div className="header__fixed-wrapper header__fixed-wrapper--front">
                     <div
                         className={`header__background-wrapper header__background-wrapper--sticky ${
                             useV4Design ? 'v4Design' : ''
-                        } ${isQLDesign ? 'qLDesign' : ''}`}
+                        }`}
                     />
                 </div>
             </div>
             <div className="header__content">
-                <Icon name={`${viewName}-desktop`} isQLDesign={isQLDesign} />
-                <Icon name={`${viewName}-mobile`} isQLDesign={isQLDesign} />
+                <Icon name={`${viewName}-desktop`} />
+                <Icon name={`${viewName}-mobile`} />
                 {/* <Icon name="background-pp-mobile" /> */}
-                <div className={`${isQLDesign ? 'qLDesign' : ''}`}>
-                    <h2
-                        // id used for aria-labelleby on modal container element
-                        id="header__headline"
-                        className={
-                            isPreapproved === 'true'
-                                ? `headline-${countryClassName}-preapproved`
-                                : `headline-${countryClassName}`
-                        }
-                        // eslint-disable-next-line react/no-danger
-                        dangerouslySetInnerHTML={{ __html: displayHeadline }}
-                    />
-                    {isQLDesign === true && isPreapproved === true ? (
-                        <span className="qLDesign">Pre-approved</span>
-                    ) : (
-                        ''
-                    )}
-                </div>
+                <h2
+                    // id used for aria-labelleby on modal container element
+                    id="header__headline"
+                    className={`headline-${countryClassName}`}
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{ __html: headline }}
+                />
                 {isQualifying === 'true' && qualifyingSubheadline !== '' ? (
-                    <p
-                        className={`subheadline_p subheadline-${countryClassName} qualifying  ${
-                            isQLDesign ? 'qLDesign_subheadline' : ''
-                        }`}
-                    >
-                        {/* {isPreapproved === 'true'
-                            ? preapprovalSubHeadline
-                            : qualifyingSubheadline.replace(/(\s?EUR)/g, ' €')} */}
-                        {displayQualifyingSubHeadline}
+                    <p className={`subheadline_p subheadline-${countryClassName} qualifying`}>
+                        {qualifyingSubheadline.replace(/(\s?EUR)/g, ' €')}
                     </p>
                 ) : (
                     <p
                         className={`subheadline_p subheadline-${countryClassName}`}
                         // eslint-disable-next-line react/no-danger
                         dangerouslySetInnerHTML={{
-                            __html:
-                                currencyFormat(isPreapproved === 'true' ? preapprovalSubHeadline : subheadline) ?? ''
+                            __html: currencyFormat(subheadline) ?? ''
                         }}
                     />
                 )}
