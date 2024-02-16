7b367f5bde48b88ed8d6dc2b479d7c5a9732d9b7
Surekha Weinberg <suweinberg@paypal.com>
2024-02-16 09:04:30 -0800

    add tests for de lt 0% apr account

--- tests/functional/v2/config/DE/index.js
+++ tests/functional/v2/config/DE/index.js
@@ -1,3 +1,4 @@
 export { DEV_DE_PAY_IN_1 } from './DEV_DE_PAY_IN_1';
 export { DEV_DE_LONG_TERM } from './DEV_DE_LONG_TERM';
 export { DEV_DE_MULTI } from './DEV_DE_MULTI';
+export { DEV_DE_LONG_TERM_0APR } from './DEV_DE_LONG_TERM_0APR';
