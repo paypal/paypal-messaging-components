55f25bf86fd2413184e04d842443f41c3c61d6d8
yeul <mpscqw@gmail.com>
2024-04-05 10:30:10 -0400

    ci

--- src/components/modal/v2/parts/ErrorBoundary.jsx
+++ src/components/modal/v2/parts/ErrorBoundary.jsx
@@ -1,33 +1,39 @@
-import { Component } from 'preact';
+// import { Component } from 'preact';
+import * as React from 'preact';
 // import { useXProps } from '../lib';
 
-class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    // componentDidCatch(error, errorInfo) {
-    //     this.setState();
-    //     // eslint-disable-next-line no-console
-    //     console.error('Error caught by ErrorBoundary:', error, errorInfo);
-    // }
-
-    render() {
-        // const { onError } = useXProps();
-        const {
-            // state: { hasError },
-            props: { children }
-        } = this;
-
-        // Call the onError handler provided via xprops
-        // if (hasError && onError) {
-        //     onError({
-        //         message: 'Preact modal rendering error'
-        //     });
-        // }
-        return children;
-    }
-}
-
+const ErrorBoundary = () => {
+    return <div>Test</div>;
+};
 export default ErrorBoundary;
+
+// class ErrorBoundary extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
+
+//     // componentDidCatch(error, errorInfo) {
+//     //     this.setState();
+//     //     // eslint-disable-next-line no-console
+//     //     console.error('Error caught by ErrorBoundary:', error, errorInfo);
+//     // }
+
+//     render() {
+//         // const { onError } = useXProps();
+//         const {
+//             // state: { hasError },
+//             props: { children }
+//         } = this;
+
+//         // Call the onError handler provided via xprops
+//         // if (hasError && onError) {
+//         //     onError({
+//         //         message: 'Preact modal rendering error'
+//         //     });
+//         // }
+//         return children;
+//     }
+// }
+
+// export default ErrorBoundary;
