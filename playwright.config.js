1d972d93f0f364fd976b4121871b52923cef44b6
perco12 <okeith_percy@yahoo.com>
2024-08-13 13:59:31 -0500

    updated the webserver

--- playwright.config.js
+++ playwright.config.js
@@ -24,14 +24,6 @@ module.exports = defineConfig({
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-    use: {
-        /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'https://127.0.0.1:8080',
-        ignoreHTTPSErrors: true,
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry'
-    },
-
     /* Configure projects for major browsers */
     projects: [
         {
@@ -44,43 +36,20 @@ module.exports = defineConfig({
                 }
             }
         }
-
-        // {
-        //     name: 'firefox',
-        //     use: {
-        //         ...devices['Desktop Firefox']
-        //     }
-        // },
-
-        // {
-        //     name: 'webkit',
-        //     use: { ...devices['Desktop Safari'] }
-        // },
-
-        // /* Test against mobile viewports. */
-        // {
-        //     name: 'Mobile Chrome',
-        //     use: { ...devices['Pixel 5'] }
-        // },
-        // {
-        //     name: 'Mobile Safari',
-        //     use: { ...devices['iPhone 12'] }
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //     name: 'Microsoft Edge',
-        //     use: { ...devices['Desktop Edge'], channel: 'msedge' }
-        // },
-        // {
-        //     name: 'Google Chrome',
-        //     use: { ...devices['Desktop Chrome'], channel: 'chrome' }
-        // }
     ],
-    webServer: {
-        command: 'npm run dev:ci',
-        port: 8080,
-        timeout: 120 * 1000,
-        reuseExistingServer: !process.env.CI
+    webServer: [
+        {
+            command: 'npm run dev:ci',
+            url: 'http://127.0.0.1:8080',
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI
+        }
+    ],
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL: 'https://127.0.0.1:8080',
+        ignoreHTTPSErrors: true,
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry'
     }
 });
