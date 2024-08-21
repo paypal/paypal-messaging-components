c2da4694557fcffa7dff395abd0f19628bf83941
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-08-21 12:59:43 -0400

    comment out

--- tests/playwright/practice-tests/us/setupTest.js
+++ tests/playwright/practice-tests/us/setupTest.js
@@ -1,33 +1,33 @@
-import { test as base, expect } from '@playwright/test';
+// import { test as base, expect } from '@playwright/test';
 
-const test = base.extend({
-    sdkMessage: async ({ browser }, use) => {
-        const message = async (options = {}) => {
-            // Default options
-            const { amount = 100, account = 'DEV_US_MULTI', offer = 'PAY_LATER_SHORT_TERM' } = options;
-            // Create a new browser context and page
-            const context = await browser.newContext();
-            const page = await context.newPage();
+// const test = base.extend({
+//     sdkMessage: async ({ browser }, use) => {
+//         const message = async (options = {}) => {
+//             // Default options
+//             const { amount = 100, account = 'DEV_US_MULTI', offer = 'PAY_LATER_SHORT_TERM' } = options;
+//             // Create a new browser context and page
+//             const context = await browser.newContext();
+//             const page = await context.newPage();
 
-            const url = `/snapshot/v2/sdk.html?account=${account}&amount=${amount}&offer=${offer}&env=local`;
-            await page.goto(url, { waitUntil: 'domcontentloaded' });
-            return { page, context };
-        };
-        await use(message);
-    },
-    sdkModal: async ({ browser }, use) => {
-        const modal = async (options = {}) => {
-            // Default options
-            const { amount = 100, account = 'DEV_US_MULTI', offer = 'PAY_LATER_SHORT_TERM' } = options;
-            // Create a new browser context and page
-            const context = await browser.newContext();
-            const page = await context.newPage();
+//             const url = `/snapshot/v2/sdk.html?account=${account}&amount=${amount}&offer=${offer}&env=local`;
+//             await page.goto(url, { waitUntil: 'domcontentloaded' });
+//             return { page, context };
+//         };
+//         await use(message);
+//     },
+//     sdkModal: async ({ browser }, use) => {
+//         const modal = async (options = {}) => {
+//             // Default options
+//             const { amount = 100, account = 'DEV_US_MULTI', offer = 'PAY_LATER_SHORT_TERM' } = options;
+//             // Create a new browser context and page
+//             const context = await browser.newContext();
+//             const page = await context.newPage();
 
-            await page.goto(`/snapshot/v2/standalone-modal.html?account=${account}&amount=${amount}&offer=${offer}`);
-            page.waitForLoadState('domcontentloaded');
-            return { page, context };
-        };
-        await use(modal);
-    }
-});
-export { test, expect };
+//             await page.goto(`/snapshot/v2/standalone-modal.html?account=${account}&amount=${amount}&offer=${offer}`);
+//             page.waitForLoadState('domcontentloaded');
+//             return { page, context };
+//         };
+//         await use(modal);
+//     }
+// });
+// export { test, expect };
--- tests/playwright/practice-tests/us/us_modals.spec.js
+++ tests/playwright/practice-tests/us/us_modals.spec.js
@@ -1,55 +1,55 @@
-import { AxeBuilder } from '@axe-core/playwright';
-import { test, expect } from './setupTest';
+// import { AxeBuilder } from '@axe-core/playwright';
+// import { test, expect } from './setupTest';
 
-// US test cases, TODO: grab it from the config
-const testCases = [
-    {},
-    { amount: 200, account: 'DEV_US_MULTI', offer: '' },
-    { amount: 29, account: 'DEV_US_MULTI', offer: 'PAY_LATER_SHORT_TERM' },
-    { amount: 200, account: 'DEV_US_MULTI', offer: 'PAY_LATER_SHORT_TERM' },
-    { amount: 20001, account: 'DEV_US_MULTI', offer: 'PAY_LATER_LONG_TERM' },
-    { amount: 1501, account: 'DEV_US_MULTI', offer: 'PAY_LATER_LONG_TERM' },
-    { amount: 200, account: 'DEV_US_MULTI', offer: 'PAYPAL_CREDIT_NO_INTEREST' },
+// // US test cases, TODO: grab it from the config
+// const testCases = [
+//     {},
+//     { amount: 200, account: 'DEV_US_MULTI', offer: '' },
+//     { amount: 29, account: 'DEV_US_MULTI', offer: 'PAY_LATER_SHORT_TERM' },
+//     { amount: 200, account: 'DEV_US_MULTI', offer: 'PAY_LATER_SHORT_TERM' },
+//     { amount: 20001, account: 'DEV_US_MULTI', offer: 'PAY_LATER_LONG_TERM' },
+//     { amount: 1501, account: 'DEV_US_MULTI', offer: 'PAY_LATER_LONG_TERM' },
+//     { amount: 200, account: 'DEV_US_MULTI', offer: 'PAYPAL_CREDIT_NO_INTEREST' },
 
-    { amount: 200, account: 'DEV_US_SHORT_TERM_CHECKOUT' },
-    { amount: 200, account: 'DEV_US_SHORT_TERM_PL2GO' },
-    { amount: 200, account: 'DEV_US_LONG_TERM_CHECKOUT', offer: 'PAY_LATER_LONG_TERM' },
-    { amount: 200, account: 'DEV_US_LONG_TERM_PL2GO', offer: 'PAY_LATER_LONG_TERM' }
-];
+//     { amount: 200, account: 'DEV_US_SHORT_TERM_CHECKOUT' },
+//     { amount: 200, account: 'DEV_US_SHORT_TERM_PL2GO' },
+//     { amount: 200, account: 'DEV_US_LONG_TERM_CHECKOUT', offer: 'PAY_LATER_LONG_TERM' },
+//     { amount: 200, account: 'DEV_US_LONG_TERM_PL2GO', offer: 'PAY_LATER_LONG_TERM' }
+// ];
 
-test.describe('sdk', () => {
-    test.setTimeout(120000); // Set a custom timeout of 2 minutes for this test
+// test.describe('sdk', () => {
+//     test.setTimeout(120000); // Set a custom timeout of 2 minutes for this test
 
-    test('modal should not have any automatically detectable accessibility issues', async ({ sdkModal }) => {
-        await Promise.all(
-            testCases.map(async ({ account, amount, offer }) => {
-                await test.step(`accessibility test for ${account}`, async () => {
-                    // Navigate to page
-                    const { page, context } = await sdkModal({ amount, account, offer });
+//     test('modal should not have any automatically detectable accessibility issues', async ({ sdkModal }) => {
+//         await Promise.all(
+//             testCases.map(async ({ account, amount, offer }) => {
+//                 await test.step(`accessibility test for ${account}`, async () => {
+//                     // Navigate to page
+//                     const { page, context } = await sdkModal({ amount, account, offer });
 
-                    const messageButton = await page.$('button');
-                    await messageButton.click();
+//                     const messageButton = await page.$('button');
+//                     await messageButton.click();
 
-                    // Get the iframe that contains the modal
-                    const modalIframe = await page.$('iframe[name*="__zoid__paypal_credit_modal"]');
-                    const modalFrame = await modalIframe.contentFrame();
+//                     // Get the iframe that contains the modal
+//                     const modalIframe = await page.$('iframe[name*="__zoid__paypal_credit_modal"]');
+//                     const modalFrame = await modalIframe.contentFrame();
 
-                    // Wait for the modal to become visible
-                    await modalFrame.locator('.content__wrapper').waitFor({
-                        state: 'visible'
-                    });
+//                     // Wait for the modal to become visible
+//                     await modalFrame.locator('.content__wrapper').waitFor({
+//                         state: 'visible'
+//                     });
 
-                    // TODO: 'best-practice' and 'wcag2aa' are resulting in errors, investigate
-                    const results = await new AxeBuilder({ page })
-                        .include(modalIframe)
-                        .withTags(['wcag2a', 'wcag21a', 'wcag21aa'])
-                        .analyze();
+//                     // TODO: 'best-practice' and 'wcag2aa' are resulting in errors, investigate
+//                     const results = await new AxeBuilder({ page })
+//                         .include(modalIframe)
+//                         .withTags(['wcag2a', 'wcag21a', 'wcag21aa'])
+//                         .analyze();
 
-                    expect(results.violations).toEqual([]);
-                    // Close the context after the test
-                    await context.close();
-                });
-            })
-        );
-    });
-});
+//                     expect(results.violations).toEqual([]);
+//                     // Close the context after the test
+//                     await context.close();
+//                 });
+//             })
+//         );
+//     });
+// });
