6ae27df6e65fd86d9faec2b495cd86a4fe8481c4
perco12 <okeith_percy@yahoo.com>
2024-08-21 13:39:21 -0500

    update skipped tests

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 3 : 0,
     // retries: 1,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 5 : undefined,
--- tests/playwright/practice-tests/accessibility.spec.js
+++ tests/playwright/practice-tests/accessibility.spec.js
@@ -1,43 +1,43 @@
-import { test, expect } from '@playwright/test';
-import { AxeBuilder } from '@axe-core/playwright';
+// import { test, expect } from '@playwright/test';
+// import { AxeBuilder } from '@axe-core/playwright';
 
-test.describe('smart', () => {
-    test.skip('message should not have any automatically detectable accessibility issues', async ({ page }) => {
-        // Navigate to page
-        await page.goto(`/standalone.html?account=DEV_US_MULTI&amount=200&offer=PAY_LATER_SHORT_TERM`);
-        page.waitForLoadState('domcontentloaded');
+// test.describe('smart', () => {
+//     test.skip('message should not have any automatically detectable accessibility issues', async ({ page }) => {
+//         // Navigate to page
+//         await page.goto(`/standalone.html?account=DEV_US_MULTI&amount=200&offer=PAY_LATER_SHORT_TERM`);
+//         page.waitForLoadState('domcontentloaded');
 
-        const zoidIFrame = await page.$('iframe[name*="__zoid__paypal_message__"]');
-        const messageIframe = await zoidIFrame.contentFrame();
+//         const zoidIFrame = await page.$('iframe[name*="__zoid__paypal_message__"]');
+//         const messageIframe = await zoidIFrame.contentFrame();
 
-        const button = await messageIframe.$('button');
-        // TODO: 'best-practice' and 'wcag2aa' are resulting in errors, investigate
-        const results = await new AxeBuilder({ page })
-            .include(button)
-            .withTags(['wcag2a', 'wcag21a', 'wcag21aa'])
-            .analyze();
+//         const button = await messageIframe.$('button');
+//         // TODO: 'best-practice' and 'wcag2aa' are resulting in errors, investigate
+//         const results = await new AxeBuilder({ page })
+//             .include(button)
+//             .withTags(['wcag2a', 'wcag21a', 'wcag21aa'])
+//             .analyze();
 
-        expect(results.violations).toEqual([]);
-    });
-    test.skip('modal should not have any automatically detectable accessibility issues', async ({ page }) => {
-        // Navigate to page
-        await page.goto(`/snapshot/v2/standalone-modal.html?account=DEV_US_MULTI&amount=59&offer=PAY_LATER_SHORT_TERM`);
-        page.waitForLoadState('domcontentloaded');
+//         expect(results.violations).toEqual([]);
+//     });
+//     test.skip('modal should not have any automatically detectable accessibility issues', async ({ page }) => {
+//         // Navigate to page
+//         await page.goto(`/snapshot/v2/standalone-modal.html?account=DEV_US_MULTI&amount=59&offer=PAY_LATER_SHORT_TERM`);
+//         page.waitForLoadState('domcontentloaded');
 
-        //     const messageButton = await page.$('button');
-        //     await messageButton.click();
+//         //     const messageButton = await page.$('button');
+//         //     await messageButton.click();
 
-        //     const modalIframe = await page.$('iframe[name*="__zoid__paypal_credit_modal"]');
-        //     const modalFrame = await modalIframe.contentFrame();
+//         //     const modalIframe = await page.$('iframe[name*="__zoid__paypal_credit_modal"]');
+//         //     const modalFrame = await modalIframe.contentFrame();
 
-        //     await modalFrame.locator('.content__wrapper').waitFor({
-        //         state: 'visible'
-        //     });
+//         //     await modalFrame.locator('.content__wrapper').waitFor({
+//         //         state: 'visible'
+//         //     });
 
-        //     const results = await new AxeBuilder({ page })
-        //         .include(modalIframe)
-        //         .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice'])
-        //         .analyze();
-        //     expect(results.violations).toEqual([]);
-    });
-});
+//         //     const results = await new AxeBuilder({ page })
+//         //         .include(modalIframe)
+//         //         .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice'])
+//         //         .analyze();
+//         //     expect(results.violations).toEqual([]);
+//     });
+// });
