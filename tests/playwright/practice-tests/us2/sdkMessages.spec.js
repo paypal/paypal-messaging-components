ad0f322b438df71c8251c4f24e15d3dbda645e43
perco12 <okeith_percy@yahoo.com>
2024-08-22 17:19:47 -0500

    make slight changes in the way to expect the button to be

--- tests/playwright/practice-tests/us2/sdkMessages.spec.js
+++ tests/playwright/practice-tests/us2/sdkMessages.spec.js
@@ -1,12 +1,22 @@
+import { expect } from '@playwright/test';
 import { test } from './modals_fixture';
+
+// Helper function to navigate to the URL and wait for the page to load
+export const navigatePage = async (page, account, amount, offer) => {
+    const url = `/snapshot/v2/sdk.html?account=${account}&amount=${amount}&offer=${offer}`;
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.waitForLoadState('domcontentloaded');
+};
 
 test.describe('US SDK Messages Accessibility Tests', () => {
     test('Short Term', async ({ page, runAxeCoreScan }) => {
-        await page.goto('/snapshot/v2/sdk.html?account=DEV_US_SHORT_TERM&amount=100&offer=PAY_LATER_SHORT_TERM');
-        await page.waitForLoadState('domcontentloaded');
-        const zoidIFrame = await page.waitForSelector('iframe[name*="__zoid__paypal_message__"]');
-        const messageIframe = await zoidIFrame.contentFrame();
+        navigatePage(page, 'DEV_US_MULTI', 100, 'PAY_LATER_SHORT_TERM');
+        await expect(page.frameLocator('iframe[name*="__zoid__paypal_message__"]').getByRole('button')).toBeVisible({
+            timeout: 30000
+        });
 
+        const zoidIFrame = await page.$('iframe[name*="__zoid__paypal_message__"]');
+        const messageIframe = await zoidIFrame.contentFrame();
         const button = await messageIframe.$('button');
         // Run Axe accessibility checks on the modal iframe
         await runAxeCoreScan(button);
@@ -23,18 +33,18 @@ test.describe('US SDK Messages Accessibility Tests', () => {
         // Run Axe accessibility checks on the modal iframe
         await runAxeCoreScan(button);
     });
-    // test('Short Term PL2GO', async ({ page, runAxeCoreScan }) => {
-    //     await page.goto(
-    //         '/snapshot/v2/sdk.html?account=DEV_US_SHORT_TERM_PL2GO&amount=100&offer=PAY_LATER_SHORT_TERM&env=local'
-    //     );
-    //     await page.waitForLoadState('domcontentloaded');
-    //     const zoidIFrame = await page.waitForSelector('iframe[name*="__zoid__paypal_message__"]');
-    //     const messageIframe = await zoidIFrame.contentFrame();
+    test('Short Term PL2GO', async ({ page, runAxeCoreScan }) => {
+        await page.goto(
+            '/snapshot/v2/sdk.html?account=DEV_US_SHORT_TERM_PL2GO&amount=100&offer=PAY_LATER_SHORT_TERM&env=local'
+        );
+        await page.waitForLoadState('domcontentloaded');
+        const zoidIFrame = await page.waitForSelector('iframe[name*="__zoid__paypal_message__"]');
+        const messageIframe = await zoidIFrame.contentFrame();
 
-    //     const button = await messageIframe.$('button');
-    //     // Run Axe accessibility checks on the modal iframe
-    //     await runAxeCoreScan(button);
-    // });
+        const button = await messageIframe.$('button');
+        // Run Axe accessibility checks on the modal iframe
+        await runAxeCoreScan(button);
+    });
     test('Long Term', async ({ page, runAxeCoreScan }) => {
         await page.goto(
             '/snapshot/v2/sdk.html?account=DEV_US_LONG_TERM&amount=100&offer=PAY_LATER_LONG_TERM&env=local'
@@ -47,30 +57,30 @@ test.describe('US SDK Messages Accessibility Tests', () => {
         // Run Axe accessibility checks on the modal iframe
         await runAxeCoreScan(button);
     });
-    // test('Long Term Checkout', async ({ page, runAxeCoreScan }) => {
-    //     await page.goto(
-    //         '/snapshot/v2/sdk.html?account=DEV_US_LONG_TERM_CHECKOUT&amount=100&offer=PAY_LATER_LONG_TERM&env=local'
-    //     );
-    //     await page.waitForLoadState('domcontentloaded');
-    //     const zoidIFrame = await page.waitForSelector('iframe[name*="__zoid__paypal_message__"]');
-    //     const messageIframe = await zoidIFrame.contentFrame();
+    test('Long Term Checkout', async ({ page, runAxeCoreScan }) => {
+        await page.goto(
+            '/snapshot/v2/sdk.html?account=DEV_US_LONG_TERM_CHECKOUT&amount=100&offer=PAY_LATER_LONG_TERM&env=local'
+        );
+        await page.waitForLoadState('domcontentloaded');
+        const zoidIFrame = await page.waitForSelector('iframe[name*="__zoid__paypal_message__"]');
+        const messageIframe = await zoidIFrame.contentFrame();
 
-    //     const button = await messageIframe.$('button');
-    //     // Run Axe accessibility checks on the modal iframe
-    //     await runAxeCoreScan(button);
-    // });
-    // test('Long Term PL2GO', async ({ page, runAxeCoreScan }) => {
-    //     await page.goto(
-    //         '/snapshot/v2/sdk.html?account=DEV_US_LONG_TERM_PL2GO&amount=100&offer=PAY_LATER_LONG_TERM&env=local'
-    //     );
-    //     await page.waitForLoadState('domcontentloaded');
-    //     const zoidIFrame = await page.waitForSelector('iframe[name*="__zoid__paypal_message__"]');
-    //     const messageIframe = await zoidIFrame.contentFrame();
+        const button = await messageIframe.$('button');
+        // Run Axe accessibility checks on the modal iframe
+        await runAxeCoreScan(button);
+    });
+    test('Long Term PL2GO', async ({ page, runAxeCoreScan }) => {
+        await page.goto(
+            '/snapshot/v2/sdk.html?account=DEV_US_LONG_TERM_PL2GO&amount=100&offer=PAY_LATER_LONG_TERM&env=local'
+        );
+        await page.waitForLoadState('domcontentloaded');
+        const zoidIFrame = await page.waitForSelector('iframe[name*="__zoid__paypal_message__"]');
+        const messageIframe = await zoidIFrame.contentFrame();
 
-    //     const button = await messageIframe.$('button');
-    //     // Run Axe accessibility checks on the modal iframe
-    //     await runAxeCoreScan(button);
-    // });
+        const button = await messageIframe.$('button');
+        // Run Axe accessibility checks on the modal iframe
+        await runAxeCoreScan(button);
+    });
     test('No Interest', async ({ page, runAxeCoreScan }) => {
         await page.goto('/snapshot/v2/sdk.html?account=DEV_US_NO_INTEREST&amount=100&offer=NI&env=local');
         await page.waitForLoadState('domcontentloaded');
