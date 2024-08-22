ac750d66bab9ce2d0fee613975cdebda71aed083
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-08-22 14:37:57 -0400

    config updates and yml

--- playwright.config.js
+++ playwright.config.js
@@ -88,7 +88,7 @@ module.exports = defineConfig({
         baseURL: 'https://localhost.paypal.com:8080',
         ignoreHTTPSErrors: true,
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        trace: 'on',
         bypassCSP: true
     }
 });
