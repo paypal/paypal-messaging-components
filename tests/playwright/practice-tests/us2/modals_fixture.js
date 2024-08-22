85b45e953da495fa208c239e2658996aca6c90f8
perco12 <okeith_percy@yahoo.com>
2024-08-22 17:29:49 -0500

    remove comments

--- tests/playwright/practice-tests/us2/modals_fixture.js
+++ tests/playwright/practice-tests/us2/modals_fixture.js
@@ -1,8 +1,6 @@
-// modalsFixture.js
 import { test as base, expect } from '@playwright/test';
 import { AxeBuilder } from '@axe-core/playwright';
 
-// Function to generate URL based on parameters
 export const generateUrl = ({ account, amount, offer }) => {
     let url = `/snapshot/v2/standalone-modal.html?account=${account}`;
     if (amount !== undefined) {
@@ -14,19 +12,7 @@ export const generateUrl = ({ account, amount, offer }) => {
     return url;
 };
 
-// Extend Playwright base test with custom fixtures
 export const test = base.extend({
-    // Fixture for navigating to the page
-    // navigatePage: async ({ page }, use) => {
-    //     const navigate = async ({ account, amount, offer }) => {
-    //         const url = generateUrl({ account, amount, offer });
-    //         await page.goto(url);
-    //         await page.waitForLoadState('domcontentloaded');
-    //     };
-    //     await use(navigate);
-    // },
-
-    // Fixture for loading the modal
     loadModal: async ({ page }, use) => {
         const loadModal = async () => {
             const messageButton = await page.$('button');
@@ -41,10 +27,8 @@ export const test = base.extend({
         await use(loadModal);
     },
 
-    // Fixture for running Axe accessibility checks
     runAxeCoreScan: async ({ page }, use) => {
         const runAxeCoreScan = async modalIframe => {
-            // TODO: US non qualifying & qualifying long term 'best-practice' tag error
             const results = await new AxeBuilder({ page })
                 .include(modalIframe)
                 .withTags(['wcag2a', 'wcag21a', 'wcag21aa'])
