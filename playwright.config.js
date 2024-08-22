f42ea634a218661d717bbc4a86ff2103ac29336d
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-08-22 15:40:45 -0400

    naming

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
     testDir: './tests/playwright/practice-tests',
     /* Run tests in files in parallel */
-    fullyParallel: false,
+    fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
