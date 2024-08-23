6fed01133b933bf0009cbc02062f1a5394974f4d
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-08-23 10:03:03 -0400

    test setup variation

--- tests/playwright/practice-tests/SDKModals/US/usModals.spec.js
+++ tests/playwright/practice-tests/SDKModals/US/usModals.spec.js
@@ -1,59 +1,59 @@
-import { test, navigatePage } from './setup';
+// import { test, navigatePage } from './setup';
 
-test.describe('US SDK Modals Accessibility Tests', () => {
-    test('US Modal PRODUCT LIST', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_MULTI', 200, '');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
+// test.describe('US SDK Modals Accessibility Tests', () => {
+//     test('US Modal PRODUCT LIST', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_MULTI', 200, '');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
 
-    test('US Modal SHORT TERM NQ', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_MULTI', 29, 'PAY_LATER_SHORT_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
+//     test('US Modal SHORT TERM NQ', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_MULTI', 29, 'PAY_LATER_SHORT_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
 
-    test('US Modal SHORT TERM Q', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_MULTI', 200, 'PAY_LATER_SHORT_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
+//     test('US Modal SHORT TERM Q', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_MULTI', 200, 'PAY_LATER_SHORT_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
 
-    test('US Modal LONG TERM NQ', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_MULTI', 20001, 'PAY_LATER_LONG_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
+//     test('US Modal LONG TERM NQ', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_MULTI', 20001, 'PAY_LATER_LONG_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
 
-    test('US Modal LONG TERM Q', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_MULTI', 1501, 'PAY_LATER_LONG_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
-    test('US Modal NO INTEREST', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_MULTI', 200, 'PAYPAL_CREDIT_NO_INTEREST');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
-    // TODO: check inputs for checkout and pl2go
-    test('US Modal SHORT TERM CHECKOUT', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_SHORT_TERM_CHECKOUT', 200, 'PAY_LATER_SHORT_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
-    test('US Modal LONG TERM CHECKOUT', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_LONG_TERM_CHECKOUT', 200, 'PAY_LATER_LONG_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
-    test('US Modal SHORT TERM PL2GO', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_SHORT_TERM_PL2GO', 200, 'PAY_LATER_SHORT_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
-    test('US Modal LONG TERM PL2GO', async ({ page, loadModal, runAxeCoreScan }) => {
-        await navigatePage(page, 'DEV_US_LONG_TERM_PL2GO', 200, 'PAY_LATER_LONG_TERM');
-        const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe);
-    });
-});
+//     test('US Modal LONG TERM Q', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_MULTI', 1501, 'PAY_LATER_LONG_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
+//     test('US Modal NO INTEREST', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_MULTI', 200, 'PAYPAL_CREDIT_NO_INTEREST');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
+//     // TODO: check inputs for checkout and pl2go
+//     test('US Modal SHORT TERM CHECKOUT', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_SHORT_TERM_CHECKOUT', 200, 'PAY_LATER_SHORT_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
+//     test('US Modal LONG TERM CHECKOUT', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_LONG_TERM_CHECKOUT', 200, 'PAY_LATER_LONG_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
+//     test('US Modal SHORT TERM PL2GO', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_SHORT_TERM_PL2GO', 200, 'PAY_LATER_SHORT_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
+//     test('US Modal LONG TERM PL2GO', async ({ page, loadModal, runAxeCoreScan }) => {
+//         await navigatePage(page, 'DEV_US_LONG_TERM_PL2GO', 200, 'PAY_LATER_LONG_TERM');
+//         const modalIframe = await loadModal();
+//         await runAxeCoreScan(modalIframe);
+//     });
+// });
---
+++ tests/playwright/practice-tests/usTest/setup.js
@@ -0,0 +1,35 @@
+// modalsFixture.js
+import { test as base, expect } from '@playwright/test';
+import { AxeBuilder } from '@axe-core/playwright';
+
+
+// Extend Playwright base test with custom fixtures
+export const test = base.extend({
+    // Fixture for loading the modal
+    loadModal: async ({ page }, use) => {
+        const loadModal = async () => {
+            const messageButton = await page.$('button');
+            await messageButton.click();
+            const modalIframe = await page.waitForSelector('iframe[name*="__zoid__paypal_credit_modal"]');
+            const modalFrame = await modalIframe.contentFrame();
+            await modalFrame.locator('.content__wrapper').waitFor({
+                state: 'visible'
+            });
+            return modalIframe;
+        };
+        await use(loadModal);
+    },
+
+    // Fixture for running Axe accessibility checks
+    runAxeCoreScan: async ({ page }, use) => {
+        const runAxeCoreScan = async modalIframe => {
+            // TODO: US non qualifying & qualifying long term 'best-practice' tag error
+            const results = await new AxeBuilder({ page })
+                .include(modalIframe)
+                .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+                .analyze();
+            expect(results.violations).toEqual([]);
+        };
+        await use(runAxeCoreScan);
+    }
+});
