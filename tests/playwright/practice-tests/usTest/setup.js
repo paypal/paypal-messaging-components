a90ef5eef66143aa567104cfe987548c87ee6de6
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-08-23 10:47:37 -0400

    variation

--- tests/playwright/practice-tests/usTest/setup.js
+++ tests/playwright/practice-tests/usTest/setup.js
@@ -2,6 +2,18 @@
 import { test as base, expect } from '@playwright/test';
 import { AxeBuilder } from '@axe-core/playwright';
 
+// Combined function to generate URL and navigate
+const navigatePage = async (page, { account, amount, offer }) => {
+    let url = `/snapshot/v2/standalone-modal.html?account=${account}`;
+    if (amount !== undefined) {
+        url += `&amount=${amount}`;
+    }
+    if (offer !== undefined && offer !== '') {
+        url += `&offer=${offer}`;
+    }
+    await page.goto(url);
+    await page.waitForLoadState('domcontentloaded');
+};
 
 // Extend Playwright base test with custom fixtures
 export const test = base.extend({
@@ -12,9 +24,7 @@ export const test = base.extend({
             await messageButton.click();
             const modalIframe = await page.waitForSelector('iframe[name*="__zoid__paypal_credit_modal"]');
             const modalFrame = await modalIframe.contentFrame();
-            await modalFrame.locator('.content__wrapper').waitFor({
-                state: 'visible'
-            });
+            await modalFrame.locator('.content__wrapper').waitFor({ state: 'visible' });
             return modalIframe;
         };
         await use(loadModal);
@@ -22,8 +32,7 @@ export const test = base.extend({
 
     // Fixture for running Axe accessibility checks
     runAxeCoreScan: async ({ page }, use) => {
-        const runAxeCoreScan = async modalIframe => {
-            // TODO: US non qualifying & qualifying long term 'best-practice' tag error
+        const runAxeCoreScan = async (modalIframe) => {
             const results = await new AxeBuilder({ page })
                 .include(modalIframe)
                 .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
@@ -31,5 +40,14 @@ export const test = base.extend({
             expect(results.violations).toEqual([]);
         };
         await use(runAxeCoreScan);
+    },
+
+    // Fixture for navigating the page
+    navigatePage: async ({ page }, use) => {
+        await use(async (account, amount, offer) => {
+            await navigatePage(page, { account, amount, offer });
+        });
     }
 });
+
+export { expect };
--- tests/playwright/practice-tests/usTest/test.spec.js
+++ tests/playwright/practice-tests/usTest/test.spec.js
@@ -1,80 +1,18 @@
 import { test } from './setup';
-import { AxeBuilder } from '@axe-core/playwright';
-
-// Function to generate URL based on parameters
-const generateUrl = ({ account, amount, offer }) => {
-    let url = `/snapshot/v2/standalone-modal.html?account=${account}`;
-    if (amount !== undefined) {
-        url += `&amount=${amount}`;
-    }
-    if (offer !== undefined && offer !== '') {
-        url += `&offer=${offer}`;
-    }
-    return url;
-};
-// Helper function to navigate to the URL and wait for the page to load
-const navigatePage = async (page, account, amount, offer) => {
-    const url = generateUrl({ account, amount, offer });
-    await page.goto(url);
-    await page.waitForLoadState('domcontentloaded');
-};
 
 test.describe('US SDK Modals Accessibility Tests', () => {
-    test.describe('US SDK Modals Accessibility Tests', () => {
-        test('US Modal PRODUCT LIST', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_MULTI', 200, '');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-
-        test('US Modal SHORT TERM NQ', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_MULTI', 29, 'PAY_LATER_SHORT_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-
-        test('US Modal SHORT TERM Q', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_MULTI', 200, 'PAY_LATER_SHORT_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-
-        test('US Modal LONG TERM NQ', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_MULTI', 20001, 'PAY_LATER_LONG_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-
-        test('US Modal LONG TERM Q', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_MULTI', 1501, 'PAY_LATER_LONG_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-        test('US Modal NO INTEREST', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_MULTI', 200, 'PAYPAL_CREDIT_NO_INTEREST');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-        // TODO: check inputs for checkout and pl2go
-        test('US Modal SHORT TERM CHECKOUT', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_SHORT_TERM_CHECKOUT', 200, 'PAY_LATER_SHORT_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-        test('US Modal LONG TERM CHECKOUT', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_LONG_TERM_CHECKOUT', 200, 'PAY_LATER_LONG_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-        test('US Modal SHORT TERM PL2GO', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_SHORT_TERM_PL2GO', 200, 'PAY_LATER_SHORT_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
-        test('US Modal LONG TERM PL2GO', async ({ page, loadModal, runAxeCoreScan }) => {
-            await navigatePage(page, 'DEV_US_LONG_TERM_PL2GO', 200, 'PAY_LATER_LONG_TERM');
-            const modalIframe = await loadModal();
-            await runAxeCoreScan(modalIframe);
-        });
+    test('US Modal PRODUCT LIST', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+        // Use navigatePage fixture to go to the appropriate URL
+        await navigatePage('DEV_US_MULTI', 200, '');
+        const modalIframe = await loadModal();
+        await runAxeCoreScan(modalIframe);
     });
+
+    test('US Modal SHORT TERM NQ', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+        await navigatePage('DEV_US_MULTI', 29, 'PAY_LATER_SHORT_TERM');
+        const modalIframe = await loadModal();
+        await runAxeCoreScan(modalIframe);
+    });
+
+    // Add more test cases as needed
 });
