cbaa42497a513ed2453b30ca75114a00b8b89a8b
perco12 <okeith_percy@yahoo.com>
2024-08-23 19:08:33 -0500

    debug yml

--- playwright.config.js
+++ playwright.config.js
@@ -93,7 +93,7 @@ module.exports = defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
         bypassCSP: true,
-        headless: true,
+        headless: !!process.env.CI || undefined,
         video: 'on'
     }
 });
