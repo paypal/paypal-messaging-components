b4b97f47078ec0a8f41ea82806fee080d4594a21
perco12 <okeith_percy@yahoo.com>
2024-08-26 10:32:48 -0500

    update test time

--- playwright.config.js
+++ playwright.config.js
@@ -12,18 +12,16 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
     testDir: './tests/playwright/tests',
-    // /* Run tests in files in parallel */
-    // fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    // retries: 1,
+
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 40 : undefined,
+    workers: 40,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    timeout: 5 * 60000,
     /* Configure projects for major browsers */
     projects: [
         {
