d04a8e6fc5c52ffaa073cd6030b790bdae1fb44c
perco12 <okeith_percy@yahoo.com>
2024-08-26 14:07:14 -0500

    update amount of workers

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    workers: 40,
+    workers: 80,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     timeout: 5 * 60000,
