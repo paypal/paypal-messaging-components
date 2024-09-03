bb029a6b211e157af47ceb9de371a8a94c71832d
perco12 <okeith_percy@yahoo.com>
2024-09-03 16:31:56 -0500

    add DE messages

--- tests/playwright/tests/sdk/messages/longTerm.spec.js
+++ tests/playwright/tests/sdk/messages/longTerm.spec.js
@@ -1,18 +1,28 @@
 import { messageTest } from '../../../pages/messages_fixture';
 
 messageTest.describe('Long Term', () => {
-    messageTest('US Message long term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US Message long term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_LONG_TERM', amount: 29, offer: 'PAY_LATER_LONG_TERM' });
         const messageIFrame = await loadMessage();
         await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
-    messageTest('US Message long term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US Message long term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_LONG_TERM', amount: 100, offer: 'PAY_LATER_LONG_TERM' });
         const messageIFrame = await loadMessage();
         await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
-    messageTest('US Message long term checkout', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US Message long term checkout', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_LONG_TERM_CHECKOUT', amount: 100, offer: 'PAY_LATER_LONG_TERM' });
+        const messageIFrame = await loadMessage();
+        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+    });
+    messageTest('@DE Message long term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_DE_LONG_TERM', amount: 100, offer: 'PAY_LATER_LONG_TERM' });
+        const messageIFrame = await loadMessage();
+        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+    });
+    messageTest('@DE Message long term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_DE_LONG_TERM', amount: 29, offer: 'PAY_LATER_LONG_TERM' });
         const messageIFrame = await loadMessage();
         await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
--- tests/playwright/tests/sdk/messages/multi.spec.js
+++ tests/playwright/tests/sdk/messages/multi.spec.js
@@ -1,32 +1,54 @@
 import { messageTest } from '../../../pages/messages_fixture';
 
 messageTest.describe('Multi Messages', () => {
-    messageTest('US long term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US long term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_MULTI', amount: 20001, offer: 'PAY_LATER_LONG_TERM' });
         const messageIframe = await loadMessage();
         await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
 
-    messageTest('US long term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US long term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_MULTI', amount: 1501, offer: 'PAY_LATER_LONG_TERM' });
         const messageIframe = await loadMessage();
         await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
 
-    messageTest('US product list', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US product list', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_MULTI', amount: 200, offer: '' });
         const messageIframe = await loadMessage();
         await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
 
-    messageTest('US short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_MULTI', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIframe = await loadMessage();
         await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
 
-    messageTest('US short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('@US short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_MULTI', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
+        const messageIframe = await loadMessage();
+        await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+    });
+    // DE
+    messageTest('@DE multi qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_DE_MULTI', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
+        const messageIframe = await loadMessage();
+        await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+    });
+
+    messageTest('@DE multi non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_DE_MULTI', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
+        const messageIframe = await loadMessage();
+        await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+    });
+    messageTest('@DE pay in 1 qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_DE_MULTI', amount: 200, offer: 'PAY_LATER_PAY_IN_1' });
+        const messageIframe = await loadMessage();
+        await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+    });
+    messageTest('@DE pay in 1 non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_DE_MULTI', amount: 1001.1, offer: 'PAY_LATER_PAY_IN_1' });
         const messageIframe = await loadMessage();
         await runAxeCoreScan(messageIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
     });
