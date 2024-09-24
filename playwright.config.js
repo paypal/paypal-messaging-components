bdbf31f7aaa01257ba87cef7b9159d0d8ea8232c
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-09-24 11:52:06 -0400

    comma missint

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig({
     retries: process.env.CI ? 2 : 0,
     workers: 80,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['line'],['html']]
+    reporter: [['line'],['html']],
     timeout: 5 * 60000,
     /* Configure projects for major browsers */
     projects: [
