42d88b714e76b6eda1a81f887ed69e6f1887b987
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-09-25 16:17:35 -0400

    feat: Accessibility Automated failing tests fix (DTCRCMERC-3548) (#1133)
    * h4 fix
    
    * revert
    
    * test reporter
    
    * adjust for failing message tests
    
    * remove env
    
    * remove extra yml
    
    * lint
    
    * lint
    
    * refactor
    
    * force failing test
    
    * line and html reporter
    
    * comma missint
    
    * lint
    
    * remove line
    
    * rule and yml update
    
    * demo page colors
    
    * fixes
    
    * adjustment
    
    * folder adjustment

--- tests/playwright/tests/sdk/messages/shortTerm.spec.js
+++ tests/playwright/tests/sdk/messages/shortTerm.spec.js
@@ -1,93 +1,96 @@
 import { messageTest } from '../../../pages/messages_fixture';
 
 messageTest.describe('@AU Short Term', () => {
-    messageTest('AU Message short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('AU Message short term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_AU_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('AU Message short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('AU Message short term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_AU_SHORT_TERM', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
 });
 messageTest.describe('@ES Short Term', () => {
-    messageTest('ES Message short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('ES Message short term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_ES_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('ES Message short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('ES Message short term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_ES_SHORT_TERM', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
 });
 messageTest.describe('@FR Short Term', () => {
-    messageTest('FR Message short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('FR Message short term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_FR_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('FR Message short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('FR Message short term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_FR_SHORT_TERM', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
 });
 messageTest.describe('@GB Short Term', () => {
-    messageTest('GB Message short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('GB Message short term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_GB_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('GB Message short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('GB Message short term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_GB_SHORT_TERM', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('GB Message short term checkout qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_GB_SHORT_TERM_CHECKOUT', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
-        const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
-    });
+    messageTest(
+        'GB Message short term checkout qualifying',
+        async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+            await navigatePage({ account: 'DEV_GB_SHORT_TERM_CHECKOUT', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
+            const messageIFrame = await loadMessage();
+            await messageAxeCoreScan(messageIFrame);
+        }
+    );
 
     messageTest(
         'GB Message short term checkout non qualifying',
-        async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+        async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
             await navigatePage({ account: 'DEV_GB_SHORT_TERM_CHECKOUT', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
             const messageIFrame = await loadMessage();
-            await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+            await messageAxeCoreScan(messageIFrame);
         }
     );
 });
 messageTest.describe('@IT Short Term', () => {
-    messageTest('IT Message short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('IT Message short term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_IT_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('IT Message short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('IT Message short term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_IT_SHORT_TERM', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
 });
 messageTest.describe('@US Short Term', () => {
-    messageTest('US Message short term non qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('US Message short term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('US Message short term qualifying', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('US Message short term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_SHORT_TERM', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
-    messageTest('US Message short term checkout', async ({ navigatePage, loadMessage, runAxeCoreScan }) => {
+    messageTest('US Message short term checkout', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_SHORT_TERM_CHECKOUT', amount: 100, offer: 'PAY_LATER_SHORT_TERM' });
         const messageIFrame = await loadMessage();
-        await runAxeCoreScan(messageIFrame, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await messageAxeCoreScan(messageIFrame);
     });
 });
--- tests/playwright/tests/sdk/modals/longTerm.spec.js
+++ tests/playwright/tests/sdk/modals/longTerm.spec.js
@@ -1,41 +1,41 @@
 import { modalTest } from '../../../pages/modals_fixture';
 
 modalTest.describe('@US Long Term Modals', () => {
-    modalTest('US Modal long term non qualifying', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+    modalTest('US Modal long term non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_LONG_TERM', amount: 20001, offer: 'PAY_LATER_LONG_TERM' });
         const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await modalAxeCoreScan(modalIframe);
     });
 
-    modalTest('US Modal long term qualifying', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+    modalTest('US Modal long term qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_LONG_TERM', amount: 1501, offer: 'PAY_LATER_LONG_TERM' });
         const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await modalAxeCoreScan(modalIframe);
     });
-    modalTest('US Modal long term checkout', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+    modalTest('US Modal long term checkout', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({
             account: 'DEV_US_LONG_TERM_CHECKOUT',
             amount: 200,
             offer: 'PAY_LATER_LONG_TERM'
         });
         const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await modalAxeCoreScan(modalIframe);
     });
-    modalTest('US Modal long term pl2go', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+    modalTest('US Modal long term pl2go', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_LONG_TERM_PL2GO', amount: 200, offer: 'PAY_LATER_LONG_TERM' });
         const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await modalAxeCoreScan(modalIframe);
     });
 });
 modalTest.describe('@DE Long Term Modals', () => {
-    modalTest('DE Modal LONG TERM Q', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+    modalTest('DE Modal LONG TERM Q', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_DE_LONG_TERM', amount: 500, offer: 'PAY_LATER_LONG_TERM' });
         const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await modalAxeCoreScan(modalIframe);
     });
-    modalTest('DE Modal LONG TERM NQ', async ({ navigatePage, loadModal, runAxeCoreScan }) => {
+    modalTest('DE Modal LONG TERM NQ', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_DE_LONG_TERM', amount: 50, offer: 'PAY_LATER_LONG_TERM' });
         const modalIframe = await loadModal();
-        await runAxeCoreScan(modalIframe, ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
+        await modalAxeCoreScan(modalIframe);
     });
 });
