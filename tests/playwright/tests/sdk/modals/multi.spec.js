ce17377740d5efd53d47d6728fafec6cc940cba9
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-09-30 10:41:26 -0400

    condense shorterm and multi

--- tests/playwright/tests/sdk/modals/multi.spec.js
+++ tests/playwright/tests/sdk/modals/multi.spec.js
@@ -1,42 +1,41 @@
 import { modalTest } from '../../../pages/modals_fixture';
 
 modalTest.describe('@US Multi Modals', () => {
-    modalTest('US Modal long term non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 20001, offer: 'PAY_LATER_LONG_TERM' });
-        const modalIframe = await loadModal();
-        await modalAxeCoreScan(modalIframe);
-    });
-
-    modalTest('US Modal long term qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+    modalTest('US Modal long term Multi Q', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_MULTI', amount: 1501, offer: 'PAY_LATER_LONG_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-
-    modalTest('US Modal product list', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 200, offer: '' });
+    modalTest('US Modal long term Multi NQ', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_US_MULTI', amount: 20001, offer: 'PAY_LATER_LONG_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-
-    modalTest('US Modal short term non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
-        const modalIframe = await loadModal();
-        await modalAxeCoreScan(modalIframe);
-    });
-
-    modalTest('US Modal short term qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+    modalTest('US Modal short term Multi Q', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_MULTI', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-});
-modalTest.describe('@DE Multi Modals', () => {
-    modalTest('DE Modal PRODUCT LIST', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_DE_MULTI', amount: 200, offer: '' });
+    modalTest('US Modal short term Multi NQ', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_US_MULTI', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
+    modalTest('US Modal No Interest Credit', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_US_MULTI', amount: 29, offer: 'PAYPAL_CREDIT_NO_INTEREST' });
+        const modalIframe = await loadModal();
+        await modalAxeCoreScan(modalIframe);
+    });
+});
+modalTest.describe('@US/DE Product List Modals', () => {
+    modalTest('US, DE Modal Product List', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_US_MULTI', amount: 200, offer: '' });
+        const modalIframe = await loadModal();
+        await modalAxeCoreScan(modalIframe);
+    });
+});
+
+modalTest.describe('@DE Multi Modals', () => {
     modalTest('DE Modal PAY IN ONE', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_DE_MULTI', amount: 200, offer: 'PAY_LATER_PAY_IN_1' });
         const modalIframe = await loadModal();
--- tests/playwright/tests/sdk/modals/shortTerm.spec.js
+++ tests/playwright/tests/sdk/modals/shortTerm.spec.js
@@ -1,80 +1,49 @@
 import { modalTest } from '../../../pages/modals_fixture';
 
-modalTest.describe('@US Short Term', () => {
-    modalTest('US Modal short term non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
-        const modalIframe = await loadModal();
-        await modalAxeCoreScan(modalIframe);
-    });
-
-    modalTest('US Modal short term qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_SHORT_TERM', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
-        const modalIframe = await loadModal();
-        await modalAxeCoreScan(modalIframe);
-    });
-
-    modalTest('US Modal short term checkout', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+modalTest.describe('@US-Checkout Short Term Checkout', () => {
+    modalTest('US Modal short term checkout Q', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_US_SHORT_TERM_CHECKOUT', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-});
-modalTest.describe('@AU Short Term', () => {
-    modalTest('AU Modal qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_AU_SHORT_TERM', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
+    modalTest('US Modal short term checkout NQ', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_US_SHORT_TERM_CHECKOUT', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-    modalTest('AU Modal non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_AU_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
+    modalTest('GB Modal short term checkout Q', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_GB_SHORT_TERM_CHECKOUT', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-});
-modalTest.describe('@ES Short Term', () => {
-    modalTest('ES Modal qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_ES_SHORT_TERM', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
-        const modalIframe = await loadModal();
-        await modalAxeCoreScan(modalIframe);
-    });
-    modalTest('ES Modal non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_ES_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
-        const modalIframe = await loadModal();
-        await modalAxeCoreScan(modalIframe);
-    });
-});
-modalTest.describe('@FR Short Term', () => {
-    modalTest('FR Modal qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_FR_SHORT_TERM', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
-        const modalIframe = await loadModal();
-        await modalAxeCoreScan(modalIframe);
-    });
-    modalTest('FR Modal non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_FR_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
+    modalTest('GB Modal short term checkout NQ', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_GB_SHORT_TERM_CHECKOUT', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
 });
 modalTest.describe('@GB Short Term', () => {
-    modalTest('GB Modal qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+    modalTest('GB  Modal short term Q', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_GB_SHORT_TERM', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-    modalTest('GB Modal non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+    modalTest('GB  Modal short term NQ', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
         await navigatePage({ account: 'DEV_GB_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
 });
-modalTest.describe('@IT Short Term', () => {
-    modalTest('IT Modal qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_IT_SHORT_TERM', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
+
+modalTest.describe('@US/FR/IT/ES/AU Short Term', () => {
+    modalTest('US, FR, IT, ES, AU short term Q', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_FR_SHORT_TERM', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
-    modalTest('IT Modal non qualifying', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_IT_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
+
+    modalTest('US, FR, IT, ES, AU short term NQ', async ({ navigatePage, loadModal, modalAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV_FR_SHORT_TERM', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
         const modalIframe = await loadModal();
         await modalAxeCoreScan(modalIframe);
     });
