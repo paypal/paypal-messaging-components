99d89c82d81af13808a84ff0dbc1a9d820f8acb8
opercy <opercy@paypal.com>
2024-10-01 11:52:19 -0500

    update names

--- tests/playwright/tests/sdk/messages/multi.spec.js
+++ tests/playwright/tests/sdk/messages/multi.spec.js
@@ -6,36 +6,36 @@ messageTest.describe('Multi Messages', () => {
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
-    messageTest('No Interest', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+    messageTest('NI', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV0000000NIQ' });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
-    messageTest('No Interest US only', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+    messageTest('NI US only', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV000NINONUS' });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('Pay Monthly Credit', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+    messageTest('Credit', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV0000000PMG', amount: 1501 });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('Short Term Qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+    messageTest('Short Term Q', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV000000GPLQ', amount: 200 });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('Short Term Non Qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+    messageTest('Short Term NQ', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV00000GPLNQ' });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('Long Term Qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+    messageTest('Long Term Q', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
         await navigatePage({ account: 'DEV00USLTMQGZ', amount: 200 });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
