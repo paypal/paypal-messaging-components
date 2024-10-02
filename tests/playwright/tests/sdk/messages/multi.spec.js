a29c55080547a173532f6025ea9177da4fef6760
perco12 <141269770+perco12@users.noreply.github.com>
2024-10-02 09:46:09 -0500

    refactor: Condense Messages to a Few for Accessibility Automated Testing  (#1138)
    * condense messages to a few
    
    * Run Message Accessibility Test Only
    
    * update the url
    
    * update names
    
    * test all playwright tests

--- tests/playwright/tests/sdk/messages/multi.spec.js
+++ tests/playwright/tests/sdk/messages/multi.spec.js
@@ -1,56 +1,47 @@
 import { messageTest } from '../../../pages/messages_fixture';
 
-messageTest.describe('@US Multi Messages', () => {
-    messageTest('US long term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 20001, offer: 'PAY_LATER_LONG_TERM' });
+messageTest.describe('Multi Messages', () => {
+    messageTest('Generic', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV0USGENERIC', url: '/accessibility/colorsdk.html' });
+        const messageIframe = await loadMessage();
+        await messageAxeCoreScan(messageIframe);
+    });
+    messageTest('NI', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV0000000NIQ' });
+        const messageIframe = await loadMessage();
+        await messageAxeCoreScan(messageIframe);
+    });
+    messageTest('NI US only', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV000NINONUS' });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('US long term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 1501, offer: 'PAY_LATER_LONG_TERM' });
+    messageTest('Credit', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV0000000PMG', amount: 1501 });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('US product list', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 200, offer: '' });
+    messageTest('Short Term Q', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV000000GPLQ', amount: 200 });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('US short term non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
+    messageTest('Short Term NQ', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV00000GPLNQ' });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
 
-    messageTest('US short term qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_US_MULTI', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
+    messageTest('Long Term Q', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV00USLTMQGZ', amount: 200 });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
-});
-
-messageTest.describe('@DE Multi Messages', () => {
-    messageTest('DE multi qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_DE_MULTI', amount: 200, offer: 'PAY_LATER_SHORT_TERM' });
-        const messageIframe = await loadMessage();
-        await messageAxeCoreScan(messageIframe);
-    });
-
-    messageTest('DE multi non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_DE_MULTI', amount: 29, offer: 'PAY_LATER_SHORT_TERM' });
-        const messageIframe = await loadMessage();
-        await messageAxeCoreScan(messageIframe);
-    });
-    messageTest('DE pay in 1 qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_DE_PAY_IN_1', amount: 200, offer: 'PAY_LATER_PAY_IN_1' });
-        const messageIframe = await loadMessage();
-        await messageAxeCoreScan(messageIframe);
-    });
-    messageTest('DE pay in 1 non qualifying', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
-        await navigatePage({ account: 'DEV_DE_PAY_IN_1', amount: 1001.1, offer: 'PAY_LATER_PAY_IN_1' });
+    messageTest('Buttons Message', async ({ navigatePage, loadMessage, messageAxeCoreScan }) => {
+        await navigatePage({ account: 'DEV0GENERICPL' });
         const messageIframe = await loadMessage();
         await messageAxeCoreScan(messageIframe);
     });
