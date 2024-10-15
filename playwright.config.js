85f6e59a425182be4ab12076869fceda5c299ab3
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-10-15 10:08:23 -0400

    add back in

--- playwright.config.js
+++ playwright.config.js
@@ -28,19 +28,6 @@ module.exports = defineConfig({
                 ...devices['Desktop Chrome']
             }
         },
-        // {
-        //     name: 'firefox',
-        //     use: {
-        //         ...devices['Desktop Firefox']
-        //     }
-        // },
-
-        // {
-        //     name: 'webkit',
-        //     use: {
-        //         ...devices['Desktop Safari']
-        //     }
-        // },
         /* Test against mobile viewports. */
         {
             name: 'Mobile Chrome',
@@ -48,22 +35,6 @@ module.exports = defineConfig({
                 ...devices['Pixel 5']
             }
         },
-
-        // {
-        //     name: 'Mobile Safari',
-        //     use: {
-        //         ...devices['iPhone 12']
-        //     }
-        // },
-
-        // /* Test against branded browsers. */
-        // {
-        //     name: 'Microsoft Edge',
-        //     use: {
-        //         ...devices['Desktop Edge'],
-        //         channel: 'msedge'
-        //     }
-        // },
         {
             name: 'Google Chrome',
             use: {
