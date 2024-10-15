8727dc385722cc55cf196c1dc3e51968caf9e295
Erika Colette - NYC (she/her) <72584889+erikacolette29@users.noreply.github.com>
2024-10-15 09:59:06 -0400

    tet snapshots add back in broswers

--- playwright.config.js
+++ playwright.config.js
@@ -28,13 +28,42 @@ module.exports = defineConfig({
                 ...devices['Desktop Chrome']
             }
         },
-        /* mobile viewports. */
+        // {
+        //     name: 'firefox',
+        //     use: {
+        //         ...devices['Desktop Firefox']
+        //     }
+        // },
+
+        // {
+        //     name: 'webkit',
+        //     use: {
+        //         ...devices['Desktop Safari']
+        //     }
+        // },
+        /* Test against mobile viewports. */
         {
             name: 'Mobile Chrome',
             use: {
                 ...devices['Pixel 5']
             }
         },
+
+        // {
+        //     name: 'Mobile Safari',
+        //     use: {
+        //         ...devices['iPhone 12']
+        //     }
+        // },
+
+        // /* Test against branded browsers. */
+        // {
+        //     name: 'Microsoft Edge',
+        //     use: {
+        //         ...devices['Desktop Edge'],
+        //         channel: 'msedge'
+        //     }
+        // },
         {
             name: 'Google Chrome',
             use: {
