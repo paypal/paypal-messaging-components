95ed87c17016d6ace42f2bf9ec4d058892b94177
Anushree Shah <anushah@paypal.com>
2026-01-27 06:35:27 -0800

    refactor: UK Pi3 Preapproval ModXO changes (#1259)

--- src/components/modal/v2/parts/views/ShortTerm/Content.jsx
+++ src/components/modal/v2/parts/views/ShortTerm/Content.jsx
@@ -38,6 +38,7 @@ export const ShortTerm = ({
 
     const preapprovalDisclaimerHeadline = preapproval?.preapprovalDisclaimerHeadline;
     const preapprovalDisclaimerBody = preapproval?.preapprovalDisclaimerBody;
+    const preapprovalPrivacyDisclaimer = preapproval?.preapprovalPrivacyDisclaimer;
     const countryClassName = country?.toLowerCase();
 
     const renderCheckoutCtaButton = () => {
@@ -122,30 +123,23 @@ export const ShortTerm = ({
                         <div className={`content__row donuts ${useNewCheckoutDesign === 'true' ? 'checkout' : ''}`}>
                             <div className="donuts__container">
                                 {elements.map((installment, index) => (
-                                    <Fragment>
-                                        <Donut
-                                            key={index}
-                                            useV4Design={useV4Design}
-                                            useV5Design={useV5Design}
-                                            use5Dot1Design={use5Dot1Design}
-                                            useNewCheckoutDesign={useNewCheckoutDesign}
-                                            qualifying={qualifying}
-                                            // regex replaces EUR with the euro symbol €
-                                            periodicPayment={
-                                                installment?.total_payment
-                                                    ? installment.total_payment.replace(/(\s?EUR)/g, ' €')
-                                                    : localeFormattedPayment
-                                            }
-                                            currentNum={index + 1}
-                                            timeStamp={installment?.payment_date ?? donutTimestamps[index]}
-                                            numOfPayments={elements.length}
-                                        />
-                                        {country === 'GB' &&
-                                            useV5Design === 'true' &&
-                                            useNewCheckoutDesign === 'true' && (
-                                                <span className={`dashed-line-${index}`} />
-                                            )}
-                                    </Fragment>
+                                    <Donut
+                                        key={index}
+                                        useV4Design={useV4Design}
+                                        useV5Design={useV5Design}
+                                        use5Dot1Design={use5Dot1Design}
+                                        useNewCheckoutDesign={useNewCheckoutDesign}
+                                        qualifying={qualifying}
+                                        // regex replaces EUR with the euro symbol €
+                                        periodicPayment={
+                                            installment?.total_payment
+                                                ? installment.total_payment.replace(/(\s?EUR)/g, ' €')
+                                                : localeFormattedPayment
+                                        }
+                                        currentNum={index + 1}
+                                        timeStamp={installment?.payment_date ?? donutTimestamps[index]}
+                                        numOfPayments={elements.length}
+                                    />
                                 ))}
                             </div>
                         </div>
@@ -153,6 +147,7 @@ export const ShortTerm = ({
                             <PreapprovalDisclaimer
                                 preapprovalDisclaimerBody={preapprovalDisclaimerBody}
                                 preapprovalDisclaimerHeadline={preapprovalDisclaimerHeadline}
+                                preapprovalPrivacyDisclaimer={preapprovalPrivacyDisclaimer}
                                 country={country}
                                 useNewCheckoutDesign={useNewCheckoutDesign}
                             />
