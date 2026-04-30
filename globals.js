87c27b0069e62c6be348b7d63b6601e39c1a9102
perco12 <141269770+perco12@users.noreply.github.com>
2026-04-29 21:06:01 -0500

    feat: add prequalification route to CYSP CTA button (#1304)

--- globals.js
+++ globals.js
@@ -40,7 +40,8 @@ module.exports = (env = { TARGET: 'sdk' }) => ({
             __TREATMENTS__: '/credit-presentment/experiments/local',
             __MODAL__: '/credit-presentment/smart/modal',
             __LOGGER__: '/credit-presentment/glog',
-            __CREDIT_APPLY__: '/credit-application/paypal-credit-card/da/us/billing'
+            __CREDIT_APPLY__: '/credit-application/paypal-credit-card/da/us/billing',
+            __PREQUALIFICATION__: '/paylateracq/prequalify'
         },
         __FAQ__: {
             __BASE_URL__: 'https://developer.paypal.com/docs/checkout/pay-later/us'
