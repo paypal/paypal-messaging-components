de53b56c8f9f2283afe1bdb93f9d06901bb00bc6
anushreeshah2405 <anushah@paypal.com>
2026-06-03 10:12:10 -0700

    functional tests added

---
+++ tests/functional/v2/config/CA/DEV_CA_LONG_TERM_CHECKOUT.js
@@ -0,0 +1,49 @@
+export const DEV_CA_LONG_TERM_CHECKOUT = {
+    testFileName: 'longTerm',
+    country: 'CA',
+    description: 'CA merchant eligible for pay monthly (long term)',
+    minAmount: 49,
+    maxAmount: 10000,
+    amounts: [
+        {
+            value: '49.00',
+            message: 'Qualifying Pay Monthly',
+            expectedValue: 'Pay Monthly',
+            modalContent: {
+                offerHeadline: '$9.23/mo. for 6 months',
+                updatedOfferHeadline: '$94.17/mo. for 6 months',
+                offerFieldValues: ['26%', '$6.37', '$55.37'],
+                aprDisclaimer: '*APR is 0% to 31.99%. Terms and rates vary based on purchase amount and your credit.',
+                cta: 'Continue with Pay Monthly'
+            }
+        },
+        {
+            value: '1000.00',
+            message: 'Qualifying Pay Monthly',
+            expectedValue: 'Pay Monthly',
+            modalContent: {
+                offerHeadline: '$188.33/mo. for 6 months',
+                updatedOfferHeadline: '$94.17/mo. for 6 months',
+                offerFieldValues: ['26%', '$130.00', '$1,130.00'],
+                aprDisclaimer: '*APR is 0% to 31.99%. Terms and rates vary based on purchase amount and your credit.',
+                cta: 'Continue with Pay Monthly'
+            }
+        },
+        {
+            value: '0.01',
+            message: 'Non-qualifying Pay Monthly',
+            expectedValue: 'Pay Monthly',
+            modalContent: {
+                belowMinAmountErr: 'Enter an amount of $49.00 or more.'
+            }
+        },
+        {
+            value: '10000.01',
+            message: 'Non-qualifying Pay Monthly',
+            expectedValue: 'Pay Monthly',
+            modalContent: {
+                aboveMaxAmountErr: 'Enter an amount no larger than $10,000.00.'
+            }
+        }
+    ]
+};
---
+++ tests/functional/v2/config/CA/DEV_CA_LONG_TERM_CHECKOUT_FR.js
@@ -0,0 +1,51 @@
+export const DEV_CA_LONG_TERM_CHECKOUT_FR = {
+    testFileName: 'longTerm',
+    country: 'CA',
+    description: 'CA merchant eligible for pay monthly (long term) - French',
+    minAmount: 49,
+    maxAmount: 10000,
+    amounts: [
+        {
+            value: '49.00',
+            message: 'Qualifying Pay Monthly',
+            expectedValue: 'Payer par mois',
+            modalContent: {
+                offerHeadline: '$9.23 CAD/mois pendant 6 mois',
+                updatedOfferHeadline: '$94.17 CAD/mois pendant 6 mois',
+                offerFieldValues: ['26%', '$6.37', '$55.37'],
+                aprDisclaimer:
+                    "*Le TAEG est de 0 % à 31,99 %. Les conditions et les taux varient en fonction du montant de l'achat et de votre crédit.",
+                cta: 'Continuer avec Payer par mois'
+            }
+        },
+        {
+            value: '1000.00',
+            message: 'Qualifying Pay Monthly',
+            expectedValue: 'Payer par mois',
+            modalContent: {
+                offerHeadline: '$188.33 CAD/mois pendant 6 mois',
+                updatedOfferHeadline: '$94.17 CAD/mois pendant 6 mois',
+                offerFieldValues: ['26%', '$130.00', '$1,130.00'],
+                aprDisclaimer:
+                    "*Le TAEG est de 0 % à 31,99 %. Les conditions et les taux varient en fonction du montant de l'achat et de votre crédit.",
+                cta: 'Continuer avec Payer par mois'
+            }
+        },
+        {
+            value: '0.01',
+            message: 'Non-qualifying Pay Monthly',
+            expectedValue: 'Payer par mois',
+            modalContent: {
+                belowMinAmountErr: 'Indiquez un montant de $49.00 CAD ou plus'
+            }
+        },
+        {
+            value: '10000.01',
+            message: 'Non-qualifying Pay Monthly',
+            expectedValue: 'Payer par mois',
+            modalContent: {
+                aboveMaxAmountErr: 'Indiquez un montant de $10,000.00 CAD ou moins'
+            }
+        }
+    ]
+};
