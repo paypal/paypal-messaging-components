d0cf9f0fe3a2401836acbea8f48db93fac1ac16d
Farzin <fjabbary@paypal.com>
2026-06-22 10:10:55 -0700

    feat: Adjust UK Pi3 for regulatory changes (#1330) (#1334)

--- tests/functional/v2/config/GB/DEV_GB_SHORT_TERM.js
+++ tests/functional/v2/config/GB/DEV_GB_SHORT_TERM.js
@@ -10,7 +10,8 @@ export const DEV_GB_SHORT_TERM = {
             message: 'Non-qualifying Pay in 3',
             expectedValue: 'Pay in 3 interest-free payments',
             modalContent: {
-                subheadline: 'Available for purchases of £30 to £2,000. No sign-up fees or late fees.',
+                subheadline:
+                    'Available for purchases of £30 to £2,000. No interest or late fees. Eligibility subject to status.',
                 periodicPayment: null
             }
         },
@@ -19,7 +20,8 @@ export const DEV_GB_SHORT_TERM = {
             message: 'Qualifying Pay in 3',
             expectedValue: 'Pay in 3 interest-free payments of £10.00',
             modalContent: {
-                subheadline: 'Split your purchase of £30.00 into 3 with no sign-up fees or late fees.',
+                subheadline:
+                    'Split your purchase of £30.00 into 3 with no interest or late fees. Eligibility subject to status.',
                 periodicPayment: '£10.00'
             }
         },
@@ -28,7 +30,8 @@ export const DEV_GB_SHORT_TERM = {
             message: 'Non-qualifying Pay in 3',
             expectedValue: 'Pay in 3 interest-free payments',
             modalContent: {
-                subheadline: 'Available for purchases of £30 to £2,000. No sign-up fees or late fees.',
+                subheadline:
+                    'Available for purchases of £30 to £2,000. No interest or late fees. Eligibility subject to status.',
                 periodicPayment: ''
             }
         }
