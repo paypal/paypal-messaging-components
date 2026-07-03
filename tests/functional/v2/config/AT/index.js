00bf7819bc4983f0c84083ff7d609af3848bdf89
lyerabati16 <lyerabati@paypal.com>
2026-07-03 16:29:51 +0530

    feat: Add FTs and Snapshots with Dev accounts configuration for AT offers

---
+++ tests/functional/v2/config/AT/index.js
@@ -0,0 +1,7 @@
+export { DEV_AT_PAY_IN_1 } from './DEV_AT_PAY_IN_1';
+export { DEV_AT_PAY_IN_1_EN } from './DEV_AT_PAY_IN_1_EN';
+export { DEV_AT_LONG_TERM } from './DEV_AT_LONG_TERM';
+export { DEV_AT_LONG_TERM_EN } from './DEV_AT_LONG_TERM_EN';
+export { DEV_AT_MULTI } from './DEV_AT_MULTI';
+export { DEV_AT_LONG_TERM_0APR } from './DEV_AT_LONG_TERM_0APR';
+export { DEV_AT_LONG_TERM_EN_0APR } from './DEV_AT_LONG_TERM_EN_0APR';
