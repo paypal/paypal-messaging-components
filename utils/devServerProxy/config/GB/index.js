8820b39720e1aa1f2894a5b614794b36b7bbfd61
lyerabati16 <lyerabati@paypal.com>
2026-07-06 16:23:03 +0530

    feat: Add devServerProxy accounts configuration for GB Pi30

--- utils/devServerProxy/config/GB/index.js
+++ utils/devServerProxy/config/GB/index.js
@@ -1,4 +1,100 @@
 export default {
+    DEV_GB_PAY_IN_1: {
+        country: 'GB',
+        modalViews: [
+            {
+                template: 'pay_in_1.json',
+                product: 'PAY_LATER_PAY_IN_1'
+            }
+        ],
+        messageThresholds: [
+            {
+                amount: 0,
+                template: 'pi30nq.json',
+                product: 'PAY_LATER_PAY_IN_1'
+            },
+            {
+                amount: 1,
+                template: 'pi30q.json',
+                product: 'PAY_LATER_PAY_IN_1'
+            },
+            {
+                amount: 900.01,
+                template: 'pi30nq.json',
+                product: 'PAY_LATER_PAY_IN_1'
+            }
+        ],
+        offers: {
+            PAY_LATER_PAY_IN_1: [
+                {
+                    totalPayments: 1,
+                    apr: 0,
+                    nominalRate: 0,
+                    minAmount: 1,
+                    maxAmount: 900
+                }
+            ]
+        }
+    },
+    DEV_GB_MULTI: {
+        country: 'GB',
+        modalViews: [
+            {
+                template: 'product_list.json',
+                product: 'PRODUCT_LIST'
+            },
+            {
+                template: 'pay_in_1.json',
+                product: 'PAY_LATER_PAY_IN_1'
+            },
+            {
+                template: 'short_term.json',
+                product: 'PAY_LATER_SHORT_TERM'
+            }
+        ],
+        messageThresholds: [
+            {
+                amount: 0,
+                template: 'pi30nq.json',
+                product: 'PAY_LATER_PAY_IN_1'
+            },
+            {
+                amount: 1,
+                template: 'pi30q.json',
+                product: 'PAY_LATER_PAY_IN_1'
+            },
+            {
+                amount: 75,
+                template: 'plq.json',
+                product: 'PAY_LATER_SHORT_TERM'
+            },
+            {
+                amount: 3000.01,
+                template: 'pl.json',
+                product: 'PAY_LATER_SHORT_TERM'
+            }
+        ],
+        offers: {
+            PAY_LATER_PAY_IN_1: [
+                {
+                    totalPayments: 1,
+                    apr: 0,
+                    nominalRate: 0,
+                    minAmount: 1,
+                    maxAmount: 900
+                }
+            ],
+            PAY_LATER_SHORT_TERM: [
+                {
+                    totalPayments: 3,
+                    apr: 0,
+                    nominalRate: 0,
+                    minAmount: 30,
+                    maxAmount: 3000
+                }
+            ]
+        }
+    },
     DEV_GB_SHORT_TERM: {
         country: 'GB',
         modalViews: [
