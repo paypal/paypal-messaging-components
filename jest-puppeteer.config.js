8740fcb7901e1e38050bdbf0308424de30a183e5
opercy <opercy@paypal.com>
2026-07-16 16:43:30 -0500

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -9,6 +9,7 @@ module.exports = {
         // slows puppeteer in milliseconds so you can see what is going on
         slowMo: showBrowser ? 50 : 0,
         ignoreHTTPSErrors: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
         server: {
             port: 8080
         }
