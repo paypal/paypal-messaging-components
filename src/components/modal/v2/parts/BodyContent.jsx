374364e92374156ef0f0af36dadd34ed20bbb7fd
surekhaw <71471412+surekhaw@users.noreply.github.com>
2026-08-03 13:27:40 -0700

    feat: Pl2go darkmode (#1370)

--- src/components/modal/v2/parts/BodyContent.jsx
+++ src/components/modal/v2/parts/BodyContent.jsx
@@ -9,6 +9,7 @@ import {
     useScroll,
     useDidUpdateEffect,
     useTransitionState,
+    usePrefersDarkMode,
     isLander
 } from '../lib';
 import Header from './Header';
@@ -65,6 +66,9 @@ const BodyContent = () => {
     const useV5Design = productMeta?.useV5Design === 'true';
     const use5Dot1Design = productMeta?.['v5.1'];
     const useNewCheckoutDesign = features?.includes('new-checkout-design') ? 'true' : 'false';
+    const prefersDarkMode = usePrefersDarkMode();
+    const useDarkMode =
+        productMeta?.enableDarkMode === 'true' && (prefersDarkMode || !!features?.includes('use-dark-mode'));
 
     // add v4Design or v5Design class to root html to update lander specific styles to v4 or v5 respectively
     const documentClassName = document.documentElement.className;
@@ -111,6 +115,7 @@ const BodyContent = () => {
                 content={content}
                 productMeta={productMeta}
                 useNewCheckoutDesign={useNewCheckoutDesign}
+                useDarkMode={useDarkMode}
                 use5Dot1Design={use5Dot1Design}
                 openProductList={openProductList}
             />
@@ -128,6 +133,7 @@ const BodyContent = () => {
                 content={content}
                 productMeta={productMeta}
                 useNewCheckoutDesign={useNewCheckoutDesign}
+                useDarkMode={useDarkMode}
                 use5Dot1Design={use5Dot1Design}
                 openProductList={openProductList}
             />
@@ -174,12 +180,13 @@ const BodyContent = () => {
                     useV4Design={useV4Design}
                     useV5Design={useV5Design}
                     use5Dot1Design={use5Dot1Design}
+                    useDarkMode={useDarkMode}
                 />
             )}
             <div
                 className={`content__container ${useV4Design ? 'v4Design' : ''} ${useV5Design ? 'v5Design' : ''} ${
                     useNewCheckoutDesign === 'true' ? 'checkout' : ''
-                } ${use5Dot1Design ? 'v5Dot1Design' : ''} `}
+                } ${use5Dot1Design ? 'v5Dot1Design' : ''} ${useDarkMode ? 'darkMode' : ''}`}
             >
                 <main className="main">
                     <div className="content__body">{viewComponents[viewName]}</div>
