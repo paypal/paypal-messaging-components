ae6770236a01b1c3ac7a076d6efce4c3f27485a4
judowning <judowning@paypal.com>
2026-08-14 16:00:07 -0700

    Add AU PM messages, mutations, and test accounts

---
+++ src/server/locale/AU/mutations/long_term_nq_gtz.js
@@ -0,0 +1,146 @@
+import Logo from '../../../message/logos';
+import {
+    textWrap,
+    messageLogoWidth,
+    altNoWrap,
+    setLogoTop,
+    logo20x1,
+    xSmallFallback
+} from '../../../message/mediaQueries';
+import { flexLogoMutations, textLogoMutations } from '../../../message/logoMutations';
+
+const flex = [
+    [
+        'default',
+        {
+            logo: Logo.PP_PAYPAL.WHITE,
+            headline: [
+                {
+                    tag: 'xsmall'
+                },
+                {
+                    tag: 'medium'
+                }
+            ],
+            disclaimer: ['extra', 'default']
+        }
+    ],
+    [
+        'ratio:20x1',
+        {
+            styles: [logo20x1()]
+        }
+    ],
+    [
+        'ratio:8x1',
+        {
+            headline: [
+                {
+                    tag: 'xsmall'
+                },
+                {
+                    tag: 'medium'
+                }
+            ]
+        }
+    ],
+    ['color:white-no-border', { logo: Logo.PP_PAYPAL.COLOR }],
+    ...flexLogoMutations
+];
+
+export default {
+    'layout:flex': flex,
+    'layout:text': [
+        [
+            'default',
+            ({ textSize }) => ({
+                styles: [
+                    `@media screen and (max-width: ${textSize * 18.5}px) {
+                        .message__headline > .tag--medium > span.br:first-child { white-space: normal; }
+                    }`,
+                    xSmallFallback(textSize * 14),
+                    textWrap(textSize * 32, textSize, 'AU'),
+                    messageLogoWidth(false, textSize * 4, textSize * 1.25),
+                    setLogoTop(textSize * 14)
+                ],
+                logo: Logo.PP_PAYPAL.COLOR,
+                headline: [
+                    {
+                        tag: 'medium',
+                        br: ['purchases']
+                    },
+                    {
+                        tag: 'xsmall'
+                    }
+                ],
+                disclaimer: ['extra', 'default']
+            })
+        ],
+        [
+            'logo.type:primary && logo.position:right',
+            ({ textSize }) => ({
+                styles: [
+                    xSmallFallback(textSize * 13),
+                    setLogoTop(textSize * 32.5),
+                    messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25)
+                ]
+            })
+        ],
+        [
+            'logo.type:primary && logo.position:top',
+            ({ textSize }) => ({
+                styles: [xSmallFallback(textSize * 13), messageLogoWidth(textSize * 6, textSize * 4, textSize * 1.25)]
+            })
+        ],
+        [
+            'logo.type:alternative',
+            ({ textSize }) => ({
+                styles: [
+                    `@media screen and (max-width: ${textSize * 10.6}px) { .message__content { white-space: nowrap; }}`,
+                    xSmallFallback(textSize * 14),
+                    altNoWrap(textSize * 10.6),
+                    textWrap(textSize * 32, textSize, 'AU'),
+                    messageLogoWidth(textSize * 1.75, textSize * 4, textSize * 1.25)
+                ],
+                logo: Logo.PP_PAYPAL.COLOR[0]
+            })
+        ],
+        [
+            'logo.type:none',
+            ({ textSize }) => ({
+                styles: [xSmallFallback(textSize * 14)],
+                logo: false,
+                headline: [
+                    {
+                        tag: 'medium',
+                        br: ['purchases'],
+                        replace: [[/\.$/, '']]
+                    },
+                    {
+                        tag: 'xsmall.2',
+                        replace: [['later.', 'later']]
+                    }
+                ]
+            })
+        ],
+        [
+            'logo.type:inline',
+            ({ textSize }) => ({
+                styles: [xSmallFallback(textSize * 14), `.message__logo { width: ${textSize * 4}px }`],
+                logo: Logo.NO_PP_MONOGRAM.COLOR,
+                headline: [
+                    {
+                        tag: 'medium',
+                        br: ['purchases'],
+                        replace: [[/\.$/, '']]
+                    },
+                    {
+                        tag: 'xsmall.2',
+                        replace: [['later.', 'later']]
+                    }
+                ]
+            })
+        ],
+        ...textLogoMutations
+    ]
+};
