891715bb219090cd8265360085ff94c725fed1e0
User <user@example.com>
2026-09-23 12:35:16 -0500

    fix: scope FR disclosure span margin fix to avoid other-country snapshot regressions (DTCRCMERC-5665)

--- src/components/modal/v2/parts/views/LongTerm/Content.jsx
+++ src/components/modal/v2/parts/views/LongTerm/Content.jsx
@@ -252,7 +252,7 @@ export const LongTerm = ({
                     useNewCheckoutDesign === 'true' ? 'checkout' : ''
                 } ${useV5Design === 'true' ? 'v5Design' : ''} ${getEuroStyleClass(country)} ${
                     isPrequalExperience ? 'prequal-fixed-offset' : ''
-                } ${useDarkMode ? 'darkMode' : ''}`}
+                } ${useDarkMode ? 'darkMode' : ''} ${country?.toLowerCase() ?? ''}`}
             >
                 {getDisclosure(disclosure)}
             </div>
