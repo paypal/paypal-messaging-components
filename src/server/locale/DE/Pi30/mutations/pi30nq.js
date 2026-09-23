a449d045f4e9af7daf0e88dde4d3ae0caae027d5
mandevi <mandevi@paypal.com>
2026-09-23 19:45:13 +0530

    Fix the headline breaking in grayscale and monochrome flex

--- src/server/locale/DE/Pi30/mutations/pi30nq.js
+++ src/server/locale/DE/Pi30/mutations/pi30nq.js
@@ -18,8 +18,7 @@ const headlineBreaks = [
         breaks: ['kaufen,']
     },
     {
-        sizes: ['medium'],
-        breaks: ['von']
+        sizes: ['medium']
     }
 ].reduce((acc, item) => {
     const { sizes, breaks } = item;
